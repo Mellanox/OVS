Mellanox/OVS

commit d8f39c9e2da7c5dc0491d5f45b368626755e8b34

ofproto-dpif: Add offloaded:yes to dpif/dump-flow
When executing command: ovs-appctl dpif/dump-flows <bridge name> and
in case a flow is offloaded - add an "offloaded:yes" indication.

Signed-off-by: Ophir Munk <ophirmu@mellanox.com>

ofproto/ofproto-dpif.c
@@ -5731,6 +5731,9 @@ ofproto_unixctl_dpif_dump_flows(struct unixctl_conn *conn,
         dpif_flow_stats_format(&f.stats, &ds);
         ds_put_cstr(&ds, ", actions:");
         format_odp_actions(&ds, f.actions, f.actions_len, portno_names);
+        if (f.attrs.offloaded) {
+            ds_put_cstr(&ds, ", offloaded:yes");
+        }
         ds_put_char(&ds, '\n');
     }
     dpif_flow_dump_thread_destroy(flow_dump_thread);

commit 822a33604662d0c885dcb2421f48cc38e01252ca

ofproto-dpif: remove offload:yes from dump-flows
1. When executing ovs-appctl dpif/dump-flows -m br-int a printout of
"offloaded:yes" was added in case the flow was offloaded. This printout
is redundant since the it already exists when executing ovs-appctl
dpctl/dump-flows. This commit removes the redundant printout.
2. A reduncant new line was add to "offloaded:yes\n" printout in file
lib/dpctl.c. this commit removes the redundant new line.

Fixes: d8f39c9e2da7 ("ofproto-dpif: Add offloaded:yes to dpif/dump-flow")
Fixes: 9d8a2e5dc88d ("dpif: Add marked packets stats")
Signed-off-by: Ophir Munk <ophirmu@mellanox.com>

lib/dpctl.c
@@ -828,7 +828,7 @@ format_dpif_flow(struct ds *ds, const struct dpif_flow *f, struct hmap *ports,
 
     dpif_flow_stats_format(&f->stats, ds, false);
     if (dpctl_p->verbosity && f->attrs.offloaded) {
-        ds_put_cstr(ds, ", offloaded:yes\n");
+        ds_put_cstr(ds, ", offloaded:yes");
     }
     if (dpctl_p->verbosity && f->attrs.dp_layer) {
         ds_put_format(ds, ", dp:%s", f->attrs.dp_layer);

ofproto/ofproto-dpif.c
@@ -5732,9 +5732,6 @@ ofproto_unixctl_dpif_dump_flows(struct unixctl_conn *conn,
         dpif_flow_stats_format(&f.stats, &ds, verbosity);
         ds_put_cstr(&ds, ", actions:");
         format_odp_actions(&ds, f.actions, f.actions_len, portno_names);
-        if (f.attrs.offloaded) {
-            ds_put_cstr(&ds, ", offloaded:yes");
-        }
         ds_put_char(&ds, '\n');
     }
     dpif_flow_dump_thread_destroy(flow_dump_thread);