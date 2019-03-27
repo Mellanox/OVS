Mellanox/OVS

commit 056785fa7c4ebf0ae8986e24657fb1d194b4db0c

dpif-netdev: Add is_hwol flag to indicate HW offload
A flow can be in one of three offloading modes:
1. No offload. In this case we should not call rte_flow_query().
2. Partial offload (RSS + MARK actions only). In this case
we should not call rte_flow_query() since the flow actions are
executed in SW which will take care of updating the flow statistics.
3. Full offload. In this case rte_flow_query() must be called as
implemented in [1].

When adding a flow we set flag 'is_hwol' to indicate whether HW counters
should be read during flow_dump_next() call.

[1]
Commit 63573d1fe8:
("dpif-netdev: Read hw stats during flow_dump_next() call")

Signed-off-by: Ophir Munk <ophirmu@mellanox.com>

lib/dpif-netdev.c
@@ -522,6 +522,7 @@ struct dp_netdev_flow {
 
     bool dead;
     uint32_t mark;               /* Unique flow mark assigned to a flow */
+    bool is_hwol;                /* true if flow is fully offloaded */
 
     /* Statistics. */
     struct dp_netdev_flow_stats stats;
@@ -2359,6 +2360,7 @@ dp_netdev_flow_offload_put(struct dp_flow_offload_item *offload)
         }
     }
     info.flow_mark = mark;
+    info.is_hwol = false;
 
     ovs_mutex_lock(&pmd->dp->hw_offload_mutex);
     port = dp_netdev_lookup_port(pmd->dp, in_port);
@@ -2371,6 +2373,7 @@ dp_netdev_flow_offload_put(struct dp_flow_offload_item *offload)
                           offload->actions_len, &flow->mega_ufid, &info,
                           NULL);
     ovs_mutex_unlock(&pmd->dp->hw_offload_mutex);
+    flow->is_hwol = info.is_hwol;
 
     if (ret) {
         if (!modification) {
@@ -3041,6 +3044,9 @@ dp_netdev_flow_to_dpif_flow(const struct dp_netdev_flow *netdev_flow,
     flow->ufid_present = true;
     flow->pmd_id = netdev_flow->pmd_id;
     get_dpif_flow_stats(netdev_flow, &flow->stats);
+
+    flow->attrs.offloaded = netdev_flow->is_hwol;
+    flow->attrs.dp_layer = "ovs-dpdk";
 }
 
 static int
@@ -3637,10 +3643,9 @@ dpif_netdev_flow_dump_next(struct dpif_flow_dump_thread *thread_,
                 flow = netdev_flows[n_flows] = CONTAINER_OF(node,
                                                     struct dp_netdev_flow,
                                                     node);
-                /* Read offload stats in case ufid equals mega_ufid. */
+                /* Read hardware stats in case of hardware offload */
                 if (netdev_is_flow_api_enabled() &&
-                   (!memcmp(&flow->ufid, &flow->mega_ufid,
-                            sizeof flow->ufid))) {
+                   (flow->is_hwol)) {
                     dp_netdev_offload_used(flow, pmd);
                 }
             }

lib/netdev-dpdk.c
@@ -4130,8 +4130,8 @@ struct flow_actions {
 
 #define NETDEV_DPDK_CLASS(NAME, INIT, CONSTRUCT, DESTRUCT,    \
                           SET_CONFIG, SET_TX_MULTIQ, SEND,    \
-                          GET_CARRIER, GET_STATS,			  \
-                          GET_CUSTOM_STATS,					  \
+                          GET_CARRIER, GET_STATS,             \
+                          GET_CUSTOM_STATS,                   \
                           GET_FEATURES, GET_STATUS,           \
                           RECONFIGURE, RXQ_RECV)              \
 {                                                             \
@@ -4166,7 +4166,7 @@ struct flow_actions {
     netdev_dpdk_get_carrier_resets,                           \
     netdev_dpdk_set_miimon,                                   \
     GET_STATS,                                                \
-    GET_CUSTOM_STATS,										  \
+    GET_CUSTOM_STATS,                                         \
     GET_FEATURES,                                             \
     NULL,                       /* set_advertisements */      \
     NULL,                       /* get_pt_mode */             \

lib/netdev-rte-offloads.c
@@ -1511,6 +1511,7 @@ netdev_dpdk_add_rte_flow_offload(struct netdev_rte_port *rte_port,
             }
 
             rte_port->default_rte_flow[vport->table_id] = def_flow;
+            info->is_hwol = true;
         }
     } else { /* Previous actions cannot be offloaded to hw,
                 try offloading Mark and RSS actions */
@@ -1790,7 +1791,7 @@ netdev_vport_vxlan_add_rte_flow_offload(struct netdev_rte_port * rte_port,
                                         struct nlattr *nl_actions,
                                         size_t actions_len,
                                         const ovs_u128 * ufid ,
-                                        struct offload_info * info OVS_UNUSED,
+                                        struct offload_info * info,
                               struct dpif_flow_stats * flow_stats  OVS_UNUSED)
 {
     if (!actions_len || !nl_actions) {
@@ -1880,7 +1881,7 @@ netdev_vport_vxlan_add_rte_flow_offload(struct netdev_rte_port * rte_port,
         } else {
             VLOG_DBG("second flow of decap created");
         }
-
+        info->is_hwol = false;
         ufid_hw_offload_add_rte_flow(ufid_hw_offload, flow,
                                              rte_port_phy_arr[i]->dpdk_port_id,
                                              0);
@@ -1897,7 +1898,7 @@ out:
 
 int netdev_vport_flow_put(struct netdev * netdev , struct match * match,
               struct nlattr *actions OVS_UNUSED, size_t actions_len OVS_UNUSED,
-              const ovs_u128 * ufid , struct offload_info * info OVS_UNUSED,
+              const ovs_u128 * ufid , struct offload_info * info,
               struct dpif_flow_stats * flow_stats  OVS_UNUSED)
 {
     odp_port_t in_port = match->flow.in_port.odp_port;

lib/netdev.h
@@ -207,6 +207,7 @@ struct offload_info {
      * it will be in the pkt meta data.
      */
     uint32_t flow_mark;
+    bool is_hwol; /* true if flow is fully offloaded */
 };
 struct dpif_class;
 struct netdev_flow_dump;

commit 35308c0b3688a3b66f715fac19fe4b57a6d20f9a

dpif-netdev: Initialize is_hwol to false.
During the dp_netdev_flow_add() call we initialize the added flow fields
with default values. The initialization of is_hwol was missing. It should
be initialized to false since the flow may be not offloaded. In case the
flow is offloaded the is_hwol field will be set to true during the call
to netdev_vport_flow_put().

Fixes: 056785fa7c4e ("dpif-netdev: Add is_hwol flag to indicate HW offload")
Signed-off-by: Ophir Munk <ophirmu@mellanox.com>

lib/dpif-netdev.c
@@ -3224,6 +3224,7 @@ dp_netdev_flow_add(struct dp_netdev_pmd_thread *pmd,
     flow->dead = false;
     flow->batch = NULL;
     flow->mark = INVALID_FLOW_MARK;
+    flow->is_hwol = false;
     *CONST_CAST(unsigned *, &flow->pmd_id) = pmd->core_id;
     *CONST_CAST(struct flow *, &flow->flow) = match->flow;
     *CONST_CAST(ovs_u128 *, &flow->ufid) = *ufid;