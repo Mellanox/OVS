Mellanox/OVS

commit 7c2490fc8e5f08b53b6c3814a32d17121b82d62b

netdev-rte-offloads: Add tunnel encap hw offload.
Tunnel encap action is being done as part of a clone action.
This patch supports a clone actions and as part of it, support
both tunnel_push and output actions.

These actions are being offloaded to the hw using rte_flow with
actions of RAW_ENCAP and PORT_ID.

Since flow will be fully offloaded, the sw will not see it. So,
there is a need to add a count action as well, so stats and
flow aging work properly.

Signed-off-by: Asaf Penso <asafp@mellanox.com>

lib/netdev-rte-offloads.c
@@ -963,6 +963,78 @@ get_output_port(const struct nlattr *a,
     return 0;
 }
 
+static void
+netdev_rte_add_raw_encap_flow_action(const struct nlattr *a,
+                                     struct rte_flow_action_raw_encap *encap,
+                                     struct flow_actions *actions)
+{
+    const struct ovs_action_push_tnl *tunnel = nl_attr_get(a);
+    encap->data = (uint8_t *)tunnel->header;
+    encap->preserve = NULL;
+    encap->size = tunnel->header_len;
+
+    add_flow_action(actions, RTE_FLOW_ACTION_TYPE_RAW_ENCAP, encap);
+}
+
+static int
+netdev_rte_add_clone_flow_action(const struct nlattr *nlattr,
+                                 struct rte_flow_action_raw_encap *raw_encap,
+                                 struct rte_flow_action_count *count,
+                                 struct rte_flow_action_port_id *output,
+                                 struct flow_actions *actions)
+{
+    const struct nlattr *clone_actions = nl_attr_get(nlattr);
+    size_t clone_actions_len = nl_attr_get_size(nlattr);
+    const struct nlattr *ca;
+    unsigned int cleft;
+    int result = 0;
+
+    NL_ATTR_FOR_EACH_UNSAFE (ca, cleft, clone_actions, clone_actions_len) {
+        int clone_type = nl_attr_type(ca);
+        if (clone_type == OVS_ACTION_ATTR_TUNNEL_PUSH) {
+            netdev_rte_add_raw_encap_flow_action(ca, raw_encap, actions);
+
+        } else if (clone_type == OVS_ACTION_ATTR_OUTPUT) {
+            result = get_output_port(ca, output);
+            if (result) {
+                break;
+            }
+            netdev_rte_add_count_flow_action(count, actions);
+            netdev_rte_add_port_id_flow_action(output, actions);
+        }
+    }
+
+    return result;
+}
+
+static struct rte_flow *
+netdev_dpdk_add_jump_to_non_root_table(struct netdev *netdev,
+                                       struct offload_info *info)
+{
+    struct rte_flow_attr flow_attr = {
+        .group = 0,
+        .priority = 1,
+        .ingress = 1,
+        .egress = 0,
+        .transfer = 1
+    };
+
+    struct flow_patterns patterns = { .items = NULL, .cnt = 0 };
+    struct flow_actions actions = { .actions = NULL, .cnt = 0 };
+    struct rte_flow_action_jump jump = {0};
+    struct rte_flow *flow = NULL;
+
+    add_flow_pattern(&patterns, RTE_FLOW_ITEM_TYPE_END, NULL, NULL);
+
+    jump.group = 1; // TODO: need to find the table id
+    add_flow_action(&actions, RTE_FLOW_ACTION_TYPE_JUMP, &jump);
+
+    flow = netdev_rte_offload_flow(netdev, info, &patterns, &actions,
+                                   &flow_attr);
+
+    return flow;
+}
+
 static struct rte_flow *
 netdev_rte_offloads_add_flow(struct netdev *netdev,
                              const struct match *match,
@@ -1004,6 +1076,9 @@ netdev_rte_offloads_add_flow(struct netdev *netdev,
     struct rte_flow_action_jump jump = {0};
     struct rte_flow_action_count count = {0};
     struct rte_flow_action_port_id output = {0};
+    struct rte_flow_action_port_id clone_output = {0};
+    struct rte_flow_action_count clone_count = {0};
+    struct rte_flow_action_raw_encap clone_raw_encap = {0};
     struct netdev_rte_port *vport = NULL;
 
     NL_ATTR_FOR_EACH_UNSAFE (a, left, nl_actions, actions_len) {
@@ -1017,6 +1092,7 @@ netdev_rte_offloads_add_flow(struct netdev *netdev,
             netdev_rte_add_count_flow_action(&count, &actions);
             action_bitmap |= 1 << OVS_ACTION_ATTR_TUNNEL_POP;
             result = 0;
+
         } else if ((enum ovs_action_attr) type == OVS_ACTION_ATTR_OUTPUT) {
             result = get_output_port(a, &output);
             if (result) {
@@ -1025,6 +1101,16 @@ netdev_rte_offloads_add_flow(struct netdev *netdev,
             netdev_rte_add_count_flow_action(&count, &actions);
             netdev_rte_add_port_id_flow_action(&output, &actions);
             action_bitmap |= 1 << OVS_ACTION_ATTR_OUTPUT;
+
+        } else if ((enum ovs_action_attr) type == OVS_ACTION_ATTR_CLONE) {
+            result = netdev_rte_add_clone_flow_action(a, &clone_raw_encap,
+                                                      &clone_count,
+                                                      &clone_output, &actions);
+            if (result) {
+                break;
+            }
+            action_bitmap |= 1 << OVS_ACTION_ATTR_CLONE;
+
         } else {
             /* Unsupported action for offloading */
             result = -1;
@@ -1040,6 +1126,19 @@ netdev_rte_offloads_add_flow(struct netdev *netdev,
         VLOG_DBG("Flow with Mark and RSS actions: NIC offload was %s",
                 flow ? "succeeded" : "failed");
     } else {
+        /* For better performance the clone action is offloaded to the
+         * vport table, and a jump rule is added to table 0 */
+        if (action_bitmap & (1 << OVS_ACTION_ATTR_CLONE)) {
+            flow = netdev_dpdk_add_jump_to_non_root_table(netdev, info);
+            VLOG_DBG("Flow with catch-all and jump actions: "
+                    "eSwitch offload was %s", flow ? "succeeded" : "failed");
+            if (!flow) {
+                goto out;
+            }
+            /* The flows for encap should be added to group 1 */
+            flow_attr.group = 1;
+        }
+
         /* Actions are supported, offload the flow */
         flow_attr.transfer = 1;
         flow = netdev_rte_offload_flow(netdev, info, &patterns, &actions,

commit 021095b24e49b53125bf4736f8eca6132ca28a0b

netdev-rte-offloads: Fix roleback for failure cases with flow put
In case flow put encounters an error, all settings should be roled back.

Issue: 1786626
Fixes: 7c2490fc8e5f ("netdev-rte-offloads: Add tunnel encap hw offload.")
Signed-off-by: Eli Britstein <elibr@mellanox.com>

lib/netdev-rte-offloads.c
@@ -1046,6 +1046,9 @@ err:
     return -1;
 }
 
+static int
+netdev_offloads_flow_del(const ovs_u128 *ufid);
+
 int
 netdev_rte_offloads_flow_put(struct netdev *netdev, struct match *match,
                              struct nlattr *actions, size_t actions_len,
@@ -1072,8 +1075,7 @@ netdev_rte_offloads_flow_put(struct netdev *netdev, struct match *match,
 
     if (ufid_hw_offload) {
         VLOG_DBG("got modification and destroying previous rte_flow");
-        ufid_hw_offload_remove(ufid, &rte_port->ufid_to_rte);
-        ret = netdev_rte_port_ufid_hw_offload_free(ufid_hw_offload);
+        ret = netdev_offloads_flow_del(ufid);
         if (ret < 0) {
             return -ret;
         }
@@ -1083,7 +1085,8 @@ netdev_rte_offloads_flow_put(struct netdev *netdev, struct match *match,
     ufid_hw_offload = netdev_rte_port_ufid_hw_offload_alloc(2, ufid);
     if (!ufid_hw_offload) {
         VLOG_WARN("failed to allocate ufid_hw_offlaod, OOM");
-        return ENOMEM;
+        ret = ENOMEM;
+        goto err;
     }
 
     ufid_hw_offload_add(ufid_hw_offload, &rte_port->ufid_to_rte);
@@ -1092,21 +1095,28 @@ netdev_rte_offloads_flow_put(struct netdev *netdev, struct match *match,
     ret = netdev_rte_offloads_validate_flow(match, false);
     if (ret < 0) {
         VLOG_DBG("flow pattern is not supported");
-        return EINVAL;
+        ret = EINVAL;
+        goto err;
     }
 
     rte_flow = netdev_rte_offloads_add_flow(netdev, match, actions,
                                             actions_len, ufid, info,
                                             &rte_flow0);
     if (!rte_flow) {
-        return ENODEV;
+        ret = ENODEV;
+        goto err;
     }
 
     if (rte_flow0) {
         ufid_hw_offload_add_rte_flow(ufid_hw_offload, rte_flow0, netdev);
     }
     ufid_hw_offload_add_rte_flow(ufid_hw_offload, rte_flow, netdev);
+
     return 0;
+
+err:
+    netdev_offloads_flow_del(ufid);
+    return ret;
 }
 
 static int