Mellanox/OVS

commit 2cb10221f6118ec7c1ebcfe4b5f24a4f8e7192bc

offloads: add flow restore support
Currently partial offload accelerates flow lookup using packet mark.
Add a platform allowing partial offload to restore flow state information.

Signed-off-by: Oz Shlomo <ozsh@mellanox.com>

lib/dpif-netdev.c
@@ -573,8 +573,11 @@ struct dp_netdev_flow {
     struct ovs_refcount ref_cnt;
 
     bool dead;
+
+    /* HW offload state */
     uint32_t mark;               /* Unique flow mark assigned to a flow */
     bool is_hwol;                /* true if flow is fully offloaded */
+    size_t offloaded_actions_len;
 
     /* Statistics. */
     struct dp_netdev_flow_stats stats;
@@ -6735,6 +6738,24 @@ smc_lookup_batch(struct dp_netdev_pmd_thread *pmd,
     pmd_perf_update_counter(&pmd->perf_stats, PMD_STAT_SMC_HIT, n_smc_hit);
 }
 
+static inline struct dp_netdev_flow *
+process_partial_offload(struct dp_netdev_pmd_thread *pmd, odp_port_t port_no,
+                           uint32_t mark, struct dp_packet *packet)
+{
+    struct dp_netdev_port *port = dp_netdev_lookup_port(pmd->dp, port_no);
+    struct dp_netdev_flow *flow = NULL;
+    struct dp_netdev_actions* actions = NULL;
+
+    flow = mark_to_flow_find(pmd, mark);
+    if (flow)
+        actions = dp_netdev_flow_get_actions(flow);
+
+    netdev_flow_restore_state(port->netdev, mark, packet,
+                              (actions? actions->actions: NULL),
+                              (actions? actions->size : 0),
+                              (flow? &flow->offloaded_actions_len : 0));
+    return flow;
+}
 /* Try to process all ('cnt') the 'packets' using only the datapath flow cache
  * 'pmd->flow_cache'. If a flow is not found for a packet 'packets[i]', the
  * miniflow is copied into 'keys' and the packet pointer is moved at the
@@ -6799,7 +6820,7 @@ dfc_processing(struct dp_netdev_pmd_thread *pmd,
 
         if ((*recirc_depth_get() == 0) &&
             dp_packet_has_flow_mark(packet, &mark)) {
-            flow = mark_to_flow_find(pmd, mark);
+            flow = process_partial_offload(pmd, port_no, mark, packet);
             if (OVS_LIKELY(flow)) {
                 tcp_flags = parse_tcp_flags(packet);
                 if (OVS_LIKELY(batch_enable)) {

lib/netdev-provider.h
@@ -866,6 +866,16 @@ struct netdev_class {
                     size_t actions_len, const ovs_u128 *ufid,
                     struct offload_info *info, struct dpif_flow_stats *);
 
+    /* Restore the flow/packet metadata state for continued processing
+     * in software.
+     * 'actions' are in netlink format.
+     * 'offloaded_actions_len' is populated according to the offloaded actions
+     * Return 0 if successful, otherwise returns a positive errno value. */
+    int (*flow_restore_state)(struct netdev *,  uint32_t flow_mark,
+                              struct dp_packet *,
+                              struct nlattr *actions, size_t actions_len,
+                              size_t *offloaded_actions_len);
+
     /* Queries a flow specified by ufid on netdev.
      * Fills output buffer as 'wbuffer' in flow_dump_next, which
      * needs to be be pre allocated.

lib/netdev.c
@@ -2207,6 +2207,21 @@ netdev_flow_put(struct netdev *netdev, struct match *match,
 }
 
 int
+netdev_flow_restore_state(struct netdev *netdev,  uint32_t flow_mark,
+                             struct dp_packet *packet,
+                             struct nlattr *actions, size_t actions_len,
+                             size_t *offloaded_actions_len)
+{
+    const struct netdev_class *class = netdev->netdev_class;
+
+    return (class->flow_restore_state
+            ? class->flow_restore_state(netdev, flow_mark, packet,
+                                        actions, actions_len,
+                                        offloaded_actions_len)
+            : EOPNOTSUPP);
+}
+
+int
 netdev_flow_get(struct netdev *netdev, struct match *match,
                 struct nlattr **actions, const ovs_u128 *ufid,
                 struct dpif_flow_stats *stats,

lib/netdev.h
@@ -222,6 +222,10 @@ bool netdev_flow_dump_next(struct netdev_flow_dump *, struct match *,
 int netdev_flow_put(struct netdev *, struct match *, struct nlattr *actions,
                     size_t actions_len, const ovs_u128 *,
                     struct offload_info *, struct dpif_flow_stats *);
+int netdev_flow_restore_state(struct netdev *netdev,  uint32_t flow_mark,
+                                  struct dp_packet *packet,
+                                  struct nlattr *actions, size_t actions_len,
+                                  size_t *offloaded_actions_len);
 int netdev_flow_get(struct netdev *, struct match *, struct nlattr **actions,
                     const ovs_u128 *, struct dpif_flow_stats *,
                     struct dpif_flow_attrs *, struct ofpbuf *wbuffer);

commit a0d21d1ca2713602f12efc867ec60edebf1eed17

offloads: Initialize flow fields
Initialize the new offloaded_actions_len field

Signed-off-by: Oz Shlomo <ozsh@mellanox.com>
Fixes: 2cb10221f ("offload: add flow restore support")

lib/dpif-netdev.c
@@ -3634,6 +3634,7 @@ dp_netdev_flow_add(struct dp_netdev_pmd_thread *pmd,
     flow->batch = NULL;
     flow->mark = INVALID_FLOW_MARK;
     flow->is_hwol = false;
+    flow->offloaded_actions_len = 0;
     *CONST_CAST(unsigned *, &flow->pmd_id) = pmd->core_id;
     *CONST_CAST(struct flow *, &flow->flow) = match->flow;
     *CONST_CAST(ovs_u128 *, &flow->ufid) = *ufid;