Mellanox/OVS

commit 3a5446701fdc272deaa7f6be5f0c42762cbae95c

offload: add support for set_meta rte_flow action
Signed-off-by: Oz Shlomo <ozsh@mellanox.com>

lib/netdev-dpdk.c
@@ -4596,6 +4596,17 @@ dump_flow_action(struct rte_flow_action *actions, struct ds *s)
         } else {
             ds_put_cstr(s, "  Mark = null\n");
         }
+    } else if (actions->type == RTE_FLOW_ACTION_TYPE_SET_META) {
+            const struct rte_flow_action_set_meta *meta = actions->conf;
+
+            ds_put_cstr(s, "rte flow meta action:\n");
+            if (meta) {
+                ds_put_format(s,
+                              "  meta: data=%d mask=%x\n",
+                              meta->data,meta->mask);
+            } else {
+                ds_put_cstr(s, "  meta = null\n");
+            }
     } else if (actions->type == RTE_FLOW_ACTION_TYPE_RAW_ENCAP) {
         const struct rte_flow_action_raw_encap *raw_encap = actions->conf;
         char encap_str[1000];

lib/netdev-rte-offloads.c
@@ -538,6 +538,7 @@ struct flow_action_items {
     struct rte_flow_action_jump jump;
     struct rte_flow_action_count count;
     struct rte_flow_action_mark mark;
+    struct rte_flow_action_set_meta meta;
     struct rte_flow_action_port_id output;
     struct rte_flow_action_port_id clone_output;
     struct rte_flow_action_count clone_count;
@@ -3730,6 +3731,18 @@ netdev_rte_add_mark_flow_action(struct rte_flow_action_mark *mark,
     mark->id = mark_id;
     add_flow_action(actions, RTE_FLOW_ACTION_TYPE_MARK, mark);
 }
+
+static void
+netdev_rte_add_meta_flow_action(struct rte_flow_action_set_meta *meta,
+                                uint32_t mark_id,
+                                struct flow_actions *actions)
+{
+    memset(meta, 0, sizeof *meta);
+    meta->data = mark_id;
+    meta->mask = RTE_BE32(0xffff);
+    add_flow_action(actions, RTE_FLOW_ACTION_TYPE_SET_META, meta);
+}
+
 
 static int
 netdev_dpdk_offload_ct_actions(struct flow_data *fdata,

lib/dp-packet.h
@@ -571,6 +571,18 @@ dp_packet_has_flow_mark(const struct dp_packet *p, uint32_t *mark)
     return false;
 }
 
+static inline bool
+dp_packet_has_flow_meta(const struct dp_packet *p, uint32_t *meta)
+{
+    if (p->mbuf.ol_flags & (PKT_RX_METADATA | PKT_TX_METADATA)) {
+        *meta = p->mbuf.metadata;
+        return true;
+    }
+
+    return false;
+}
+
+
 static inline void
 dp_packet_set_flow_mark(struct dp_packet *p, uint32_t mark)
 {

commit 316db65221d963d00ae2110066b0deb904045d02

offload: fix meta tag
Fixes: 3a5446701 (offload: add support for set_meta rte_flow action)
Signed-off-by: Oz Shlomo <ozsh@mellanox.com>

lib/netdev-dpdk.c
@@ -4603,7 +4603,7 @@ dump_flow_action(struct rte_flow_action *actions, struct ds *s)
             if (meta) {
                 ds_put_format(s,
                               "  meta: data=%d mask=%x\n",
-                              meta->data,meta->mask);
+                              RTE_BE32(meta->data), RTE_BE32(meta->mask));
             } else {
                 ds_put_cstr(s, "  meta = null\n");
             }

lib/netdev-rte-offloads.c
@@ -3750,7 +3750,7 @@ netdev_rte_add_meta_flow_action(struct rte_flow_action_set_meta *meta,
                                 struct flow_actions *actions)
 {
     memset(meta, 0, sizeof *meta);
-    meta->data = mark_id;
+    meta->data = RTE_BE32(mark_id);
     meta->mask = RTE_BE32(0xffff);
     add_flow_action(actions, RTE_FLOW_ACTION_TYPE_SET_META, meta);
 }