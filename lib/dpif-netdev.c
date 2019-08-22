Mellanox/OVS

commit d9dc1dbd43dd7f06e8d77538de359bc8a661a9c5

dpif-netdev: skip actions that were executed by HW
In partial offload scenarios it is possible that a subset of the action
list was executed by HW. Skip those actions when the flow continues
its processing in SW.

Signed-off-by: Oz Shlomo <ozsh@mellanox.com>

lib/dpif-netdev.c
@@ -6622,16 +6622,27 @@ static inline void
 packet_batch_per_flow_execute(struct packet_batch_per_flow *batch,
                               struct dp_netdev_pmd_thread *pmd)
 {
-    struct dp_netdev_actions *actions;
     struct dp_netdev_flow *flow = batch->flow;
+    struct dp_netdev_actions *updated_actions;
+    struct dp_netdev_actions *actions;
+    size_t updated_actions_size;
 
-    dp_netdev_flow_used(flow, batch->array.count, batch->byte_count,
-                        batch->tcp_flags, pmd->ctx.now / 1000);
+    /* update the stats only if the flow was not fully offloaded */
+    if (!flow->is_hwol)
+        dp_netdev_flow_used(flow, batch->array.count, batch->byte_count,
+                            batch->tcp_flags, pmd->ctx.now / 1000);
 
+    /* skip the actions that were executed by the HW */
     actions = dp_netdev_flow_get_actions(flow);
+    updated_actions = actions;
+    updated_actions_size = actions->size;
+    if (flow->offloaded_actions_len) {
+        updated_actions_size -= flow->offloaded_actions_len;
+        updated_actions += (flow->offloaded_actions_len / sizeof (struct nlattr ));
+    }
 
     dp_netdev_execute_actions(pmd, &batch->array, true, &flow->flow,
-                              actions->actions, actions->size);
+                              updated_actions->actions, updated_actions_size);
 }
 
 static inline void

commit 2daf59b133d6c64f6dc5d560000f2b71998042fe

dpif-netdev: Fix reading missed flows stats
In current implementation the SW counters are read (dp_netdev_flow_use)
only for non-offloaded flows as the pseudo code in [1].
The logic behind it was that for offloaded flows, we should run in [1]
only under HW misses cases. The expectation was that in miss cases the
HW counters are already updated and will be read. The current
implementation avoided reading the same counters in SW again.  However,
the NIC actually returns 0 when jumping to a table with no matching
rule.  Therefore instead of relying on the HW counters we should still
read the SW counters.  This commit removes the if-statement in [1] and
reads the SW counters unconditionally.
The commit is also a must for the following scenario. Assuming two flows:

Flow 1: match(recirc=0, ...) action: ct, set_recirc(4)
Flow 2: match(recirc=4, ...) action: output(<port>)

If a HW miss occurred in Flow 1 the packet will continue to be handled in
SW. In this example it will recirculate and will match Flow 2.
Even if Flow 2 was offloaded the packet now is on the SW path and
Flow 2 counters must be read in SW.

[1]
if (flow is not offloaded)
    read_statistcis();

Fixes: d9dc1dbd43dd ("dpif-netdev: skip actions that were executed by HW")
Signed-off-by: Ophir Munk <ophirmu@mellanox.com>

lib/dpif-netdev.c
@@ -6693,9 +6693,8 @@ packet_batch_per_flow_execute(struct packet_batch_per_flow *batch,
     size_t updated_actions_size;
 
     /* update the stats only if the flow was not fully offloaded */
-    if (!flow->is_hwol)
-        dp_netdev_flow_used(flow, batch->array.count, batch->byte_count,
-                            batch->tcp_flags, pmd->ctx.now / 1000);
+    dp_netdev_flow_used(flow, batch->array.count, batch->byte_count,
+                        batch->tcp_flags, pmd->ctx.now / 1000);
 
     /* skip the actions that were executed by the HW */
     actions = dp_netdev_flow_get_actions(flow);