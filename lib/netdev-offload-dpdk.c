e4c8381c44c3b3a8030fa09b57c09a58815c98e9
Eli Britstein <elibr@mellanox.com>
2019-11-21 21:12:52 +0000

    netdev-offload-dpdk: Support tunnel pop action
    Support tunnel pop action with marking the packet with a flow miss
    context ID, and jump to the vport table.
    
    Issue: 1975006
    Change-Id: I7e2acba16a0b18721c7602837ba89ece4e4b4528
    Co-authored-by: Eli Britstein <elibr@mellanox.com>
    Signed-off-by: Ophir Munk <ophirmu@mellanox.com>
    Signed-off-by: Eli Britstein <elibr@mellanox.com>

--- lib/netdev-offload-dpdk.c
+++ lib/netdev-offload-dpdk.c
@@ -57,6 +57,11 @@ static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(600, 600);
  */
 static struct cmap ufid_to_rte_flow = CMAP_INITIALIZER;
 
+struct act_resources {
+    uint32_t next_table_id;
+    uint32_t flow_miss_ctx_id;
+};
+
 struct ufid_to_rte_flow_data {
     struct cmap_node node;
     ovs_u128 ufid;
@@ -64,6 +69,7 @@ struct ufid_to_rte_flow_data {
     struct rte_flow *rte_flow;
     bool actions_offloaded;
     struct dpif_flow_stats stats;
+    struct act_resources act_resources;
 };
 
 /* Find rte_flow with @ufid. */
@@ -89,7 +95,8 @@ ufid_to_rte_flow_data_find(const ovs_u128 *ufid, bool warn)
 
 static inline struct ufid_to_rte_flow_data *
 ufid_to_rte_flow_associate(const ovs_u128 *ufid, struct netdev *netdev,
-                           struct rte_flow *rte_flow, bool actions_offloaded)
+                           struct rte_flow *rte_flow, bool actions_offloaded,
+                           struct act_resources *act_resources)
 {
     size_t hash = hash_bytes(ufid, sizeof *ufid, 0);
     struct ufid_to_rte_flow_data *data = xzalloc(sizeof *data);
@@ -110,6 +117,7 @@ ufid_to_rte_flow_associate(const ovs_u128 *ufid, struct netdev *netdev,
     data->netdev = netdev_ref(netdev);
     data->rte_flow = rte_flow;
     data->actions_offloaded = actions_offloaded;
+    memcpy(&data->act_resources, act_resources, sizeof data->act_resources);
 
     cmap_insert(&ufid_to_rte_flow,
                 CONST_CAST(struct cmap_node *, &data->node), hash);
@@ -317,7 +325,6 @@ static struct context_metadata table_id_md = {
     .data_size = sizeof(struct table_id_data),
 };
 
-OVS_UNUSED
 static int
 get_table_id(odp_port_t vport, uint32_t *table_id)
 {
@@ -335,7 +342,6 @@ get_table_id(odp_port_t vport, uint32_t *table_id)
                                        table_id);
 }
 
-OVS_UNUSED
 static void
 put_table_id(uint32_t table_id)
 {
@@ -365,7 +371,6 @@ static struct context_metadata flow_miss_ctx_md = {
     .data_size = sizeof(struct flow_miss_ctx),
 };
 
-OVS_UNUSED
 static int
 get_flow_miss_ctx_id(struct flow_miss_ctx *flow_ctx_data,
                      uint32_t *miss_ctx_id)
@@ -378,7 +383,6 @@ get_flow_miss_ctx_id(struct flow_miss_ctx *flow_ctx_data,
                                        miss_ctx_id);
 }
 
-OVS_UNUSED
 static void
 put_flow_miss_ctx_id(uint32_t flow_ctx_id)
 {
@@ -389,6 +393,13 @@ static int
 find_flow_miss_ctx(int flow_ctx_id, struct flow_miss_ctx *ctx)
 {
     return get_context_data_by_id(&flow_miss_ctx_md, flow_ctx_id, ctx);
+}
+
+static void
+put_action_resources(struct act_resources *act_resources)
+{
+    put_table_id(act_resources->next_table_id);
+    put_flow_miss_ctx_id(act_resources->flow_miss_ctx_id);
 }
 
 /*
@@ -794,6 +805,14 @@ dump_flow_action(struct ds *s, struct ds *s_extra,
         ds_put_cstr(s, "vxlan_encap / ");
         dump_vxlan_encap(s_extra, items);
         ds_put_cstr(s_extra, ";");
+    } else if (actions->type == RTE_FLOW_ACTION_TYPE_JUMP) {
+        const struct rte_flow_action_jump *jump = actions->conf;
+
+        ds_put_cstr(s, "jump ");
+        if (jump) {
+            ds_put_format(s, "group %"PRIu32" ", jump->group);
+        }
+        ds_put_cstr(s, "/ ");
     } else {
         ds_put_format(s, "unknown rte flow action (%d)\n", actions->type);
     }
@@ -1573,11 +1592,52 @@ parse_clone_actions(struct netdev *netdev,
     return 0;
 }
 
+static void
+add_mark_action(struct flow_actions *actions,
+                uint32_t mark_id)
+{
+    struct rte_flow_action_mark *mark = xzalloc(sizeof *mark);
+
+    mark->id = mark_id;
+    add_flow_action(actions, RTE_FLOW_ACTION_TYPE_MARK, mark);
+}
+
+static void
+add_jump_action(struct flow_actions *actions, uint32_t group)
+{
+    struct rte_flow_action_jump *jump = xzalloc (sizeof *jump);
+
+    jump->group = group;
+    add_flow_action(actions, RTE_FLOW_ACTION_TYPE_JUMP, jump);
+}
+
+static int
+add_tnl_pop_action(struct flow_actions *actions,
+                   const struct nlattr *nla,
+                   struct act_resources *act_resources)
+{
+    struct flow_miss_ctx miss_ctx;
+    odp_port_t port;
+
+    port = nl_attr_get_odp_port(nla);
+    miss_ctx.vport = port;
+    if (get_flow_miss_ctx_id(&miss_ctx, &act_resources->flow_miss_ctx_id)) {
+        return -1;
+    }
+    add_mark_action(actions, act_resources->flow_miss_ctx_id);
+    if (get_table_id(port, &act_resources->next_table_id)) {
+        return -1;
+    }
+    add_jump_action(actions, act_resources->next_table_id);
+    return 0;
+}
+
 static int
 parse_flow_actions(struct netdev *netdev,
                    struct flow_actions *actions,
                    struct nlattr *nl_actions,
-                   size_t nl_actions_len)
+                   size_t nl_actions_len,
+                   struct act_resources *act_resources)
 {
     struct nlattr *nla;
     size_t left;
@@ -1617,6 +1677,10 @@ parse_flow_actions(struct netdev *netdev,
                                     clone_actions_len)) {
                 return -1;
             }
+        } else if (nl_attr_type(nla) == OVS_ACTION_ATTR_TUNNEL_POP) {
+            if (add_tnl_pop_action(actions, nla, act_resources)) {
+                return -1;
+            }
         } else {
             VLOG_DBG_RL(&rl, "Unsupported action type %d", nl_attr_type(nla));
             return -1;
@@ -1636,7 +1700,8 @@ static struct rte_flow *
 netdev_offload_dpdk_actions(struct netdev *netdev,
                             struct flow_patterns *patterns,
                             struct nlattr *nl_actions,
-                            size_t actions_len)
+                            size_t actions_len,
+                            struct act_resources *act_resources)
 {
     const struct rte_flow_attr flow_attr = { .ingress = 1, .transfer = 1 };
     struct flow_actions actions = { .actions = NULL, .cnt = 0 };
@@ -1644,7 +1709,8 @@ netdev_offload_dpdk_actions(struct netdev *netdev,
     struct rte_flow_error error;
     int ret;
 
-    ret = parse_flow_actions(netdev, &actions, nl_actions, actions_len);
+    ret = parse_flow_actions(netdev, &actions, nl_actions, actions_len,
+                             act_resources);
     if (ret) {
         goto out;
     }
@@ -1665,17 +1731,22 @@ netdev_offload_dpdk_add_flow(struct netdev *netdev,
 {
     struct flow_patterns patterns = { .items = NULL, .cnt = 0 };
     struct ufid_to_rte_flow_data *flows_data = NULL;
+    struct act_resources act_resources;
     bool actions_offloaded = true;
     struct rte_flow *flow;
+    int ret;
 
-    if (parse_flow_match(&patterns, match)) {
+    memset(&act_resources, 0, sizeof act_resources);
+
+    ret = parse_flow_match(&patterns, match);
+    if (ret) {
         VLOG_DBG_RL(&rl, "%s: matches of ufid "UUID_FMT" are not supported",
                     netdev_get_name(netdev), UUID_ARGS((struct uuid *) ufid));
         goto out;
     }
 
     flow = netdev_offload_dpdk_actions(netdev, &patterns, nl_actions,
-                                       actions_len);
+                                       actions_len, &act_resources);
     if (!flow) {
         /* If we failed to offload the rule actions fallback to MARK+RSS
          * actions.
@@ -1689,11 +1760,14 @@ netdev_offload_dpdk_add_flow(struct netdev *netdev,
         goto out;
     }
     flows_data = ufid_to_rte_flow_associate(ufid, netdev, flow,
-                                            actions_offloaded);
+                                            actions_offloaded, &act_resources);
     VLOG_DBG("%s: installed flow %p by ufid "UUID_FMT,
              netdev_get_name(netdev), flow, UUID_ARGS((struct uuid *)ufid));
 
 out:
+    if (ret) {
+        put_action_resources(&act_resources);
+    }
     free_flow_patterns(&patterns);
     return flows_data;
 }
@@ -1714,6 +1788,7 @@ netdev_offload_dpdk_flow_destroy(struct ufid_to_rte_flow_data *rte_flow_data)
     ret = netdev_dpdk_rte_flow_destroy(netdev, rte_flow, &error);
 
     if (ret == 0) {
+        put_action_resources(&rte_flow_data->act_resources);
         ufid_to_rte_flow_disassociate(rte_flow_data);
         VLOG_DBG_RL(&rl, "%s: rte_flow 0x%"PRIxPTR
                     " flow destroy %d ufid " UUID_FMT,
