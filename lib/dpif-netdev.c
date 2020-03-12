11625fcf65d0980f1f3521b6237dadef3e791b9e
Eli Britstein <elibr@mellanox.com>
2020-03-12 13:32:13 +0000

    dpif-netdev: Fix flow modification after failure
    dp_netdev_flow_offload_main thread is asynchronous, by the cited commit.
    There might be a case where there are modification requests of the same
    flow submitted before handled. Then, if the first handling fails, the
    rule for the flow is deleted, and the mark is freed. Then, the following
    one should not be handled as a modification, but rather an "add" event.
    
    Issue: 2105293
    Change-Id: Iad97f3c9fb5d476feec9a2513274534e8b9cd30d
    Fixes: 02bb2824e51d ("dpif-netdev: do hw flow offload in a thread")
    Signed-off-by: Eli Britstein <elibr@mellanox.com>

--- lib/dpif-netdev.c
+++ lib/dpif-netdev.c
@@ -2506,7 +2506,8 @@ dp_netdev_flow_offload_put(struct dp_offload_item *offload_item)
     struct dp_netdev_flow *flow = offload->flow;
     odp_port_t in_port = flow->flow.in_port.odp_port;
     const char *dpif_type_str = offload_item->dpif_type_str;
-    bool modification = offload->op == DP_NETDEV_FLOW_OFFLOAD_OP_MOD;
+    bool modification = offload->op == DP_NETDEV_FLOW_OFFLOAD_OP_MOD
+                        && flow->mark != INVALID_FLOW_MARK;
     struct offload_info info;
     struct netdev *port;
     uint32_t mark;
@@ -2518,7 +2519,6 @@ dp_netdev_flow_offload_put(struct dp_offload_item *offload_item)
 
     if (modification) {
         mark = flow->mark;
-        ovs_assert(mark != INVALID_FLOW_MARK);
     } else {
         /*
          * If a mega flow has already been offloaded (from other PMD
