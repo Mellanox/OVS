Mellanox/OVS

commit da8fd85f47e98660d4c095df979aae1997b37280

conntrack: Bidirectional CT offloading
Instead of offloading each CT direction separately, wait for both
directions before offloading. This is done to enable counting the
offloaded connections.

Issue: 2182760
Change-Id: I4d704f8d461627edc6eb97d9fe2b9dc5671b2ca1
Signed-off-by: Eli Britstein <elibr@mellanox.com>

lib/dpif-netdev.c
@@ -438,7 +438,7 @@ struct dp_offload_item {
     const char *dpif_type_str;
     union {
         struct dp_flow_offload_item flow_offload_item;
-        struct ct_flow_offload_item ct_offload_item;
+        struct ct_flow_offload_item ct_offload_item[CT_DIR_NUM];
     };
 };
 
@@ -467,7 +467,8 @@ dp_netdev_append_ct_offload(struct ct_flow_offload_item *offload)
 
     offload_item = xzalloc(sizeof *offload_item);
     offload_item->type = DP_CT_OFFLOAD_ITEM;
-    offload_item->ct_offload_item = *offload;
+    memcpy(offload_item->ct_offload_item, offload,
+           sizeof offload_item->ct_offload_item);
     offload_item->port_mutex = offload->mutex;
     offload_item->dpif_type_str = offload->class_type;
 
@@ -525,10 +526,14 @@ static void
 dp_netdev_ct_offload_add_item(struct ct_flow_offload_item *offload)
 {
     struct match match;
+    int dir;
 
-    offload->op = DP_NETDEV_FLOW_OFFLOAD_OP_ADD;
-    dp_netdev_fill_ct_match(&match, offload);
-    dp_netdev_get_mega_ufid((const struct match *)&match, &offload->ufid);
+    for (dir = 0; dir < CT_DIR_NUM; dir++) {
+        offload[dir].op = DP_NETDEV_FLOW_OFFLOAD_OP_ADD;
+        dp_netdev_fill_ct_match(&match, &offload[dir]);
+        dp_netdev_get_mega_ufid((const struct match *)&match,
+                                &offload[dir].ufid);
+    }
 
     dp_netdev_append_ct_offload(offload);
 }
@@ -536,7 +541,8 @@ dp_netdev_ct_offload_add_item(struct ct_flow_offload_item *offload)
 static void
 dp_netdev_ct_offload_del_item(struct ct_flow_offload_item *offload)
 {
-    offload->op = DP_NETDEV_FLOW_OFFLOAD_OP_DEL;
+    offload[CT_DIR_INIT].op = DP_NETDEV_FLOW_OFFLOAD_OP_DEL;
+    offload[CT_DIR_REP].op = DP_NETDEV_FLOW_OFFLOAD_OP_DEL;
     dp_netdev_append_ct_offload(offload);
 }
 
@@ -2684,9 +2690,9 @@ dp_netdev_create_ct_actions(struct ofpbuf *buf,
 }
 
 static int
-dp_netdev_ct_offload_add(struct dp_offload_item *offload_item)
+dp_netdev_ct_offload_add(struct dp_offload_item *offload_item, int dir)
 {
-    struct ct_flow_offload_item offload = offload_item->ct_offload_item;
+    struct ct_flow_offload_item offload = offload_item->ct_offload_item[dir];
     struct offload_info info = { .flow_mark = INVALID_FLOW_MARK, };
     struct nlattr *actions;
     struct netdev *port;
@@ -2756,9 +2762,9 @@ out:
 }
 
 static int
-dp_netdev_ct_offload_del(struct dp_offload_item *offload_item)
+dp_netdev_ct_offload_del(struct dp_offload_item *offload_item, int dir)
 {
-    struct ct_flow_offload_item offload = offload_item->ct_offload_item;
+    struct ct_flow_offload_item offload = offload_item->ct_offload_item[dir];
     struct netdev *port;
     int ret = 0;
 
@@ -2779,9 +2785,7 @@ static void *
 dp_netdev_flow_offload_main(void *data OVS_UNUSED)
 {
     struct dp_offload_item *offload_item;
-    const ovs_u128 *ufid = NULL;
     struct ovs_list *list;
-    const char *flow_type;
     const char *op;
     int ret;
 
@@ -2801,8 +2805,6 @@ dp_netdev_flow_offload_main(void *data OVS_UNUSED)
             struct dp_flow_offload_item *dp_offload =
                     &offload_item->flow_offload_item;
 
-            flow_type = "netdev";
-            ufid = &dp_offload->flow->mega_ufid;
             switch (dp_offload->op) {
             case DP_NETDEV_FLOW_OFFLOAD_OP_ADD:
                 op = "add";
@@ -2820,31 +2822,35 @@ dp_netdev_flow_offload_main(void *data OVS_UNUSED)
                 OVS_NOT_REACHED();
             }
             dp_netdev_free_flow_offload(dp_offload);
+            VLOG_DBG("%s to %s netdev flow "UUID_FMT"\n",
+                     ret == 0 ? "succeed" : "failed", op,
+                     UUID_ARGS((struct uuid *)&dp_offload->flow->mega_ufid));
         } else if (offload_item->type == DP_CT_OFFLOAD_ITEM) {
             struct ct_flow_offload_item *ct_offload =
-                    &offload_item->ct_offload_item;
+                    offload_item->ct_offload_item;
+            int dir;
 
-            flow_type = "ct";
-            ufid = &ct_offload->ufid;
-            switch (ct_offload->op) {
-            case DP_NETDEV_FLOW_OFFLOAD_OP_ADD:
-                op = "add";
-                ret = dp_netdev_ct_offload_add(offload_item);
-                break;
-            case DP_NETDEV_FLOW_OFFLOAD_OP_DEL:
-                op = "delete";
-                ret = dp_netdev_ct_offload_del(offload_item);
-                break;
-            default:
-                OVS_NOT_REACHED();
+            for (dir = 0; dir < CT_DIR_NUM; dir++) {
+                switch (ct_offload[dir].op) {
+                case DP_NETDEV_FLOW_OFFLOAD_OP_ADD:
+                    op = "add";
+                    ret = dp_netdev_ct_offload_add(offload_item, dir);
+                    break;
+                case DP_NETDEV_FLOW_OFFLOAD_OP_DEL:
+                    op = "delete";
+                    ret = dp_netdev_ct_offload_del(offload_item, dir);
+                    break;
+                default:
+                    OVS_NOT_REACHED();
+                }
+                VLOG_DBG("%s to %s ct(%s) flow "UUID_FMT"\n",
+                         ret == 0 ? "succeed" : "failed", op,
+                         dir == CT_DIR_INIT ? "init" : "reply",
+                         UUID_ARGS((struct uuid *)&ct_offload[dir].ufid));
             }
         } else {
             OVS_NOT_REACHED();
         }
-
-        VLOG_DBG("%s to %s %s flow "UUID_FMT"\n",
-                 ret == 0 ? "succeed" : "failed", op, flow_type,
-                 UUID_ARGS((struct uuid *)ufid));
         free(offload_item);
         ovsrcu_quiesce();
     }

lib/conntrack.c
@@ -311,7 +311,8 @@ static void
 conntrack_offload_del_conn(struct conntrack *ct,
                            struct conn *conn)
 {
-    struct ct_flow_offload_item item;
+    struct ct_flow_offload_item item[CT_DIR_NUM];
+    struct conn *conn_dir;
     int dir;
 
     if (!ct->offload_class || !ct->offload_class->conn_del) {
@@ -319,11 +320,17 @@ conntrack_offload_del_conn(struct conntrack *ct,
     }
 
     for (dir = 0; dir < CT_DIR_NUM; dir ++) {
-        if (!conntrack_offload_fill_item_common(&item, conn, dir)) {
+        if (conn->nat_conn &&
+            conn->nat_conn->offloads.dir_info[dir].class_type) {
+            conn_dir = conn->nat_conn;
+        } else {
+            conn_dir = conn;
+        }
+        if (!conntrack_offload_fill_item_common(&item[dir], conn_dir, dir)) {
             continue;
         }
-        ct->offload_class->conn_del(&item);
     }
+    ct->offload_class->conn_del(item);
 }
 
 /* Initializes the connection tracker 'ct'.  The caller is responsible for
@@ -500,9 +507,6 @@ conn_clean(struct conntrack *ct, struct conn *conn)
 
     if (netdev_is_flow_api_enabled()) {
         conntrack_offload_del_conn(ct, conn);
-        if (conn->nat_conn) {
-            conntrack_offload_del_conn(ct, conn->nat_conn);
-        }
     }
 
     conn_clean_cmn(ct, conn);
@@ -1452,13 +1456,14 @@ conntrack_swap_conn_key(const struct conn_key *key,
 static void
 conntrack_offload_fill_item_add(struct ct_flow_offload_item *item,
                                 struct conn *conn,
-                                struct dp_packet *pkt,
+                                int dir,
                                 uint32_t mark,
                                 ovs_u128 label)
 {
-    int dir = ct_get_packet_dir(pkt->md.reply);
+    /* nat_conn has opposite directions. */
+    bool reply = !!conn->master_conn ^ dir;
 
-    if (pkt->md.reply) {
+    if (reply) {
         item->key = conn->rev_key;
         conntrack_swap_conn_key(&conn->key, &item->nat.key);
     } else {
@@ -1494,21 +1499,18 @@ conntrack_offload_fill_item_add(struct ct_flow_offload_item *item,
 }
 
 static void
-conntrack_offload_prepare_add(struct ct_flow_offload_item *item,
-                              struct conn *conn,
+conntrack_offload_prepare_add(struct conn *conn,
                               struct dp_packet *packet,
-                              uint32_t mark,
-                              ovs_u128 label,
                               struct ovs_mutex *port_mutex,
                               const char *class_type)
 {
-    int dir = ct_get_packet_dir(packet->md.reply);
+    /* nat_conn has opposite directions. */
+    bool reply = !!conn->master_conn ^ packet->md.reply;
+    int dir = ct_get_packet_dir(reply);
 
     conn->offloads.dir_info[dir].port = packet->md.in_port.odp_port;
     conn->offloads.dir_info[dir].port_mutex = port_mutex;
     conn->offloads.dir_info[dir].class_type = class_type;
-    conntrack_offload_fill_item_add(item, conn, packet, mark, label);
-
     conn->offloads.dir_info[dir].dont_free = true;
     conn->offloads.dir_info[dir].pkt_ct_state = packet->md.ct_state;
 }
@@ -1522,8 +1524,11 @@ conntrack_offload_add_conn(struct conntrack *ct,
                            uint32_t mark,
                            ovs_u128 label)
 {
-    int dir = ct_get_packet_dir(packet->md.reply);
-    struct ct_flow_offload_item item;
+    /* nat_conn has opposite directions. */
+    bool reply = !!conn->master_conn ^ packet->md.reply;
+    struct ct_flow_offload_item item[CT_DIR_NUM];
+    uint8_t flags;
+    int dir;
 
     /* CT doesn't handle alg */
     if (conn->alg || conn->alg_related || !ct->offload_class ||
@@ -1532,19 +1537,46 @@ conntrack_offload_add_conn(struct conntrack *ct,
         return;
     }
 
-    if ((packet->md.reply && !(conn->offloads.flags & CT_OFFLOAD_REP)) ||
-        (!packet->md.reply && !(conn->offloads.flags & CT_OFFLOAD_INIT))) {
-        conntrack_offload_prepare_add(&item, conn, packet, mark, label,
-                                      port_mutex, class_type);
-        ct->offload_class->conn_add(&item);
-        conn->offloads.dir_info[dir].ufid = item.ufid;
-        conn->offloads.flags |= packet->md.reply ? CT_OFFLOAD_REP :
-                                                   CT_OFFLOAD_INIT;
-
+    if ((reply && !(conn->offloads.flags & CT_OFFLOAD_REP)) ||
+        (!reply && !(conn->offloads.flags & CT_OFFLOAD_INIT))) {
+        conntrack_offload_prepare_add(conn, packet, port_mutex, class_type);
+        conn->offloads.flags |= reply ? CT_OFFLOAD_REP : CT_OFFLOAD_INIT;
+        if (conn->master_conn) {
+            conn->master_conn->offloads.flags |= reply
+                ? CT_OFFLOAD_REP : CT_OFFLOAD_INIT;
+        }
     }
 
-    if ((conn->offloads.flags & CT_OFFLOAD_BOTH) == CT_OFFLOAD_BOTH) {
+    flags = conn->offloads.flags;
+    if (conn->nat_conn) {
+        flags |= conn->nat_conn->offloads.flags;
+    } else if (conn->master_conn) {
+        flags |= conn->master_conn->offloads.flags;
+    }
+    if ((flags & CT_OFFLOAD_BOTH) == CT_OFFLOAD_BOTH) {
+        ovs_u128 *ufid[CT_DIR_NUM];
+
+        for (dir = 0; dir < CT_DIR_NUM; dir ++) {
+            if (conn->nat_conn &&
+                conn->nat_conn->offloads.dir_info[dir].class_type) {
+                conn = conn->nat_conn;
+            } else if (conn->master_conn &&
+                       conn->master_conn->offloads.dir_info[dir].class_type) {
+                conn = conn->master_conn;
+            }
+            conntrack_offload_fill_item_add(&item[dir], conn, dir, mark,
+                                            label);
+            ufid[dir] = &conn->offloads.dir_info[dir].ufid;
+        }
+        ct->offload_class->conn_add(item);
+        *ufid[CT_DIR_INIT] = item[CT_DIR_INIT].ufid;
+        *ufid[CT_DIR_REP] = item[CT_DIR_REP].ufid;
         conn->offloads.flags |= CT_OFFLOAD_SKIP;
+        if (conn->nat_conn) {
+            conn->nat_conn->offloads.flags |= CT_OFFLOAD_SKIP;
+        } else if (conn->master_conn) {
+            conn->master_conn->offloads.flags |= CT_OFFLOAD_SKIP;
+        }
     }
 }
 
@@ -2695,12 +2727,11 @@ conn_to_ct_dpif_entry(const struct conn *conn, struct ct_dpif_entry *entry,
     }
     entry->offload_status_orig = conn->offloads.dir_info[CT_DIR_INIT].status;
     entry->offload_status_reply = conn->offloads.dir_info[CT_DIR_REP].status;
-    /* nat_conn has opposite directions. */
     if (conn->nat_conn) {
         entry->offload_status_orig |=
-            conn->nat_conn->offloads.dir_info[CT_DIR_REP].status;
-        entry->offload_status_reply |=
             conn->nat_conn->offloads.dir_info[CT_DIR_INIT].status;
+        entry->offload_status_reply |=
+            conn->nat_conn->offloads.dir_info[CT_DIR_REP].status;
     }
     ovs_mutex_unlock(&conn->lock);
 

commit 99022a575b067f6f64828bf629b788d337689983

conntrack: Fix CT removal/query if not offloaded
In case the connection is not offloaded do not attempt
to query or destroy its HW rules.

Issue: 2232482
Fixes: da8fd85f47e9 ("conntrack: Bidirectional CT offloading")
Change-Id: I8d96fae5b5720708a0e174c3e5388e2c2f782447
Signed-off-by: Salem Sol <salems@mellanox.com>

lib/dpif-netdev.c
@@ -556,6 +556,10 @@ dp_netdev_ct_offload_active(struct ct_flow_offload_item *offload,
     struct dpif_flow_attrs attrs;
     struct dp_netdev *dp = offload->dp;
 
+    /* if a direction is not offloaded do not attempt to query it from HW */
+    if (!dp) {
+        return false;
+    }
     if (!dpif_netdev_get_flow_offload_status(dp->class->type,
                                              &dp->port_mutex, offload->odp_port,
                                              (const ovs_u128 *) &offload->ufid,
@@ -2775,6 +2779,10 @@ dp_netdev_ct_offload_del(struct dp_offload_item *offload_item, int dir)
     struct netdev *port;
     int ret = 0;
 
+    /* if a direction is not offloaded do not attempt to delete it from HW */
+    if (!offload.dp) {
+        return -1;
+    }
     port = netdev_ports_get(offload.odp_port,
                             dpif_normalize_type(offload_item->dp->class->type));
     if (!port) {