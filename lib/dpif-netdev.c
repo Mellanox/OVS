Mellanox/OVS

commit d478217b401e9c9638773bc9b8173955a13a5f38

dpif-netdev: Count offloaded connections
Count offloaded connections as a pre-step towards dumping them.

Issue: 2190470
Change-Id: Ic98098609b2ea78e03451a34eeea5be20b326aa7
Signed-off-by: ameerm <ameerm@mellanox.com>

lib/dpif-netdev.c
@@ -325,6 +325,9 @@ struct dp_netdev {
     struct ovs_refcount ref_cnt;
     atomic_flag destroyed;
 
+    /* Counting offloaded statistics. */
+    struct dpif_offload_stats offload_stats;
+
     /* Ports.
      *
      * Any lookup into 'ports' or any access to the dp_netdev_ports found
@@ -1681,6 +1684,7 @@ create_dp_netdev(const char *name, const struct dpif_class *class,
     *CONST_CAST(const char **, &dp->name) = xstrdup(name);
     ovs_refcount_init(&dp->ref_cnt);
     atomic_flag_clear(&dp->destroyed);
+    memset(&dp->offload_stats, 0, sizeof dp->offload_stats);
 
     ovs_mutex_init_recursive(&dp->port_mutex);
     hmap_init(&dp->ports);
@@ -2831,6 +2835,8 @@ dp_netdev_flow_offload_main(void *data OVS_UNUSED)
         } else if (offload_item->type == DP_CT_OFFLOAD_ITEM) {
             struct ct_flow_offload_item *ct_offload =
                     offload_item->ct_offload_item;
+            struct dp_netdev *dp = ct_offload->dp;
+            bool conn_deleted = false;
             int dir;
 
             for (dir = 0; dir < CT_DIR_NUM; dir++) {
@@ -2842,6 +2848,7 @@ dp_netdev_flow_offload_main(void *data OVS_UNUSED)
                 case DP_NETDEV_FLOW_OFFLOAD_OP_DEL:
                     op = "delete";
                     ret = dp_netdev_ct_offload_del(offload_item, dir);
+                    conn_deleted = conn_deleted || !ret;
                     break;
                 default:
                     OVS_NOT_REACHED();
@@ -2850,6 +2857,20 @@ dp_netdev_flow_offload_main(void *data OVS_UNUSED)
                          ret == 0 ? "succeed" : "failed", op,
                          dir == CT_DIR_INIT ? "init" : "reply",
                          UUID_ARGS((struct uuid *)&ct_offload[dir].ufid));
+            }
+            switch (ct_offload[CT_DIR_INIT].op) {
+            case DP_NETDEV_FLOW_OFFLOAD_OP_ADD:
+                 if (*ct_offload[CT_DIR_INIT].status &&
+                     *ct_offload[CT_DIR_REP].status) {
+                     dp->offload_stats.ct_connections++;
+                 }
+                 break;
+            case DP_NETDEV_FLOW_OFFLOAD_OP_DEL:
+                 if (conn_deleted && *ct_offload[CT_DIR_INIT].status &&
+                     *ct_offload[CT_DIR_REP].status) {
+                     dp->offload_stats.ct_connections--;
+                 }
+                 break;
             }
         } else {
             OVS_NOT_REACHED();
@@ -4124,6 +4145,15 @@ dpif_netdev_flow_dump_next(struct dpif_flow_dump_thread *thread_,
     }
 
     return n_flows;
+}
+
+static int
+dpif_netdev_get_offload_stats(struct dpif *dpif, struct dpif_offload_stats *offload_stats)
+{
+    struct dp_netdev *dp = get_dp_netdev(dpif);
+    offload_stats->ct_connections = dp->offload_stats.ct_connections;
+
+    return 0;
 }
 
 static int
@@ -8139,7 +8169,7 @@ const struct dpif_class dpif_netdev_class = {
     dpif_netdev_flow_dump_thread_create,
     dpif_netdev_flow_dump_thread_destroy,
     dpif_netdev_flow_dump_next,
-    NULL,                       /* get_offload_stats */
+    dpif_netdev_get_offload_stats,
     dpif_netdev_operate,
     NULL,                       /* recv_set */
     NULL,                       /* handlers_set */

lib/conntrack.c
@@ -300,6 +300,7 @@ conntrack_offload_fill_item_common(struct ct_flow_offload_item *item,
     item->ufid = conn->offloads.dir_info[dir].ufid;
     item->odp_port = conn->offloads.dir_info[dir].port;
     item->dp = conn->offloads.dir_info[dir].dp;
+    item->status = &conn->offloads.dir_info[dir].status;
 
     return dir == CT_DIR_REP
            ? !!(conn->offloads.flags & CT_OFFLOAD_REP)

lib/dpif.h
@@ -489,7 +489,9 @@ int dpif_port_dump_done(struct dpif_port_dump *);
 int dpif_port_poll(const struct dpif *, char **devnamep);
 void dpif_port_poll_wait(const struct dpif *);
 
-struct dpif_offload_stats;
+struct dpif_offload_stats {
+    uint32_t ct_connections;
+};
 
 /* Flow table operations. */
 

commit 255db1ea4507368430cc3ac751509a77a0a6cf7f

dpif-netdev: Fix CT removal crash
CT status pointer must not be accessed during CT deletion, as it points
to 'struct conn' that might have already been freed by the time the
deletion message is processed.

Issue: 2232395
Change-Id: I0b76591a04c37509ea8548084e90ced61b89f8d7
Fixes: d478217b401e ("dpif-netdev: Count offloaded connections")
Signed-off-by: Eli Britstein <elibr@mellanox.com>

lib/dpif-netdev.c
@@ -2877,7 +2877,6 @@ dp_netdev_flow_offload_main(void *data OVS_UNUSED)
             struct ct_flow_offload_item *ct_offload =
                     offload_item->ct_offload_item;
             struct dp_netdev *dp = ct_offload->dp;
-            bool conn_deleted = false;
             int rets[CT_DIR_NUM];
             int dir;
 
@@ -2893,7 +2892,6 @@ dp_netdev_flow_offload_main(void *data OVS_UNUSED)
                 case DP_NETDEV_FLOW_OFFLOAD_OP_DEL:
                     op = "delete";
                     ret = dp_netdev_ct_offload_del(offload_item, dir);
-                    conn_deleted = conn_deleted || !ret;
                     break;
                 default:
                     OVS_NOT_REACHED();
@@ -2904,19 +2902,13 @@ dp_netdev_flow_offload_main(void *data OVS_UNUSED)
                          dir == CT_DIR_INIT ? "init" : "reply",
                          UUID_ARGS((struct uuid *)&ct_offload[dir].ufid));
             }
-            switch (ct_offload[CT_DIR_INIT].op) {
-            case DP_NETDEV_FLOW_OFFLOAD_OP_ADD:
-                 if (*ct_offload[CT_DIR_INIT].status &&
-                     *ct_offload[CT_DIR_REP].status) {
-                     dp->offload_stats.ct_connections++;
-                 }
-                 break;
-            case DP_NETDEV_FLOW_OFFLOAD_OP_DEL:
-                 if (conn_deleted && *ct_offload[CT_DIR_INIT].status &&
-                     *ct_offload[CT_DIR_REP].status) {
-                     dp->offload_stats.ct_connections--;
-                 }
-                 break;
+            if (!rets[CT_DIR_INIT] && !rets[CT_DIR_REP]) {
+                if (ct_offload[CT_DIR_INIT].op ==
+                    DP_NETDEV_FLOW_OFFLOAD_OP_ADD) {
+                    dp->offload_stats.ct_connections++;
+                } else {
+                    dp->offload_stats.ct_connections--;
+                }
             }
             if ((dp->offload_stats.ct_connections &
                  CT_CONN_TS_INTERVAL_MASK) == 0) {