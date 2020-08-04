Mellanox/OVS

commit c9324767f19326c23534d807aecb8dce7c2d6955

conntrack: e2e-cache tracer: collect all new CT IDs
Added CT IDs collection in conntrack_offload_add_con.
After a new connection is offloaded connection ID is stored
in e2e-cache trace array inside dp_packet.

Issue: 2204585
Change-Id: I715b51021caa52bc19c44beecb43fa8b8e920d2a
Signed-off-by: Michael Savisko <michaelsav@mellanox.com>
Reviewed-by: Eli Britstein <elibr@mellanox.com>

lib/conntrack.c
@@ -1515,13 +1515,43 @@ conntrack_offload_prepare_add(struct conn *conn,
     conn->offloads.dir_info[dir].pkt_ct_state = packet->md.ct_state;
 }
 
+#ifdef E2E_CACHE_ENABLED
+static inline void
+e2e_cache_trace_add_ct(struct conntrack *ct,
+                       struct dp_packet *p,
+                       struct conn *conn,
+                       uint32_t mark,
+                       bool reply,
+                       ovs_u128 label)
+{
+    uint32_t e2e_trace_size = p->e2e_trace_size;
+    int dir;
+    struct ct_flow_offload_item item;
+
+    if (OVS_UNLIKELY(e2e_trace_size >= E2E_CACHE_MAX_TRACE)) {
+        p->e2e_trace_flags |= E2E_CACHE_TRACE_FLAG_OVERFLOW;
+        return;
+    }
+
+    dir = ct_get_packet_dir(reply);
+    conntrack_offload_fill_item_add(&item, conn, dir, mark, label);
+
+    p->e2e_trace_flags |= E2E_CACHE_TRACE_FLAG_CT;
+    p->e2e_trace[e2e_trace_size] = ct->offload_class->conn_get_ufid(&item);
+    p->e2e_trace_size = e2e_trace_size + 1;
+}
+#else
+#define e2e_cache_trace_add_ct(ct, p, conn, m, r, l) do { } while (0)
+#endif
+
 static void
 conntrack_offload_add_conn(struct conntrack *ct,
                            struct dp_packet *packet,
                            struct conn *conn,
                            void *dp,
                            uint32_t mark,
-                           ovs_u128 label)
+                           ovs_u128 label,
+                           bool e2e_cache_enabled)
 {
     /* nat_conn has opposite directions. */
     bool reply = !!conn->master_conn ^ packet->md.reply;
@@ -1543,6 +1573,9 @@ conntrack_offload_add_conn(struct conntrack *ct,
         if (conn->master_conn) {
             conn->master_conn->offloads.flags |= reply
                 ? CT_OFFLOAD_REP : CT_OFFLOAD_INIT;
+        }
+        if (e2e_cache_enabled) {
+            e2e_cache_trace_add_ct(ct, packet, conn, mark, reply, label);
         }
     }
 
@@ -1591,7 +1624,7 @@ conntrack_offload_add_conn(struct conntrack *ct,
 int
 conntrack_execute(struct conntrack *ct, struct dp_packet_batch *pkt_batch,
                   ovs_be16 dl_type, bool force, bool commit, uint16_t zone,
-                  const uint32_t *setmark,
+                  bool e2e_cache_enabled, const uint32_t *setmark,
                   const struct ovs_key_ct_labels *setlabel,
                   ovs_be16 tp_src, ovs_be16 tp_dst, const char *helper,
                   const struct nat_action_info_t *nat_action_info,
@@ -1628,7 +1661,8 @@ conntrack_execute(struct conntrack *ct, struct dp_packet_batch *pkt_batch,
         if (netdev_is_flow_api_enabled() &&
             (packet->md.ct_state & CS_ESTABLISHED) &&
             conn && !(conn->offloads.flags & CT_OFFLOAD_SKIP)) {
-            conntrack_offload_add_conn(ct, packet, conn, dp, mark, label);
+            conntrack_offload_add_conn(ct, packet, conn, dp, mark, label,
+                                       e2e_cache_enabled);
         }
     }
 

lib/conntrack.h
@@ -156,6 +156,7 @@ struct conntrack_offload_class {
     void (*conn_add)(struct ct_flow_offload_item *);
     void (*conn_del)(struct ct_flow_offload_item *);
     bool (*conn_active)(struct ct_flow_offload_item *, long long now);
+    ovs_u128 (*conn_get_ufid)(struct ct_flow_offload_item *);
 };
 
 struct conntrack *conntrack_init(void);
@@ -163,7 +164,7 @@ void conntrack_destroy(struct conntrack *);
 
 int conntrack_execute(struct conntrack *ct, struct dp_packet_batch *pkt_batch,
                       ovs_be16 dl_type, bool force, bool commit, uint16_t zone,
-                      const uint32_t *setmark,
+                      bool e2e_cache_enabled, const uint32_t *setmark,
                       const struct ovs_key_ct_labels *setlabel,
                       ovs_be16 tp_src, ovs_be16 tp_dst, const char *helper,
                       const struct nat_action_info_t *nat_action_info,

lib/dpif-netdev.c
@@ -575,10 +575,22 @@ dp_netdev_ct_offload_active(struct ct_flow_offload_item *offload,
     return stats.used >= now;
 }
 
+static ovs_u128
+dp_netdev_ct_offload_get_ufid(struct ct_flow_offload_item *offload)
+{
+    struct match match;
+    ovs_u128 ufid;
+
+    dp_netdev_fill_ct_match(&match, offload);
+    dp_netdev_get_mega_ufid((const struct match *)&match, &ufid);
+    return ufid;
+}
+
 static struct conntrack_offload_class dpif_ct_offload_class = {
     .conn_add = dp_netdev_ct_offload_add_item,
     .conn_del = dp_netdev_ct_offload_del_item,
     .conn_active = dp_netdev_ct_offload_active,
+    .conn_get_ufid = dp_netdev_ct_offload_get_ufid,
 };
 
 #define XPS_TIMEOUT 500000LL    /* In microseconds. */
@@ -7977,9 +7989,9 @@ dp_execute_cb(void *aux_, struct dp_packet_batch *packets_,
         }
 
         conntrack_execute(dp->conntrack, packets_, aux->flow->dl_type, force,
-                          commit, zone, setmark, setlabel, aux->flow->tp_src,
-                          aux->flow->tp_dst, helper, nat_action_info_ref,
-                          pmd->ctx.now / 1000, pmd->dp);
+                          commit, zone, e2e_cache_enabled, setmark, setlabel,
+                          aux->flow->tp_src, aux->flow->tp_dst, helper,
+                          nat_action_info_ref, pmd->ctx.now / 1000, pmd->dp);
         break;
     }
 

tests/test-conntrack.c
@@ -89,8 +89,8 @@ ct_thread_main(void *aux_)
     pkt_batch = prepare_packets(batch_size, change_conn, aux->tid, &dl_type);
     ovs_barrier_block(&barrier);
     for (i = 0; i < n_pkts; i += batch_size) {
-        conntrack_execute(ct, pkt_batch, dl_type, false, true, 0, NULL, NULL,
-                          0, 0, NULL, NULL, now, NULL);
+        conntrack_execute(ct, pkt_batch, dl_type, false, true, 0, false,
+                          NULL, NULL, 0, 0, NULL, NULL, now, NULL);
     }
     ovs_barrier_block(&barrier);
     destroy_packets(pkt_batch);
@@ -174,15 +174,15 @@ pcap_batch_execute_conntrack(struct conntrack *ct_,
 
         if (flow.dl_type != dl_type) {
             conntrack_execute(ct_, &new_batch, dl_type, false, true, 0,
-                              NULL, NULL, 0, 0, NULL, NULL, now, NULL);
+                              false, NULL, NULL, 0, 0, NULL, NULL, now, NULL);
             dp_packet_batch_init(&new_batch);
         }
         dp_packet_batch_add(&new_batch, packet);
     }
 
     if (!dp_packet_batch_is_empty(&new_batch)) {
-        conntrack_execute(ct_, &new_batch, dl_type, false, true, 0, NULL, NULL,
-                          0, 0, NULL, NULL, now, NULL);
+        conntrack_execute(ct_, &new_batch, dl_type, false, true, 0, false,
+                          NULL, NULL, 0, 0, NULL, NULL, now, NULL);
     }
 
 }

commit 96652fd7b7ef9da807f5d5a8ff98083b1fad1eca

conntrack: e2e: Add trace message for any packet of established connection
Trace message should be added for any packet of an established
connection, not like in offloads, where only the first time is needed.

Issue: 2228604
Change-Id: I27e165a68ed10dba7dcf9e3f79ca385f5276ec37
Fixes: c9324767f193 ("conntrack: e2e-cache tracer: collect all new CT IDs")
Signed-off-by: Eli Britstein <elibr@mellanox.com>

lib/conntrack.c
@@ -1520,8 +1520,8 @@ static inline void
 e2e_cache_trace_add_ct(struct conntrack *ct,
                        struct dp_packet *p,
                        struct conn *conn,
-                       uint32_t mark,
                        bool reply,
+                       uint32_t mark,
                        ovs_u128 label)
 {
     uint32_t e2e_trace_size = p->e2e_trace_size;
@@ -1541,7 +1541,7 @@ e2e_cache_trace_add_ct(struct conntrack *ct,
     p->e2e_trace_size = e2e_trace_size + 1;
 }
 #else
-#define e2e_cache_trace_add_ct(ct, p, conn, m, r, l) do { } while (0)
+#define e2e_cache_trace_add_ct(ct, p, conn, r, m, l) do { } while (0)
 #endif
 
 static void
@@ -1549,12 +1549,10 @@ conntrack_offload_add_conn(struct conntrack *ct,
                            struct dp_packet *packet,
                            struct conn *conn,
                            void *dp,
+                           bool reply,
                            uint32_t mark,
-                           ovs_u128 label,
-                           bool e2e_cache_enabled)
+                           ovs_u128 label)
 {
-    /* nat_conn has opposite directions. */
-    bool reply = !!conn->master_conn ^ packet->md.reply;
     struct ct_flow_offload_item item[CT_DIR_NUM];
     uint8_t flags;
     int dir;
@@ -1573,9 +1571,6 @@ conntrack_offload_add_conn(struct conntrack *ct,
         if (conn->master_conn) {
             conn->master_conn->offloads.flags |= reply
                 ? CT_OFFLOAD_REP : CT_OFFLOAD_INIT;
-        }
-        if (e2e_cache_enabled) {
-            e2e_cache_trace_add_ct(ct, packet, conn, mark, reply, label);
         }
     }
 
@@ -1658,11 +1653,18 @@ conntrack_execute(struct conntrack *ct, struct dp_packet_batch *pkt_batch,
                         setlabel, nat_action_info, tp_src, tp_dst, helper);
         }
         conn = packet->md.conn ? packet->md.conn : ctx.conn;
-        if (netdev_is_flow_api_enabled() &&
-            (packet->md.ct_state & CS_ESTABLISHED) &&
-            conn && !(conn->offloads.flags & CT_OFFLOAD_SKIP)) {
-            conntrack_offload_add_conn(ct, packet, conn, dp, mark, label,
-                                       e2e_cache_enabled);
+        if ((packet->md.ct_state & CS_ESTABLISHED) && conn) {
+            /* nat_conn has opposite directions. */
+            bool reply = !!conn->master_conn ^ packet->md.reply;
+
+            if (netdev_is_flow_api_enabled() &&
+                !(conn->offloads.flags & CT_OFFLOAD_SKIP)) {
+                conntrack_offload_add_conn(ct, packet, conn, dp, reply, mark,
+                                           label);
+            }
+            if (e2e_cache_enabled) {
+                e2e_cache_trace_add_ct(ct, packet, conn, reply, mark, label);
+            }
         }
     }
 