Mellanox/OVS

commit 771ee72ee108d1701cbf0b3bfb597433d336070f

dpif-netdev: e2e: Add flow/CT addition/deletion mechanism
Add APIs for adding/deletion of UFIDs into/from e2e-cache
thread's DB. Implement async deletion on context of e2e-cache
thread with sending message via dedicated queue.

Issue: 2241373
Change-Id: I28e9a4c32abda6503e44ea63e83726fa49460b07
Signed-off-by: Michael Savisko <michaelsav@mellanox.com>
Reviewed-by: Eli Britstein <elibr@mellanox.com>
Reviewed-by: Gavi Teitz <gavi@mellanox.com>

lib/dpif-netdev.c
@@ -7009,17 +7009,24 @@ struct e2e_cache_trace_msg_item {
     struct e2e_cache_trace_message *msg;
 };
 
+struct e2e_cache_del_ufid_msg {
+    struct ovs_list node;
+    ovs_u128 ufid;
+};
+
 struct e2e_cache_thread_msg_queues {
     struct ovs_mutex mutex;
+    struct ovs_list del_ufid_msg_list;
     struct ovs_list trace_msg_list;
-    /* TODO: add other message queues here */
     pthread_cond_t cond;
 };
 
 static struct e2e_cache_thread_msg_queues e2e_cache_thread_msg_queues = {
     .mutex = OVS_MUTEX_INITIALIZER,
+    .del_ufid_msg_list =
+        OVS_LIST_INITIALIZER(&e2e_cache_thread_msg_queues.del_ufid_msg_list),
     .trace_msg_list =
-            OVS_LIST_INITIALIZER(&e2e_cache_thread_msg_queues.trace_msg_list)
+        OVS_LIST_INITIALIZER(&e2e_cache_thread_msg_queues.trace_msg_list)
 };
 
 static struct ovsthread_once e2e_cache_thread_once
@@ -7104,7 +7111,8 @@ static inline void
 e2e_cache_thread_wait_on_queues(void)
 {
     ovs_mutex_lock(&e2e_cache_thread_msg_queues.mutex);
-    if (ovs_list_is_empty(&e2e_cache_thread_msg_queues.trace_msg_list)) {
+    if (ovs_list_is_empty(&e2e_cache_thread_msg_queues.del_ufid_msg_list) &&
+        ovs_list_is_empty(&e2e_cache_thread_msg_queues.trace_msg_list)) {
         ovsrcu_quiesce_start();
         ovs_mutex_cond_wait(&e2e_cache_thread_msg_queues.cond,
                             &e2e_cache_thread_msg_queues.mutex);
@@ -7213,7 +7221,6 @@ e2e_cache_flow_db_del_protected(const ovs_u128 *ufid, uint32_t hash)
     ovsrcu_postpone(e2e_cache_flow_free, dp_flow_data);
 }
 
-OVS_UNUSED
 static void
 e2e_cache_flow_db_del(const ovs_u128 *ufid)
 {
@@ -7224,7 +7231,6 @@ e2e_cache_flow_db_del(const ovs_u128 *ufid)
     ovs_mutex_unlock(&ufid_to_flow_map_mutex);
 }
 
-OVS_UNUSED
 static void
 e2e_cache_flow_db_put(struct e2e_cache_ufid_to_flow_item *dp_flow_data)
 {
@@ -7243,6 +7249,71 @@ e2e_cache_flow_db_put(struct e2e_cache_ufid_to_flow_item *dp_flow_data)
 
     cmap_insert(&ufid_to_flow_map, &dp_flow_data->node.in_cmap, hash);
     ovs_mutex_unlock(&ufid_to_flow_map_mutex);
+}
+
+OVS_UNUSED
+static int
+e2e_cache_flow_del(const ovs_u128 *ufid)
+{
+    struct e2e_cache_del_ufid_msg *del_msg =
+        (struct e2e_cache_del_ufid_msg *) xmalloc(sizeof *del_msg);
+
+    if (OVS_UNLIKELY(!del_msg)) {
+        return -1;
+    }
+    del_msg->ufid = *ufid;
+
+    /* Insert message into queue, e2e_cache_del_ufid_msg_dequeue()
+     * is used to dequeue it from there.
+     */
+    ovs_mutex_lock(&e2e_cache_thread_msg_queues.mutex);
+    ovs_list_push_back(&e2e_cache_thread_msg_queues.del_ufid_msg_list,
+                       &del_msg->node);
+    xpthread_cond_signal(&e2e_cache_thread_msg_queues.cond);
+    ovs_mutex_unlock(&e2e_cache_thread_msg_queues.mutex);
+
+    return 0;
+}
+
+OVS_UNUSED
+static int
+e2e_cache_flow_put(const ovs_u128 *ufid, struct match *match,
+                   const struct nlattr *actions, size_t actions_len)
+{
+    struct e2e_cache_ufid_to_flow_item *dp_flow_data;
+
+    dp_flow_data = (struct e2e_cache_ufid_to_flow_item *)
+        xzalloc(sizeof *dp_flow_data);
+    if (OVS_UNLIKELY(!dp_flow_data)) {
+        return -1;
+    }
+    dp_flow_data->actions = (struct nlattr *) xmalloc(actions_len);
+    if (OVS_UNLIKELY(!dp_flow_data->actions)) {
+        free(dp_flow_data);
+        return -1;
+    }
+
+    dp_flow_data->ufid = *ufid;
+    dp_flow_data->match = *match;
+    memcpy(dp_flow_data->actions, actions, actions_len);
+    dp_flow_data->actions_size = actions_len;
+
+    e2e_cache_flow_db_put(dp_flow_data);
+    return 0;
+}
+
+static struct e2e_cache_del_ufid_msg *
+e2e_cache_del_ufid_msg_dequeue(void)
+{
+    struct ovs_list *list;
+    struct e2e_cache_del_ufid_msg *del_msg;
+
+    ovs_mutex_lock(&e2e_cache_thread_msg_queues.mutex);
+    list = ovs_list_pop_front(&e2e_cache_thread_msg_queues.del_ufid_msg_list);
+    ovs_mutex_unlock(&e2e_cache_thread_msg_queues.mutex);
+
+    del_msg = CONTAINER_OF(list, struct e2e_cache_del_ufid_msg, node);
+    return del_msg;
 }
 
 static void
@@ -7317,16 +7388,22 @@ out:
 static void *
 dp_netdev_e2e_cache_main(void *arg OVS_UNUSED)
 {
+    struct e2e_cache_del_ufid_msg *del_msg;
     struct e2e_cache_trace_message *trace_msg;
 
     for (;;) {
         e2e_cache_thread_wait_on_queues();
 
+        while ((del_msg = e2e_cache_del_ufid_msg_dequeue()) != NULL) {
+            e2e_cache_flow_db_del(&del_msg->ufid);
+            free(del_msg);
+        }
+
         trace_msg = e2e_cache_trace_msg_dequeue();
         if (OVS_LIKELY(trace_msg)) {
             /* TODO: process traces message */
 
-            free_cacheline((void *) trace_msg);
+            free_cacheline(trace_msg);
         }
     }
 

commit bf287655baaac6e9c881a9bd61e69a94ebed8ce9

dpif-netdev: e2e: Fix deletion dequeing
The list must be checked for non empty before popping an element.

Issue: 2228604
Change-Id: I3451ad9468374000c475e7c236e35efa7eb83057
Fixes: 771ee72ee108 ("dpif-netdev: e2e: Add flow/CT addition/deletion mechanism")
Signed-off-by: Eli Britstein <elibr@mellanox.com>

lib/dpif-netdev.c
@@ -7309,6 +7309,9 @@ e2e_cache_del_ufid_msg_dequeue(void)
     struct ovs_list *list;
     struct e2e_cache_del_ufid_msg *del_msg;
 
+    if (ovs_list_is_empty(&e2e_cache_thread_msg_queues.del_ufid_msg_list)) {
+        return NULL;
+    }
     ovs_mutex_lock(&e2e_cache_thread_msg_queues.mutex);
     list = ovs_list_pop_front(&e2e_cache_thread_msg_queues.del_ufid_msg_list);
     ovs_mutex_unlock(&e2e_cache_thread_msg_queues.mutex);