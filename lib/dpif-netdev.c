Mellanox/OVS

commit a407a149cb1d29f869850ecb86448873acecc2c0

dpif-netdev: e2e-cache tracer: collect all flow IDs
Added e2e-cache tracer types and new fields in struct
dp_packet for storing IDs of all flows and CT inside
the packet structure though which it passes before
applying action. All this information will be later
sent to e2e-chache thread.
Collect of ufids of all flows that packet
passes through, stored in e2e_trace array inside
dp_packet structure.

Issue: 2204585
Change-Id: I0959396eb43a216b029a19bc8db7bba6d97bfa5f
Signed-off-by: Michael Savisko <michaelsav@mellanox.com>
Reviewed-by: Eli Britstein <elibr@mellanox.com>

lib/dpif-netdev.c
@@ -6941,6 +6941,39 @@ packet_enqueue_to_flow_map(struct dp_packet *packet,
     map->tcp_flags = tcp_flags;
 }
 
+#ifdef E2E_CACHE_ENABLED
+static inline void
+e2e_cache_trace_init(struct dp_packet *p)
+{
+    p->e2e_trace_size = 0;
+    p->e2e_trace_flags = E2E_CACHE_TRACE_FLAG_NONE;
+    p->e2e_trace_port = ODPP_NONE;
+}
+
+static inline void
+e2e_cache_trace_add_flow(struct dp_packet *p,
+                         const ovs_u128 *ufid,
+                         odp_port_t port)
+{
+    uint32_t e2e_trace_size = p->e2e_trace_size;
+
+    if (OVS_UNLIKELY(e2e_trace_size >= E2E_CACHE_MAX_TRACE)) {
+        p->e2e_trace_flags |= E2E_CACHE_TRACE_FLAG_OVERFLOW;
+        return;
+    }
+    p->e2e_trace[e2e_trace_size] = *ufid;
+    p->e2e_trace_size = e2e_trace_size + 1;
+    if (OVS_LIKELY(p->e2e_trace_flags & E2E_CACHE_TRACE_FLAG_PORT_SET)) {
+        return;
+    }
+    p->e2e_trace_flags |= E2E_CACHE_TRACE_FLAG_PORT_SET;
+    p->e2e_trace_port = port;
+}
+#else
+#define e2e_cache_trace_init(p) do { } while (0)
+#define e2e_cache_trace_add_flow(p, ufid, port) do { } while (0)
+#endif
+
 /* SMC lookup function for a batch of packets.
  * By doing batching SMC lookup, we can use prefetch
  * to hide memory access latency.
@@ -6994,6 +7027,11 @@ smc_lookup_batch(struct dp_netdev_pmd_thread *pmd,
                                                flow_map, recv_idx);
                     n_smc_hit++;
                     hit = true;
+
+                    if (e2e_cache_enabled) {
+                        e2e_cache_trace_add_flow(packet, &flow->ufid,
+                                                 flow->flow.in_port.odp_port);
+                    }
                     break;
                 }
             }
@@ -7081,6 +7119,9 @@ dfc_processing(struct dp_netdev_pmd_thread *pmd,
 
         if (!md_is_valid) {
             pkt_metadata_init(&packet->md, port_no);
+            if (e2e_cache_enabled) {
+                e2e_cache_trace_init(packet);
+            }
         }
 
         if ((*recirc_depth_get() == 0) &&
@@ -7108,6 +7149,10 @@ dfc_processing(struct dp_netdev_pmd_thread *pmd,
                     packet_enqueue_to_flow_map(packet, flow, tcp_flags,
                                                flow_map, map_cnt++);
                 }
+                if (e2e_cache_enabled) {
+                    e2e_cache_trace_add_flow(packet, &flow->ufid,
+                                             flow->flow.in_port.odp_port);
+                }
                 continue;
             }
         }
@@ -7134,6 +7179,10 @@ dfc_processing(struct dp_netdev_pmd_thread *pmd,
                  * same datapath flows. */
                 packet_enqueue_to_flow_map(packet, flow, tcp_flags,
                                            flow_map, map_cnt++);
+            }
+            if (e2e_cache_enabled) {
+                e2e_cache_trace_add_flow(packet, &flow->ufid,
+                                         flow->flow.in_port.odp_port);
             }
         } else {
             /* Exact match cache missed. Group missed packets together at
@@ -7350,6 +7399,12 @@ fast_path_processing(struct dp_netdev_pmd_thread *pmd,
         smc_insert(pmd, keys[i], hash);
 
         emc_probabilistic_insert(pmd, keys[i], flow);
+
+        if (e2e_cache_enabled) {
+            e2e_cache_trace_add_flow(packet, &flow->ufid,
+                                     flow->flow.in_port.odp_port);
+        }
+
         /* Add these packets into the flow map in the same order
          * as received.
          */

lib/dp-packet.h
@@ -62,6 +62,21 @@ enum dp_packet_offload_mask {
                                         IND_ATTACHED_MBUF)
 #endif
 
+#ifdef E2E_CACHE_ENABLED
+
+/* Types and macro definitions for e2e-cache. */
+
+#define E2E_CACHE_MAX_TRACE   (10u)
+
+enum e2e_cache_trace_flags {
+    E2E_CACHE_TRACE_FLAG_NONE        = 0x0u,
+    E2E_CACHE_TRACE_FLAG_PORT_SET    = 0x1u,
+    E2E_CACHE_TRACE_FLAG_CT          = 0x2u,
+    E2E_CACHE_TRACE_FLAG_OVERFLOW    = 0x4u
+};
+
+#endif
+
 /* Buffer for holding packet data.  A dp_packet is automatically reallocated
  * as necessary if it grows too large for the available memory.
  * By default the packet type is set to Ethernet (PT_ETH).
@@ -95,6 +110,12 @@ struct dp_packet {
         struct pkt_metadata md;
         uint64_t data[DP_PACKET_CONTEXT_SIZE / 8];
     };
+#ifdef E2E_CACHE_ENABLED
+    uint32_t   e2e_trace_size;
+    uint32_t   e2e_trace_flags;
+    odp_port_t e2e_trace_port;
+    ovs_u128   e2e_trace[E2E_CACHE_MAX_TRACE];
+#endif
 };
 
 #if HAVE_AF_XDP

commit 03dd871857798f2d73e575f712247930f893d618

dpif-netdev: Omit port argument from trace function
The port is inside the packet. No need for this extra argument.

Issue: 2228604
Change-Id: I1b04815db647fb5cb4d0e18387f7ff0d3a5083c0
Fixes: a407a149cb1d ("dpif-netdev: e2e-cache tracer: collect all flow IDs")
Signed-off-by: Eli Britstein <elibr@mellanox.com>

lib/dpif-netdev.c
@@ -7050,8 +7050,7 @@ e2e_cache_trace_init(struct dp_packet *p)
 
 static inline void
 e2e_cache_trace_add_flow(struct dp_packet *p,
-                         const ovs_u128 *ufid,
-                         odp_port_t port)
+                         const ovs_u128 *ufid)
 {
     uint32_t e2e_trace_size = p->e2e_trace_size;
 
@@ -7065,7 +7064,7 @@ e2e_cache_trace_add_flow(struct dp_packet *p,
         return;
     }
     p->e2e_trace_flags |= E2E_CACHE_TRACE_FLAG_PORT_SET;
-    p->e2e_trace_port = port;
+    p->e2e_trace_port = p->md.in_port.odp_port;
 }
 
 static inline int
@@ -7494,7 +7493,7 @@ dp_netdev_e2e_cache_main(void *arg OVS_UNUSED)
 }
 #else
 #define e2e_cache_trace_init(p) do { } while (0)
-#define e2e_cache_trace_add_flow(p, ufid, port) do { } while (0)
+#define e2e_cache_trace_add_flow(p, ufid) do { } while (0)
 #define e2e_cache_trace_msg_enqueue(m) do { } while (0)
 #define e2e_cache_trace_msg_dequeue() do { } while (0)
 #define e2e_cache_thread_wait_on_queues() do { } while (0)
@@ -7556,8 +7555,7 @@ smc_lookup_batch(struct dp_netdev_pmd_thread *pmd,
                     hit = true;
 
                     if (e2e_cache_enabled) {
-                        e2e_cache_trace_add_flow(packet, &flow->mega_ufid,
-                                                 flow->flow.in_port.odp_port);
+                        e2e_cache_trace_add_flow(packet, &flow->mega_ufid);
                     }
                     break;
                 }
@@ -7677,8 +7675,7 @@ dfc_processing(struct dp_netdev_pmd_thread *pmd,
                                                flow_map, map_cnt++);
                 }
                 if (e2e_cache_enabled) {
-                    e2e_cache_trace_add_flow(packet, &flow->mega_ufid,
-                                             flow->flow.in_port.odp_port);
+                    e2e_cache_trace_add_flow(packet, &flow->mega_ufid);
                 }
                 continue;
             }
@@ -7708,8 +7705,7 @@ dfc_processing(struct dp_netdev_pmd_thread *pmd,
                                            flow_map, map_cnt++);
             }
             if (e2e_cache_enabled) {
-                e2e_cache_trace_add_flow(packet, &flow->mega_ufid,
-                                         flow->flow.in_port.odp_port);
+                e2e_cache_trace_add_flow(packet, &flow->mega_ufid);
             }
         } else {
             /* Exact match cache missed. Group missed packets together at
@@ -7928,8 +7924,7 @@ fast_path_processing(struct dp_netdev_pmd_thread *pmd,
         emc_probabilistic_insert(pmd, keys[i], flow);
 
         if (e2e_cache_enabled) {
-            e2e_cache_trace_add_flow(packet, &flow->mega_ufid,
-                                     flow->flow.in_port.odp_port);
+            e2e_cache_trace_add_flow(packet, &flow->mega_ufid);
         }
 
         /* Add these packets into the flow map in the same order