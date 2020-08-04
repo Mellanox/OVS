Mellanox/OVS

commit 2dc76a536dcff9f856081ea80c18547a50d8a5d3

dpif-netdev: e2e: Dispatch trace info to e2e-cache
Dispatch a trace message to the e2e-cache for packet batches whose
fate is OVS_ACTION_ATTR_OUTPUT or OVS_ACTION_ATTR_DROP,
containing the trace info of each packet's trace within the batch.

Issue: 2204585
Change-Id: Ide841f091880577022bff1f9752306599a90c2c5
Signed-off-by: Michael Savisko <michaelsav@mellanox.com>
Reviewed-by: Eli Britstein <elibr@mellanox.com>

lib/dp-packet.h
@@ -75,6 +75,20 @@ enum e2e_cache_trace_flags {
     E2E_CACHE_TRACE_FLAG_OVERFLOW    = 0x4u
 };
 
+OVS_PACKED(
+struct e2e_cache_trace_info {
+    uint32_t num_elements;
+    uint32_t flags;
+    odp_port_t port;
+    ovs_u128 ufids[E2E_CACHE_MAX_TRACE];
+});
+
+OVS_PACKED(
+struct e2e_cache_trace_message {
+    uint32_t num_elements;
+    struct e2e_cache_trace_info data[0];
+});
+
 #endif
 
 /* Buffer for holding packet data.  A dp_packet is automatically reallocated

lib/dpif-netdev.c
@@ -6955,8 +6955,6 @@ packet_enqueue_to_flow_map(struct dp_packet *packet,
 
 #ifdef E2E_CACHE_ENABLED
 
-struct e2e_cache_trace_message;
-
 struct e2e_cache_trace_msg_item {
     struct ovs_list node;
     struct e2e_cache_trace_message *msg;
@@ -7004,7 +7002,6 @@ e2e_cache_trace_add_flow(struct dp_packet *p,
     p->e2e_trace_port = port;
 }
 
-OVS_UNUSED
 static inline int
 e2e_cache_trace_msg_enqueue(struct e2e_cache_trace_message *msg)
 {
@@ -7059,12 +7056,75 @@ e2e_cache_thread_wait_on_queues(void)
     ovs_mutex_unlock(&e2e_cache_thread_msg_queues.mutex);
 }
 
+static void
+e2e_cache_dispatch_trace_message(struct dp_packet_batch *batch)
+{
+    struct dp_packet *packet;
+    size_t buffer_size;
+    struct e2e_cache_trace_message *buffer;
+    struct e2e_cache_trace_info *cur_trace_info;
+    uint32_t num_elements;
+
+    buffer_size = sizeof(struct e2e_cache_trace_message) +
+                         batch->count * sizeof(struct e2e_cache_trace_info);
+
+    buffer = (struct e2e_cache_trace_message *) xmalloc_cacheline(buffer_size);
+    if (OVS_UNLIKELY(!buffer)) {
+        return;
+    }
+
+    num_elements = 0;
+    cur_trace_info = &buffer->data[0];
+
+    DP_PACKET_BATCH_FOR_EACH (i, packet, batch) {
+        uint32_t e2e_trace_size = packet->e2e_trace_size;
+
+        /* Don't send "partial" traces due to overflow of the trace storage */
+        if (OVS_UNLIKELY(cur_trace_info->flags &
+                         E2E_CACHE_TRACE_FLAG_OVERFLOW)) {
+            /* TODO: add statistic counter */
+            continue;
+        }
+        /* Send only traces for packet that passed conntrack */
+        if (!(cur_trace_info->flags & E2E_CACHE_TRACE_FLAG_CT)) {
+            continue;
+        }
+
+        cur_trace_info->num_elements = e2e_trace_size;
+        cur_trace_info->port = packet->e2e_trace_port;
+
+        memcpy(&cur_trace_info->ufids[0], &packet->e2e_trace[0],
+               e2e_trace_size * sizeof packet->e2e_trace[0]);
+
+        num_elements++;
+        cur_trace_info++;
+    }
+
+    if (num_elements == 0) {
+        goto out;
+    }
+
+    buffer->num_elements = num_elements;
+
+    if (OVS_UNLIKELY(e2e_cache_trace_msg_enqueue(buffer) != 0)) {
+        /* TODO: add statistic counter of send error */
+        goto out;
+    }
+
+    /* TODO: add statistic counter on success */
+    return;
+
+out:
+    free_cacheline(buffer);
+}
+
 #else
 #define e2e_cache_trace_init(p) do { } while (0)
 #define e2e_cache_trace_add_flow(p, ufid, port) do { } while (0)
 #define e2e_cache_trace_msg_enqueue(m) do { } while (0)
 #define e2e_cache_trace_msg_dequeue() do { } while (0)
 #define e2e_cache_thread_wait_on_queues() do { } while (0)
+#define e2e_cache_dispatch_trace_message(b) do { } while (0)
 #endif /* E2E_CACHE_ENABLED */
 
 /* SMC lookup function for a batch of packets.
@@ -7798,6 +7858,10 @@ dp_execute_cb(void *aux_, struct dp_packet_batch *packets_,
                 pmd->n_output_batches++;
             }
 
+            if (e2e_cache_enabled) {
+                e2e_cache_dispatch_trace_message(packets_);
+            }
+
             DP_PACKET_BATCH_FOR_EACH (i, packet, packets_) {
                 p->output_pkts_rxqs[dp_packet_batch_size(&p->output_pkts)] =
                                                              pmd->ctx.last_rxq;
@@ -8086,6 +8150,11 @@ dp_execute_cb(void *aux_, struct dp_packet_batch *packets_,
 
         odp_update_drop_action_counter((int)*drop_reason,
                                        dp_packet_batch_size(packets_));
+
+        if (e2e_cache_enabled) {
+            e2e_cache_dispatch_trace_message(packets_);
+        }
+
         dp_packet_delete_batch(packets_, should_steal);
         return;
     }

commit 4039b65f46b030b5bae18c1ebbd5d59c8599502a

dpif-netdev: e2e: Fix dispatch trace message
Issue: 2228604
Fixes: 2dc76a536dcf ("dpif-netdev: e2e: Dispatch trace info to e2e-cache")
Change-Id: Iec29962ed64faacc700cd1d21bdf30122cbe4327
Signed-off-by: Eli Britstein <elibr@mellanox.com>

lib/dp-packet.h
@@ -78,7 +78,6 @@ enum e2e_cache_trace_flags {
 OVS_PACKED(
 struct e2e_cache_trace_info {
     uint32_t num_elements;
-    uint32_t flags;
     odp_port_t port;
     ovs_u128 ufids[E2E_CACHE_MAX_TRACE];
 });

lib/dpif-netdev.c
@@ -7323,11 +7323,11 @@ e2e_cache_del_ufid_msg_dequeue(void)
 static void
 e2e_cache_dispatch_trace_message(struct dp_packet_batch *batch)
 {
-    struct dp_packet *packet;
-    size_t buffer_size;
-    struct e2e_cache_trace_message *buffer;
     struct e2e_cache_trace_info *cur_trace_info;
+    struct e2e_cache_trace_message *buffer;
+    struct dp_packet *packet;
     uint32_t num_elements;
+    size_t buffer_size;
 
     if (ovsthread_once_start(&e2e_cache_thread_once)) {
         xpthread_cond_init(&e2e_cache_thread_msg_queues.cond, NULL);
@@ -7351,13 +7351,13 @@ e2e_cache_dispatch_trace_message(struct dp_packet_batch *batch)
         uint32_t e2e_trace_size = packet->e2e_trace_size;
 
         /* Don't send "partial" traces due to overflow of the trace storage */
-        if (OVS_UNLIKELY(cur_trace_info->flags &
+        if (OVS_UNLIKELY(packet->e2e_trace_flags &
                          E2E_CACHE_TRACE_FLAG_OVERFLOW)) {
             /* TODO: add statistic counter */
             continue;
         }
         /* Send only traces for packet that passed conntrack */
-        if (!(cur_trace_info->flags & E2E_CACHE_TRACE_FLAG_CT)) {
+        if (!(packet->e2e_trace_flags & E2E_CACHE_TRACE_FLAG_CT)) {
             continue;
         }
 