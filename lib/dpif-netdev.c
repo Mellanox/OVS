Mellanox/OVS

commit d100bb0d4c08c93c7924c37fcea466414bea4567

dpif-netdev: e2e: Add message queues
Introduce message queues object for E2E cache communication,
and add a queue for trace messages, with enqueue and deque functions
enabling interaction between datapath tracing and E2E cache thread.

Issue: 2203814
Change-Id: I8e2cf43f23f07cd131a5d863466bb232d590ade7
Signed-off-by: Michael Savisko <michaelsav@mellanox.com>
Reviewed-by: Eli Britstein <elibr@mellanox.com>

lib/dpif-netdev.c
@@ -6954,6 +6954,28 @@ packet_enqueue_to_flow_map(struct dp_packet *packet,
 }
 
 #ifdef E2E_CACHE_ENABLED
+
+struct e2e_cache_trace_message;
+
+struct e2e_cache_trace_msg_item {
+    struct ovs_list node;
+    struct e2e_cache_trace_message *msg;
+};
+
+struct e2e_cache_thread_msg_queues {
+    struct ovs_mutex mutex;
+    struct ovs_list trace_msg_list;
+    /* TODO: add other message queues here */
+    pthread_cond_t cond;
+};
+
+static struct e2e_cache_thread_msg_queues e2e_cache_thread_msg_queues = {
+    .mutex = OVS_MUTEX_INITIALIZER,
+    .trace_msg_list =
+            OVS_LIST_INITIALIZER(&e2e_cache_thread_msg_queues.trace_msg_list)
+};
+
+
 static inline void
 e2e_cache_trace_init(struct dp_packet *p)
 {
@@ -6981,10 +7003,69 @@ e2e_cache_trace_add_flow(struct dp_packet *p,
     p->e2e_trace_flags |= E2E_CACHE_TRACE_FLAG_PORT_SET;
     p->e2e_trace_port = port;
 }
+
+OVS_UNUSED
+static inline int
+e2e_cache_trace_msg_enqueue(struct e2e_cache_trace_message *msg)
+{
+    struct e2e_cache_trace_msg_item *item;
+
+    item = (struct e2e_cache_trace_msg_item *) xmalloc(sizeof *item);
+    if (OVS_UNLIKELY(!item)) {
+        return -1;
+    }
+
+    item->msg = msg;
+
+    ovs_mutex_lock(&e2e_cache_thread_msg_queues.mutex);
+    ovs_list_push_back(&e2e_cache_thread_msg_queues.trace_msg_list,
+                       &item->node);
+    xpthread_cond_signal(&e2e_cache_thread_msg_queues.cond);
+    ovs_mutex_unlock(&e2e_cache_thread_msg_queues.mutex);
+
+    return 0;
+}
+
+OVS_UNUSED
+static inline struct e2e_cache_trace_message *
+e2e_cache_trace_msg_dequeue(void)
+{
+    struct ovs_list *list;
+    struct e2e_cache_trace_msg_item *trace_msg_item;
+    struct e2e_cache_trace_message *msg;
+
+    ovs_mutex_lock(&e2e_cache_thread_msg_queues.mutex);
+    list = ovs_list_pop_front(&e2e_cache_thread_msg_queues.trace_msg_list);
+    ovs_mutex_unlock(&e2e_cache_thread_msg_queues.mutex);
+
+    trace_msg_item = CONTAINER_OF(list, struct e2e_cache_trace_msg_item, node);
+    msg = trace_msg_item->msg;
+    free(trace_msg_item);
+
+    return msg;
+}
+
+OVS_UNUSED
+static inline void
+e2e_cache_thread_wait_on_queues(void)
+{
+    ovs_mutex_lock(&e2e_cache_thread_msg_queues.mutex);
+    if (ovs_list_is_empty(&e2e_cache_thread_msg_queues.trace_msg_list)) {
+        ovsrcu_quiesce_start();
+        ovs_mutex_cond_wait(&e2e_cache_thread_msg_queues.cond,
+                            &e2e_cache_thread_msg_queues.mutex);
+        ovsrcu_quiesce_end();
+    }
+    ovs_mutex_unlock(&e2e_cache_thread_msg_queues.mutex);
+}
+
 #else
 #define e2e_cache_trace_init(p) do { } while (0)
 #define e2e_cache_trace_add_flow(p, ufid, port) do { } while (0)
-#endif
+#define e2e_cache_trace_msg_enqueue(m) do { } while (0)
+#define e2e_cache_trace_msg_dequeue() do { } while (0)
+#define e2e_cache_thread_wait_on_queues() do { } while (0)
+#endif /* E2E_CACHE_ENABLED */
 
 /* SMC lookup function for a batch of packets.
  * By doing batching SMC lookup, we can use prefetch

commit 7b538636009a245e7a98e73b672cc80612569e65

dpif-netdev: e2e: Fix trace messages dequeing
The list must be checked for non empty before popping an element.

Issue: 2228604
Change-Id: I4c73d3155da1f5219f4cdf373930890c3065490b
Fixes: d100bb0d4c08 ("dpif-netdev: e2e: Add message queues")
Signed-off-by: Eli Britstein <elibr@mellanox.com>

lib/dpif-netdev.c
@@ -7096,6 +7096,9 @@ e2e_cache_trace_msg_dequeue(void)
     struct e2e_cache_trace_msg_item *trace_msg_item;
     struct e2e_cache_trace_message *msg;
 
+    if (ovs_list_is_empty(&e2e_cache_thread_msg_queues.trace_msg_list)) {
+        return NULL;
+    }
     ovs_mutex_lock(&e2e_cache_thread_msg_queues.mutex);
     list = ovs_list_pop_front(&e2e_cache_thread_msg_queues.trace_msg_list);
     ovs_mutex_unlock(&e2e_cache_thread_msg_queues.mutex);