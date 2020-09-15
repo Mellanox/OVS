a5ddb0faf338d5d41855aa7a07e5e625d78bb5c8
Eli Britstein <elibr@nvidia.com>
2020-09-15 07:01:36 +0000

    netdev-offload-dpdk: e2e: Support count actions by shared app IDs
    Issue: 2260475
    Change-Id: If60b06e7385a543aeb2239ca196336bfa86eb62f
    Signed-off-by: Eli Britstein <elibr@nvidia.com>
    Reviewed-by: Michael Savisko <michaelsav@nvidia.com>

--- lib/netdev-offload.h
+++ lib/netdev-offload.h
@@ -88,6 +88,9 @@ struct offload_info {
                                   * to delete the original flow. */
     /* Indicates if flow is for e2e cache*/
     bool is_e2e_cache_flow;
+
+    uint32_t flows_counter;
+    uint32_t ct_counter;
 };
 
 DECLARE_EXTERN_PER_THREAD_DATA(unsigned int, netdev_offload_thread_id);
