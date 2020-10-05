4e86e28e0497ca6e2f5600ef1e96d9a80ad5fb86
Eli Britstein <elibr@nvidia.com>
2020-10-05 12:32:15 +0000

    conntrack-tp: Introduce conn_protected_update_expiration
    In order to update connection expiration of offloaded connection,
    introduce such function to update when we are already locked.
    
    Issue: 2276889
    Change-Id: I967c3e134ba1a104b3dca126570f3f43259a68b7
    Signed-off-by: Eli Britstein <elibr@nvidia.com>

--- lib/conntrack-tp.h
+++ lib/conntrack-tp.h
@@ -27,4 +27,6 @@ void conn_init_expiration(struct conntrack *ct, struct conn *conn,
                           enum ct_timeout tm, long long now);
 void conn_update_expiration(struct conntrack *ct, struct conn *conn,
                             enum ct_timeout tm, long long now);
+void conn_protected_update_expiration(struct conntrack *ct, struct conn *conn,
+                                      enum ct_timeout tm, long long now);
 #endif
