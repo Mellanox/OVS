Mellanox/OVS

commit 7c2ee697aabf6704e365c1a631b3d08bffe80302

conntrack-tp: Introduce conn_protected_update_expiration
In order to update connection expiration of offloaded connection,
introduce such function to update when we are already locked.

Issue: 2276889
Change-Id: I967c3e134ba1a104b3dca126570f3f43259a68b7
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/conntrack-tp.c
@@ -233,22 +233,55 @@ tm_to_ct_dpif_tp(enum ct_timeout tm)
 static void
 conn_update_expiration__(struct conntrack *ct, struct conn *conn,
                          enum ct_timeout tm, long long now,
-                         uint32_t tp_value)
+                         uint32_t tp_value, bool protected)
     OVS_REQUIRES(conn->lock)
 {
-    ovs_mutex_unlock(&conn->lock);
+    VLOG_DBG_RL(&rl, "Update timeout %s zone=%u with policy id=%d "
+                "val=%u sec.",
+                ct_timeout_str[tm], conn->key.zone, conn->tp_id, tp_value);
 
-    ovs_mutex_lock(&ct->ct_lock);
-    ovs_mutex_lock(&conn->lock);
+    if (!protected) {
+        ovs_mutex_unlock(&conn->lock);
+
+        ovs_mutex_lock(&ct->ct_lock);
+        ovs_mutex_lock(&conn->lock);
+    }
     if (!conn->cleaned) {
         conn->expiration = now + tp_value * 1000;
         ovs_list_remove(&conn->exp_node);
         ovs_list_push_back(&ct->exp_lists[tm], &conn->exp_node);
     }
-    ovs_mutex_unlock(&conn->lock);
-    ovs_mutex_unlock(&ct->ct_lock);
+    if (!protected) {
+        ovs_mutex_unlock(&conn->lock);
+        ovs_mutex_unlock(&ct->ct_lock);
+    }
 
     ovs_mutex_lock(&conn->lock);
+}
+
+static uint32_t
+get_tp_id_val(struct conntrack *ct, uint32_t tp_id, enum ct_timeout tm)
+{
+    struct timeout_policy *tp;
+    uint32_t val;
+
+    tp = timeout_policy_lookup(ct, tp_id);
+    if (tp) {
+        val = tp->policy.attrs[tm_to_ct_dpif_tp(tm)];
+    } else {
+        val = ct_dpif_netdev_tp_def[tm_to_ct_dpif_tp(tm)];
+    }
+    return val;
+}
+
+void
+conn_protected_update_expiration(struct conntrack *ct, struct conn *conn,
+                                 enum ct_timeout tm, long long now)
+{
+    uint32_t tp_value;
+
+    tp_value = get_tp_id_val(ct, conn->tp_id, tm);
+    conn_update_expiration__(ct, conn, tm, now, tp_value, true);
 }
 
 /* The conn entry lock must be held on entry and exit. */
@@ -257,28 +290,19 @@ conn_update_expiration(struct conntrack *ct, struct conn *conn,
                        enum ct_timeout tm, long long now)
     OVS_REQUIRES(conn->lock)
 {
-    struct timeout_policy *tp;
     uint32_t val;
 
     ovs_mutex_unlock(&conn->lock);
 
     ovs_mutex_lock(&ct->ct_lock);
     ovs_mutex_lock(&conn->lock);
-    tp = timeout_policy_lookup(ct, conn->tp_id);
-    if (tp) {
-        val = tp->policy.attrs[tm_to_ct_dpif_tp(tm)];
-    } else {
-        val = ct_dpif_netdev_tp_def[tm_to_ct_dpif_tp(tm)];
-    }
+    val = get_tp_id_val(ct, conn->tp_id, tm);
     ovs_mutex_unlock(&conn->lock);
     ovs_mutex_unlock(&ct->ct_lock);
 
     ovs_mutex_lock(&conn->lock);
-    VLOG_DBG_RL(&rl, "Update timeout %s zone=%u with policy id=%d "
-                "val=%u sec.",
-                ct_timeout_str[tm], conn->key.zone, conn->tp_id, val);
 
-    conn_update_expiration__(ct, conn, tm, now, val);
+    conn_update_expiration__(ct, conn, tm, now, val, false);
 }
 
 static void

lib/conntrack-tp.h
@@ -27,4 +27,6 @@ void conn_init_expiration(struct conntrack *ct, struct conn *conn,
                           enum ct_timeout tm, long long now);
 void conn_update_expiration(struct conntrack *ct, struct conn *conn,
                             enum ct_timeout tm, long long now);
+void conn_protected_update_expiration(struct conntrack *ct, struct conn *conn,
+                                      enum ct_timeout tm, long long now);
 #endif

commit bd4607d6f773243e282985f782aa312baac3a9fd

conntrack-tp: Fix and simplify locking for expiration updates
A protected version of connection expiration update function was
introduced, that was meant to be used when the connection lock is
already taken. Calls to this new protected function however do not
respect the lock contracts of calls made within the new function,
within 'get_tp_id_val' or when calling 'conn_update_expiration__'.

Fix lock taking before those calls, simplify order of such locking and
add new attributes to function to clarify requirements.

Issue: 2336735
Fixes: 7c2ee697aabf ("conntrack-tp: Introduce conn_protected_update_expiration")
Change-Id: I1ee663855372970026b64e9c7355f731ecf64d42
Signed-off-by: Gaetan Rivet <grive@u256.net>
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/conntrack-tp.c
@@ -233,34 +233,23 @@ tm_to_ct_dpif_tp(enum ct_timeout tm)
 static void
 conn_update_expiration__(struct conntrack *ct, struct conn *conn,
                          enum ct_timeout tm, long long now,
-                         uint32_t tp_value, bool protected)
-    OVS_REQUIRES(conn->lock)
+                         uint32_t tp_value)
+    OVS_REQUIRES(ct->ct_lock, conn->lock)
 {
     VLOG_DBG_RL(&rl, "Update timeout %s zone=%u with policy id=%d "
                 "val=%u sec.",
                 ct_timeout_str[tm], conn->key.zone, conn->tp_id, tp_value);
 
-    if (!protected) {
-        ovs_mutex_unlock(&conn->lock);
-
-        ovs_mutex_lock(&ct->ct_lock);
-        ovs_mutex_lock(&conn->lock);
-    }
     if (!conn->cleaned) {
         conn->expiration = now + tp_value * 1000;
         ovs_list_remove(&conn->exp_node);
         ovs_list_push_back(&ct->exp_lists[tm], &conn->exp_node);
     }
-    if (!protected) {
-        ovs_mutex_unlock(&conn->lock);
-        ovs_mutex_unlock(&ct->ct_lock);
-    }
-
-    ovs_mutex_lock(&conn->lock);
 }
 
 static uint32_t
 get_tp_id_val(struct conntrack *ct, uint32_t tp_id, enum ct_timeout tm)
+    OVS_REQUIRES(ct->ct_lock)
 {
     struct timeout_policy *tp;
     uint32_t val;
@@ -277,11 +266,12 @@ get_tp_id_val(struct conntrack *ct, uint32_t tp_id, enum ct_timeout tm)
 void
 conn_protected_update_expiration(struct conntrack *ct, struct conn *conn,
                                  enum ct_timeout tm, long long now)
+    OVS_REQUIRES(ct->ct_lock, conn->lock)
 {
     uint32_t tp_value;
 
     tp_value = get_tp_id_val(ct, conn->tp_id, tm);
-    conn_update_expiration__(ct, conn, tm, now, tp_value, true);
+    conn_update_expiration__(ct, conn, tm, now, tp_value);
 }
 
 /* The conn entry lock must be held on entry and exit. */
@@ -289,20 +279,16 @@ void
 conn_update_expiration(struct conntrack *ct, struct conn *conn,
                        enum ct_timeout tm, long long now)
     OVS_REQUIRES(conn->lock)
+    OVS_EXCLUDED(ct->ct_lock)
 {
-    uint32_t val;
-
     ovs_mutex_unlock(&conn->lock);
 
     ovs_mutex_lock(&ct->ct_lock);
     ovs_mutex_lock(&conn->lock);
-    val = get_tp_id_val(ct, conn->tp_id, tm);
-    ovs_mutex_unlock(&conn->lock);
+
+    conn_protected_update_expiration(ct, conn, tm, now);
+
     ovs_mutex_unlock(&ct->ct_lock);
-
-    ovs_mutex_lock(&conn->lock);
-
-    conn_update_expiration__(ct, conn, tm, now, val, false);
 }
 
 static void