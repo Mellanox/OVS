Mellanox/OVS

commit 1777cec4f56eb976c84016990cc011444af2ac89

netdev-dpdk: Introduce get netdev by devargs function
When a vport flow is removed, we need to get the netdev struct from the
devargs we kept in the offload event, in order to remove the rules.
Introduce a function to lookup a netdev by provided devargs.

Issue: 1975006
Change-Id: Ib553220ea6114e4f4b9284e695488037b60db970
Signed-off-by: Eli Britstein <elibr@mellanox.com>

lib/netdev-dpdk.c
@@ -5202,6 +5202,29 @@ out:
     return devargs;
 }
 
+struct netdev *
+netdev_dpdk_get_netdev_by_devargs(const char *devargs)
+{
+    struct netdev *netdev = NULL;
+    struct netdev_dpdk *dev;
+    dpdk_port_t port_id;
+
+    ovs_mutex_lock(&dpdk_mutex);
+    port_id = netdev_dpdk_get_port_by_devargs(devargs);
+    if (!rte_eth_dev_is_valid_port(port_id)) {
+        goto out;
+    }
+    dev = netdev_dpdk_lookup_by_port_id(port_id);
+    ovs_mutex_lock(&dev->mutex);
+    netdev = &dev->up;
+    netdev_ref(netdev);
+    ovs_mutex_unlock(&dev->mutex);
+
+out:
+    ovs_mutex_unlock(&dpdk_mutex);
+    return netdev;
+}
+
 bool
 netdev_dpdk_flow_api_supported(struct netdev *netdev)
 {

lib/netdev-dpdk.h
@@ -59,6 +59,8 @@ bool
 netdev_dpdk_is_uplink_port(struct netdev *netdev);
 const char *
 netdev_dpdk_get_port_devargs(struct netdev *netdev);
+struct netdev *
+netdev_dpdk_get_netdev_by_devargs(const char *devargs);
 
 #else
 

commit d17144b1035e7a58ac8f8f12dda2f3151909537c

netdev-dpdk: Fix no port found with netdev_dpdk_get_netdev_by_devargs
In a race condition that ports are removed and offload rules are still
accessed (deletion or statistics), a device might not be found. Add a
validity check for that.

Issue: 2297791
Change-Id: I289c529e3c24746ad5457eaf526454abaf67fd27
Fixes: 1777cec4f56e ("netdev-dpdk: Introduce get netdev by devargs function")
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/netdev-dpdk.c
@@ -5379,6 +5379,9 @@ netdev_dpdk_get_netdev_by_devargs(const char *devargs)
         goto out;
     }
     dev = netdev_dpdk_lookup_by_port_id(port_id);
+    if (!dev) {
+        goto out;
+    }
     ovs_mutex_lock(&dev->mutex);
     netdev = &dev->up;
     netdev_ref(netdev);