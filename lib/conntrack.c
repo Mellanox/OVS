Mellanox/OVS

commit 31fe82e87c2b156a8f976ee88a22f02be3f4138c

conntrack: Sync CT offloads with refcount
CT offloading is done by issueing requests to the offload queue. There
is a scenario in high load, where a connection is deleted while its
offload request has not yet been handled. In such a case the offload
request should not take place (when its time to be processed).
This changes previous behavior that blocked removal of connections until
their offload request is handled ("dont_free").

Issue: 2340602
Change-Id: I8cf164e992416ebf57b0e7f61745f2c5ba90044d
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/conntrack.c
@@ -316,6 +316,11 @@ conntrack_offload_del_conn(struct conntrack *ct,
         return;
     }
 
+    if (!conn->offloads.refcnt ||
+        ovs_refcount_unref(conn->offloads.refcnt) > 1) {
+        return;
+    }
+
     for (dir = 0; dir < CT_DIR_NUM; dir ++) {
         if (conn->nat_conn &&
             conn->nat_conn->offloads.dir_info[dir].dp) {
@@ -328,6 +333,8 @@ conntrack_offload_del_conn(struct conntrack *ct,
         }
         item[dir].ctid = conn_dir->offloads.ctid;
     }
+    item[CT_DIR_INIT].refcnt = conn->offloads.refcnt;
+    item[CT_DIR_REP].refcnt = NULL;
     ct->offload_class->conn_del(item);
 }
 
@@ -472,7 +479,7 @@ zone_limit_delete(struct conntrack *ct, uint16_t zone)
     return 0;
 }
 
-static int
+static void
 conn_clean_cmn(struct conntrack *ct, struct conn *conn)
     OVS_REQUIRES(ct->ct_lock)
 {
@@ -480,19 +487,8 @@ conn_clean_cmn(struct conntrack *ct, struct conn *conn)
         expectation_clean(ct, &conn->key);
     }
 
-    if (conn->offloads.dir_info[CT_DIR_INIT].dont_free ||
-        conn->offloads.dir_info[CT_DIR_REP].dont_free ||
-        (conn->nat_conn &&
-         (conn->nat_conn->offloads.dir_info[CT_DIR_INIT].dont_free ||
-          conn->nat_conn->offloads.dir_info[CT_DIR_REP].dont_free))) {
-        return -1;
-    }
-
     if (netdev_is_flow_api_enabled()) {
         conntrack_offload_del_conn(ct, conn);
-        if (conn->nat_conn) {
-            conntrack_offload_del_conn(ct, conn->nat_conn);
-        }
     }
 
     uint32_t hash = conn_key_hash(&conn->key, ct->hash_basis);
@@ -502,7 +498,6 @@ conn_clean_cmn(struct conntrack *ct, struct conn *conn)
     if (zl && zl->czl.zone_limit_seq == conn->zone_limit_seq) {
         zl->czl.count--;
     }
-    return 0;
 }
 
 /* Must be called with 'conn' of 'conn_type' CT_CONN_TYPE_DEFAULT.  Also
@@ -513,9 +508,7 @@ conn_clean(struct conntrack *ct, struct conn *conn)
 {
     ovs_assert(conn->conn_type == CT_CONN_TYPE_DEFAULT);
 
-    if (conn_clean_cmn(ct, conn)) {
-        return;
-    }
+    conn_clean_cmn(ct, conn);
     if (conn->nat_conn) {
         uint32_t hash = conn_key_hash(&conn->nat_conn->key, ct->hash_basis);
         cmap_remove(&ct->conns, &conn->nat_conn->cm_node, hash);
@@ -1507,7 +1500,6 @@ conntrack_offload_fill_item_add(struct ct_flow_offload_item *item,
     item->label_key = conn->label;
     item->label_mask.u64.hi = label.u64.hi ^ conn->label.u64.hi;
     item->label_mask.u64.lo = label.u64.lo ^ conn->label.u64.lo;
-    item->dont_free = &conn->offloads.dir_info[dir].dont_free;
     item->status = &conn->offloads.dir_info[dir].status;
     item->ctid_ptr = &conn->offloads.ctid;
 }
@@ -1523,7 +1515,6 @@ conntrack_offload_prepare_add(struct conn *conn,
 
     conn->offloads.dir_info[dir].port = packet->md.in_port.odp_port;
     conn->offloads.dir_info[dir].dp = dp;
-    conn->offloads.dir_info[dir].dont_free = true;
     conn->offloads.dir_info[dir].pkt_ct_state = packet->md.ct_state;
 }
 
@@ -1584,6 +1575,11 @@ conntrack_offload_add_conn(struct conntrack *ct,
                                          &item[CT_DIR_REP].ufid);
         *ufid[CT_DIR_INIT] = item[CT_DIR_INIT].ufid;
         *ufid[CT_DIR_REP] = item[CT_DIR_REP].ufid;
+        conn->offloads.refcnt = xmalloc(sizeof conn->offloads.refcnt);
+        ovs_refcount_init(conn->offloads.refcnt);
+        ovs_refcount_ref(conn->offloads.refcnt);
+        item[CT_DIR_INIT].refcnt = conn->offloads.refcnt;
+        item[CT_DIR_REP].refcnt = NULL;
         ct->offload_class->conn_add(item);
         conn->offloads.flags |= CT_OFFLOAD_SKIP;
         if (conn->nat_conn) {

lib/conntrack-private.h
@@ -72,7 +72,6 @@ struct ct_dir_info {
     ovs_u128 ufid;
     void *dp;
     int status;
-    bool dont_free;
     uint8_t pkt_ct_state;
 };
 
@@ -87,6 +86,7 @@ enum ct_offload_flag {
 struct ct_offloads {
     uint8_t flags;
     uint32_t ctid;
+    struct ovs_refcount *refcnt;
     struct ct_dir_info dir_info[CT_DIR_NUM];
 };
 

lib/conntrack.h
@@ -146,10 +146,10 @@ struct ct_flow_offload_item {
      */
     int *status;
 
-    /* dont_free flag protects a scenario of freeing the connection while the
-     * offload request has not been handled yet.
+    /* refcnt is used to handle a scenario in which a connection issued an
+     * offload request and was removed before the offload request is processed.
      */
-    bool *dont_free;
+    struct ovs_refcount *refcnt;
 };
 
 /* hw-offload callbacks */

lib/dpif-netdev.c
@@ -2973,8 +2973,7 @@ dp_netdev_ct_offload_add(struct dp_offload_item *offload_item, int dir)
 
     port = netdev_ports_get(offload.odp_port, dpif_type_str);
     if (!port) {
-        ret = -1;
-        goto out;
+        return -1;
     }
 
     dp_netdev_fill_ct_match(&match, &offload);
@@ -3017,18 +3016,11 @@ dp_netdev_ct_offload_add(struct dp_offload_item *offload_item, int dir)
     }
     ret = netdev_flow_put(port, &match, actions, buf.size, &offload.ufid, &info,
                           NULL);
-    /* A memory barrier that makes sure that the lines will be executed by
-     * order, and offload.dont_free won't be changed before offload.status is
-     * updated.
-     */
     *offload.status = !ret;
-    atomic_thread_fence(memory_order_acquire);
-    *offload.dont_free = false;
     ovs_mutex_unlock(&offload_item->dp->port_mutex);
     netdev_close(port);
     ofpbuf_uninit(&buf);
 
-out:
     return ret;
 }
 
@@ -3038,12 +3030,8 @@ dp_netdev_ct_offload_del(struct dp_offload_item *offload_item, int dir)
     const char *dpif_type_str = dpif_normalize_type(offload_item->dp->class->type);
     struct ct_flow_offload_item offload = offload_item->ct_offload_item[dir];
     struct netdev *port;
-    int ret = 0;
+    int ret;
 
-    /* if a direction is not offloaded do not attempt to delete it from HW */
-    if (!offload.dp) {
-        return -1;
-    }
     port = netdev_ports_get(offload.odp_port, dpif_type_str);
     if (!port) {
         return -1;
@@ -3083,7 +3071,6 @@ dp_release_ctid(uint32_t ctid)
     id_pool_free_id(ctid_pool, ctid);
 }
 
-
 static void
 dp_netdev_ct_offload_handle(struct dp_offload_item *offload_item)
 {
@@ -3093,6 +3080,15 @@ dp_netdev_ct_offload_handle(struct dp_offload_item *offload_item)
     int ts_index = 0;
     const char *op;
     int dir;
+
+    if (ct_offload[CT_DIR_INIT].op == DP_NETDEV_FLOW_OFFLOAD_OP_ADD &&
+        ovs_refcount_unref(ct_offload[CT_DIR_INIT].refcnt) == 1) {
+        free(ct_offload[CT_DIR_INIT].refcnt);
+        return;
+    }
+    if (ct_offload[CT_DIR_INIT].op == DP_NETDEV_FLOW_OFFLOAD_OP_DEL) {
+        free(ct_offload[CT_DIR_INIT].refcnt);
+    }
 
     if (ct_offload[CT_DIR_INIT].op == DP_NETDEV_FLOW_OFFLOAD_OP_ADD) {
         dp_alloc_ctid(ct_offload->ctid_ptr);

commit e7697ead629661571cdca5af0ccfc974c00b2441

conntrack: Fix refcount for NAT scenarios
Keep the offload refcnt of the connection in the master struct.

Issue: 2260832
Change-Id: I39f9c8b232d154c4c9f1899bad995b727b78faf8
Fixes: 31fe82e87c2b ("conntrack: Sync CT offloads with refcount")
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/conntrack.c
@@ -1555,6 +1555,7 @@ conntrack_offload_add_conn(struct conntrack *ct,
         flags |= conn->master_conn->offloads.flags;
     }
     if ((flags & CT_OFFLOAD_BOTH) == CT_OFFLOAD_BOTH) {
+        struct ovs_refcount *refcnt;
         ovs_u128 *ufid[CT_DIR_NUM];
 
         for (dir = 0; dir < CT_DIR_NUM; dir ++) {
@@ -1575,17 +1576,19 @@ conntrack_offload_add_conn(struct conntrack *ct,
                                          &item[CT_DIR_REP].ufid);
         *ufid[CT_DIR_INIT] = item[CT_DIR_INIT].ufid;
         *ufid[CT_DIR_REP] = item[CT_DIR_REP].ufid;
-        conn->offloads.refcnt = xmalloc(sizeof conn->offloads.refcnt);
-        ovs_refcount_init(conn->offloads.refcnt);
-        ovs_refcount_ref(conn->offloads.refcnt);
-        item[CT_DIR_INIT].refcnt = conn->offloads.refcnt;
+        refcnt = xmalloc(sizeof *refcnt);
+        ovs_refcount_init(refcnt);
+        ovs_refcount_ref(refcnt);
+        item[CT_DIR_INIT].refcnt = refcnt;
         item[CT_DIR_REP].refcnt = NULL;
         ct->offload_class->conn_add(item);
         conn->offloads.flags |= CT_OFFLOAD_SKIP;
         if (conn->nat_conn) {
             conn->nat_conn->offloads.flags |= CT_OFFLOAD_SKIP;
+            conn->offloads.refcnt = refcnt;
         } else if (conn->master_conn) {
             conn->master_conn->offloads.flags |= CT_OFFLOAD_SKIP;
+            conn->master_conn->offloads.refcnt = refcnt;
         }
     }
 }