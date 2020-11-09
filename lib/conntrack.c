Mellanox/OVS

commit e7697ead629661571cdca5af0ccfc974c00b2441

conntrack: Fix refcount for NAT scenarios
Keep the offload refcnt of the connection in the master struct.

Issue: 2260832
Change-Id: I39f9c8b232d154c4c9f1899bad995b727b78faf8
Fixes: 31fe82e87c2b ("conntrack: Sync CT offloads with refcount")
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/conntrack.c
@@ -1555,6 +1555,7 @@ conntrack_offload_add_conn(struct conntrack *ct,
         flags |= conn->master_conn->offloads.flags;
     }
     if ((flags & CT_OFFLOAD_BOTH) == CT_OFFLOAD_BOTH) {
+        struct ovs_refcount *refcnt;
         ovs_u128 *ufid[CT_DIR_NUM];
 
         for (dir = 0; dir < CT_DIR_NUM; dir ++) {
@@ -1575,17 +1576,19 @@ conntrack_offload_add_conn(struct conntrack *ct,
                                          &item[CT_DIR_REP].ufid);
         *ufid[CT_DIR_INIT] = item[CT_DIR_INIT].ufid;
         *ufid[CT_DIR_REP] = item[CT_DIR_REP].ufid;
-        conn->offloads.refcnt = xmalloc(sizeof conn->offloads.refcnt);
-        ovs_refcount_init(conn->offloads.refcnt);
-        ovs_refcount_ref(conn->offloads.refcnt);
-        item[CT_DIR_INIT].refcnt = conn->offloads.refcnt;
+        refcnt = xmalloc(sizeof *refcnt);
+        ovs_refcount_init(refcnt);
+        ovs_refcount_ref(refcnt);
+        item[CT_DIR_INIT].refcnt = refcnt;
         item[CT_DIR_REP].refcnt = NULL;
         ct->offload_class->conn_add(item);
         conn->offloads.flags |= CT_OFFLOAD_SKIP;
         if (conn->nat_conn) {
             conn->nat_conn->offloads.flags |= CT_OFFLOAD_SKIP;
+            conn->offloads.refcnt = refcnt;
         } else if (conn->master_conn) {
             conn->master_conn->offloads.flags |= CT_OFFLOAD_SKIP;
+            conn->master_conn->offloads.refcnt = refcnt;
         }
     }
 }

commit 350bfaa14f066dce7dea265c10e5ea9759a8fe5b

conntrack: Fix basic CT-offload refcount
Previous fix for NAT CT-offload has introduced a bug by assigning
the refcount only for NAT scenarios, missing the basic CT case. Fix it.

Issue: 2340602
Fixes: e7697ead6296 ("conntrack: Fix refcount for NAT scenarios")
Change-Id: I54b9b8c0978a2af469d3967f300807855d4d6ba1
Signed-off-by: Gaetan Rivet <grive@u256.net>

lib/conntrack.c
@@ -1589,6 +1589,8 @@ conntrack_offload_add_conn(struct conntrack *ct,
         } else if (conn->master_conn) {
             conn->master_conn->offloads.flags |= CT_OFFLOAD_SKIP;
             conn->master_conn->offloads.refcnt = refcnt;
+        } else {
+            conn->offloads.refcnt = refcnt;
         }
     }
 }