Mellanox/OVS

commit ad4b4135bb4e1043cc315a53db2d7a23ba1aedc7

dpif-netdev: e2e: Add flows database
Introduce a flows database for the e2e-cache, for tracking flows and
merged flows, with functions for finding/removing/adding flows
in/from/to the database.

Issue: 2241373
Change-Id: I7b99fa525a2e9df351ce76e64ac850c4d210b33c
Signed-off-by: Michael Savisko <michaelsav@mellanox.com>
Reviewed-by: Gavi Teitz <gavi@mellanox.com>
Reviewed-by: Eli Britstein <elibr@mellanox.com>

lib/dpif-netdev.c
@@ -933,7 +933,10 @@ struct dpif_netdev {
  * A mapping from ufid to flow for e2e cache.
  */
 struct e2e_cache_ufid_to_flow_item {
-    struct cmap_node node;
+    union {
+        struct cmap_node in_cmap;
+        struct hmap_node in_hmap;
+    } node;
     ovs_u128 ufid;
     struct match match;
     struct nlattr *actions;
@@ -7022,6 +7025,12 @@ static struct e2e_cache_thread_msg_queues e2e_cache_thread_msg_queues = {
 static struct ovsthread_once e2e_cache_thread_once
     = OVSTHREAD_ONCE_INITIALIZER;
 
+static struct ovs_mutex ufid_to_flow_map_mutex = OVS_MUTEX_INITIALIZER;
+static struct cmap ufid_to_flow_map OVS_GUARDED_BY(ufid_to_flow_map_mutex) =
+    CMAP_INITIALIZER;
+
+static struct hmap merged_flows_map = HMAP_INITIALIZER(&merged_flows_map);
+
 static void *dp_netdev_e2e_cache_main(void *arg);
 
 static inline void
@@ -7102,6 +7111,138 @@ e2e_cache_thread_wait_on_queues(void)
         ovsrcu_quiesce_end();
     }
     ovs_mutex_unlock(&e2e_cache_thread_msg_queues.mutex);
+}
+
+/* Find dp_flow_data with @ufid. */
+static inline struct e2e_cache_ufid_to_flow_item *
+e2e_cache_merged_flow_find(const ovs_u128 *ufid)
+{
+    uint32_t hash = hash_bytes(ufid, sizeof *ufid, 0);
+    struct e2e_cache_ufid_to_flow_item *data;
+
+    HMAP_FOR_EACH_WITH_HASH (data, node.in_hmap, hash, &merged_flows_map) {
+        if (ovs_u128_equals(*ufid, data->ufid)) {
+            return data;
+        }
+    }
+
+    return NULL;
+}
+
+static void
+e2e_cache_flow_free(void *arg)
+{
+    struct e2e_cache_ufid_to_flow_item *dp_flow_data =
+        (struct e2e_cache_ufid_to_flow_item *) arg;
+
+    if (dp_flow_data->actions) {
+        free(dp_flow_data->actions);
+    }
+    free(dp_flow_data);
+}
+
+OVS_UNUSED
+static void
+e2e_cache_merged_flow_db_del(const ovs_u128 *ufid)
+{
+    struct e2e_cache_ufid_to_flow_item *dp_flow_data =
+        e2e_cache_merged_flow_find(ufid);
+
+    if (OVS_UNLIKELY(!dp_flow_data)) {
+        return;
+    }
+
+    hmap_remove(&merged_flows_map, &dp_flow_data->node.in_hmap);
+    /* TODO: Add statistic counter */
+
+    e2e_cache_flow_free(dp_flow_data);
+}
+
+OVS_UNUSED
+static inline void
+e2e_cache_merged_flow_db_put(struct e2e_cache_ufid_to_flow_item *merged_flow)
+{
+    uint32_t hash =
+        hash_bytes(&merged_flow->ufid, sizeof merged_flow->ufid, 0);
+
+    hmap_insert(&merged_flows_map, &merged_flow->node.in_hmap, hash);
+}
+
+/* Find dp_flow_data with @ufid. */
+OVS_UNUSED
+static inline struct e2e_cache_ufid_to_flow_item *
+e2e_cache_ufid_to_flow_data_find(const ovs_u128 *ufid)
+{
+    size_t hash = hash_bytes(ufid, sizeof *ufid, 0);
+    struct e2e_cache_ufid_to_flow_item *data;
+
+    CMAP_FOR_EACH_WITH_HASH (data, node.in_cmap, hash, &ufid_to_flow_map) {
+        if (ovs_u128_equals(*ufid, data->ufid)) {
+            return data;
+        }
+    }
+
+    return NULL;
+}
+
+static inline struct e2e_cache_ufid_to_flow_item *
+e2e_cache_ufid_to_flow_data_find_protected(const ovs_u128 *ufid, uint32_t hash)
+{
+    struct e2e_cache_ufid_to_flow_item *data;
+
+    CMAP_FOR_EACH_WITH_HASH_PROTECTED (data, node.in_cmap, hash,
+                                       &ufid_to_flow_map) {
+        if (ovs_u128_equals(*ufid, data->ufid)) {
+            return data;
+        }
+    }
+
+    return NULL;
+}
+
+static void
+e2e_cache_flow_db_del_protected(const ovs_u128 *ufid, uint32_t hash)
+{
+    struct e2e_cache_ufid_to_flow_item *dp_flow_data;
+
+    dp_flow_data = e2e_cache_ufid_to_flow_data_find_protected(ufid, hash);
+    if (OVS_UNLIKELY(!dp_flow_data)) {
+        return;
+    }
+    cmap_remove(&ufid_to_flow_map, &dp_flow_data->node.in_cmap, hash);
+    ovsrcu_postpone(e2e_cache_flow_free, dp_flow_data);
+}
+
+OVS_UNUSED
+static void
+e2e_cache_flow_db_del(const ovs_u128 *ufid)
+{
+    size_t hash = hash_bytes(ufid, sizeof *ufid, 0);
+
+    ovs_mutex_lock(&ufid_to_flow_map_mutex);
+    e2e_cache_flow_db_del_protected(ufid, hash);
+    ovs_mutex_unlock(&ufid_to_flow_map_mutex);
+}
+
+OVS_UNUSED
+static void
+e2e_cache_flow_db_put(struct e2e_cache_ufid_to_flow_item *dp_flow_data)
+{
+    size_t hash;
+    struct e2e_cache_ufid_to_flow_item *dp_flow_data_prev;
+    const ovs_u128 *ufid = &dp_flow_data->ufid;
+
+    hash = hash_bytes(ufid, sizeof *ufid, 0);
+
+    ovs_mutex_lock(&ufid_to_flow_map_mutex);
+
+    dp_flow_data_prev = e2e_cache_ufid_to_flow_data_find_protected(ufid, hash);
+    if (dp_flow_data_prev) {
+        e2e_cache_flow_db_del_protected(ufid, hash);
+    }
+
+    cmap_insert(&ufid_to_flow_map, &dp_flow_data->node.in_cmap, hash);
+    ovs_mutex_unlock(&ufid_to_flow_map_mutex);
 }
 
 static void
@@ -9051,6 +9192,7 @@ e2e_cache_set_action_is_valid(struct nlattr *a)
     return true;
 }
 
+OVS_UNUSED
 static inline bool
 e2e_cache_flows_are_valid(struct e2e_cache_ufid_to_flow_item **netdev_flows,
                           uint16_t num)
@@ -9228,6 +9370,7 @@ e2e_cache_attach_merged_set_action(struct ofpbuf *buf, size_t tnl_offset,
     ofpbuf_insert(buf, tnl_offset, tmpbuf.data, tmpbuf.size);
 }
 
+OVS_UNUSED
 static void
 e2e_cache_merge_actions(struct e2e_cache_ufid_to_flow_item **netdev_flows,
                         uint16_t num, struct ofpbuf *buf)
@@ -9336,6 +9479,7 @@ e2e_cache_shift_tnl_fields(struct match *merged_match)
                    sizeof src->wc.masks.field);                      \
         }
 
+OVS_UNUSED
 static void
 e2e_cache_merge_match(struct e2e_cache_ufid_to_flow_item **netdev_flows,
                       uint16_t num, struct match *merged_match)

commit 11f4f97474818d66814ead4c8c683e4fe9c74df3

dpif-netdev: Fix DB insertions
With each trace message we call hmap insert, we need to check
if the flows already exists before adding it, if exists check
the actions if there is a match skip it, if not then delete the
current flow and add new one.

Issue: 2241373
Fixes: ad4b4135bb ("dpif-netdev: e2e: Add flows database")
Change-Id: I97b1f71182154a9001c9ff47d81386b61f7aed34
Signed-off-by: Salem Sol <salems@mellanox.com>

lib/dpif-netdev.c
@@ -7781,7 +7781,6 @@ e2e_cache_flow_free(void *arg)
     free(dp_flow_data);
 }
 
-OVS_UNUSED
 static void
 e2e_cache_merged_flow_db_del(const ovs_u128 *ufid)
 {
@@ -7798,13 +7797,30 @@ e2e_cache_merged_flow_db_del(const ovs_u128 *ufid)
     e2e_cache_flow_free(dp_flow_data);
 }
 
-static inline void
+static inline int
 e2e_cache_merged_flow_db_put(struct e2e_cache_ufid_to_flow_item *merged_flow)
 {
     uint32_t hash =
         hash_bytes(&merged_flow->ufid, sizeof merged_flow->ufid, 0);
+    struct e2e_cache_ufid_to_flow_item *node;
+
+    node = e2e_cache_merged_flow_find(&merged_flow->ufid);
+    /* In case the merged flow exists do nothing. */
+    if (node && (node->actions_size == merged_flow->actions_size) &&
+       !memcmp(node->actions, merged_flow->actions,
+       sizeof *merged_flow->actions)) {
+           return -1;
+    }
+
+    /* In case it's a flow modification delete the current flow
+     * before inserting the updated one.
+     */
+    if (node) {
+        e2e_cache_merged_flow_db_del(&node->ufid);
+    }
 
     hmap_insert(&merged_flows_map, &merged_flow->node.in_hmap, hash);
+    return 0;
 }
 
 /* Find dp_flow_data with @ufid. */
@@ -8053,6 +8069,7 @@ e2e_cache_process_trace_info(const struct e2e_cache_trace_info *trc_info)
     struct ofpbuf merged_actions;
     struct e2e_cache_ufid_to_flow_item *mt_flows[E2E_CACHE_MAX_TRACE];
     uint64_t merged_actions_buf[1024 / sizeof(uint64_t)];
+    int err;
 
     merged_flow = xzalloc(sizeof *merged_flow);
     if (OVS_UNLIKELY(!merged_flow)) {
@@ -8083,12 +8100,12 @@ e2e_cache_process_trace_info(const struct e2e_cache_trace_info *trc_info)
     memcpy(merged_flow->actions, actions, actions_len);
     merged_flow->actions_size = actions_len;
 
-    e2e_cache_merged_flow_db_put(merged_flow);
-
+    err = e2e_cache_merged_flow_db_put(merged_flow);
+    if (OVS_UNLIKELY(err)) {
+        goto out;
+    }
     /* TODO: placeholder for dispatch merged flow to offload thread */
-
     return 0;
-
 out:
     if (merged_flow) {
         e2e_cache_flow_free(merged_flow);