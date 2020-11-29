Mellanox/OVS

commit 96ec4d2820967882f7ea7263d7e5135f7cb1d795

dpif-netdev: Call trace message from upcall
Issue: 2228604
Change-Id: Ib9a0142913b2053c89346910ab5daeb399af2e14
Fixes: a407a149cb1d ("dpif-netdev: e2e-cache tracer: collect all flow IDs")
Signed-off-by: Eli Britstein <elibr@mellanox.com>

lib/dpif-netdev.c
@@ -7811,6 +7811,9 @@ handle_packet_upcall(struct dp_netdev_pmd_thread *pmd,
         uint32_t hash = dp_netdev_flow_hash(&netdev_flow->ufid);
         smc_insert(pmd, key, hash);
         emc_probabilistic_insert(pmd, key, netdev_flow);
+        if (e2e_cache_enabled) {
+            e2e_cache_trace_add_flow(packet, &netdev_flow->mega_ufid);
+        }
     }
     if (pmd_perf_metrics_enabled(pmd)) {
         /* Update upcall stats. */

commit ca95b28f171b682a3878de6d61b33fa1a62b663e

dpif-netdev: e2e: Fix empty dispatch messages from upcall
When a packet is processed via upcall, its actions are first executed,
before flows are created and added to the trace of the packet. The
execution of the actions, ending up with a fate action invokes a
dispatch for the packet, but its trace is still empty. Change the order
of creation of the flow/adding it to the trace and the execution to fix
it.

Issue: 2260470
Fixes: 96ec4d282096 ("dpif-netdev: Call trace message from upcall")
Change-Id: Ia6698149ce73883db6893487aafd4dbdf34be65d
Signed-off-by: Eli Britstein <elibr@nvidia.com>
Reviewed-by: Michael Savisko <michaelsav@nvidia.com>

lib/dpif-netdev.c
@@ -8890,13 +8890,6 @@ handle_packet_upcall(struct dp_netdev_pmd_thread *pmd,
         match.wc.masks.vlans[0].tci = htons(0xffff);
     }
 
-    /* We can't allow the packet batching in the next loop to execute
-     * the actions.  Otherwise, if there are any slow path actions,
-     * we'll send the packet up twice. */
-    dp_packet_batch_init_packet(&b, packet);
-    dp_netdev_execute_actions(pmd, &b, true, &match.flow,
-                              actions->data, actions->size);
-
     add_actions = put_actions->size ? put_actions : actions;
     if (OVS_LIKELY(error != ENOSPC)) {
         struct dp_netdev_flow *netdev_flow;
@@ -8921,6 +8914,14 @@ handle_packet_upcall(struct dp_netdev_pmd_thread *pmd,
             e2e_cache_trace_add_flow(packet, &netdev_flow->mega_ufid);
         }
     }
+
+    /* We can't allow the packet batching in the next loop to execute
+     * the actions.  Otherwise, if there are any slow path actions,
+     * we'll send the packet up twice. */
+    dp_packet_batch_init_packet(&b, packet);
+    dp_netdev_execute_actions(pmd, &b, true, &match.flow,
+                              actions->data, actions->size);
+
     if (pmd_perf_metrics_enabled(pmd)) {
         /* Update upcall stats. */
         cycles = cycles_counter_update(&pmd->perf_stats) - cycles;