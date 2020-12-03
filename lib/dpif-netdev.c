58c9c2bd19eb9820bfeb8600f237e02515c53e81
Eli Britstein <elibr@nvidia.com>
2020-12-03 10:05:01 +0200

    dpif-netdev: Fix offloads of modified flows
    Flows offload may issue an offload request (add) with non-supported
    actions, and modified to supported actions, but before the first offload
    request is handled. As the flow has not been assigned with a flow mark
    yet, this will trigger another "add", instead of a "modify" request.
    Fix it by triggering the request type based on actual datapath flow
    rather than flow mark that depends on offload thread timings.
    
    Issue: 2336195
    Change-Id: Ifd15e0221537c72e1d2206e6bc6560b4ef5bd3bb
    Fixes: 02bb2824e51d ("dpif-netdev: do hw flow offload in a thread")
    Signed-off-by: Eli Britstein <elibr@nvidia.com>

--- lib/dpif-netdev.c
+++ lib/dpif-netdev.c
@@ -3514,7 +3514,8 @@ e2e_cache_flow_put(bool is_ct, const ovs_u128 *ufid, const void *match,
 static void
 queue_netdev_flow_put(struct dp_netdev_pmd_thread *pmd,
                       struct dp_netdev_flow *flow, struct match *match,
-                      const struct nlattr *actions, size_t actions_len)
+                      const struct nlattr *actions, size_t actions_len,
+                      bool modify)
 {
     struct dp_flow_offload_item *offload;
     int op;
@@ -3524,11 +3525,7 @@ queue_netdev_flow_put(struct dp_netdev_pmd_thread *pmd,
     }
 
     e2e_cache_flow_put(false, &flow->mega_ufid, match, actions, actions_len);
-    if (flow->mark != INVALID_FLOW_MARK) {
-        op = DP_NETDEV_FLOW_OFFLOAD_OP_MOD;
-    } else {
-        op = DP_NETDEV_FLOW_OFFLOAD_OP_ADD;
-    }
+    op = modify ? DP_NETDEV_FLOW_OFFLOAD_OP_MOD : DP_NETDEV_FLOW_OFFLOAD_OP_ADD;
     offload = dp_netdev_alloc_flow_offload(pmd, flow, op);
     offload->match = *match;
     offload->actions = xmalloc(actions_len);
@@ -4413,7 +4410,7 @@ dp_netdev_flow_add(struct dp_netdev_pmd_thread *pmd,
     cmap_insert(&pmd->flow_table, CONST_CAST(struct cmap_node *, &flow->node),
                 dp_netdev_flow_hash(&flow->ufid));
 
-    queue_netdev_flow_put(pmd, flow, match, actions, actions_len);
+    queue_netdev_flow_put(pmd, flow, match, actions, actions_len, false);
 
     if (OVS_UNLIKELY(!VLOG_DROP_DBG((&upcall_rl)))) {
         struct ds ds = DS_EMPTY_INITIALIZER;
@@ -4500,7 +4497,7 @@ flow_put_on_pmd(struct dp_netdev_pmd_thread *pmd,
             ovsrcu_set(&netdev_flow->actions, new_actions);
 
             queue_netdev_flow_put(pmd, netdev_flow, match,
-                                  put->actions, put->actions_len);
+                                  put->actions, put->actions_len, true);
 
             if (stats) {
                 get_dpif_flow_status(pmd->dp, netdev_flow, stats, NULL);
