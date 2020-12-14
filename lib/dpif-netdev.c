Mellanox/OVS

commit 3b2ed64ea7a4336bbb795c8a3aef337bd6d5bd1c

dpif-netdev: e2e: Flow put/del for flows
Issue e2e flow put/del calls from the datapath.

Issue: 2228604
Change-Id: Id4effe5be0f0702ac9fd400ecc030e73553559b4
Signed-off-by: Eli Britstein <elibr@mellanox.com>

lib/dpif-netdev.c
@@ -3401,11 +3401,15 @@ queue_netdev_flow_del(struct dp_netdev_pmd_thread *pmd,
         return;
     }
 
+    e2e_cache_flow_del(&flow->mega_ufid);
     offload = dp_netdev_alloc_flow_offload(pmd, flow,
                                            DP_NETDEV_FLOW_OFFLOAD_OP_DEL);
     dp_netdev_append_flow_offload(offload);
 }
 
+static int
+e2e_cache_flow_put(const ovs_u128 *ufid, struct match *match,
+                   const struct nlattr *actions, size_t actions_len);
 static void
 queue_netdev_flow_put(struct dp_netdev_pmd_thread *pmd,
                       struct dp_netdev_flow *flow, struct match *match,
@@ -3418,6 +3422,7 @@ queue_netdev_flow_put(struct dp_netdev_pmd_thread *pmd,
         return;
     }
 
+    e2e_cache_flow_put(&flow->mega_ufid, match, actions, actions_len);
     if (flow->mark != INVALID_FLOW_MARK) {
         op = DP_NETDEV_FLOW_OFFLOAD_OP_MOD;
     } else {

commit 47a0cc1cbd0add65e87d8b37c678b624f20bbab7

dpif-netdev: e2e: Do not enqueue ufid msg if disabled
E2E-cache UFID messages are sent on the queue even if the e2e-cache is
disabled. No thread is there to poll the queue, the message is lost and
memory is leaked.

Issue: 2228604
Change-Id: Id1e0e876a07ff9c5c4e58d6a117b4a2d2dc2f4c1
Fixes: 3b2ed64ea7a4 ("dpif-netdev: e2e: Flow put/del for flows")
Signed-off-by: Gaetan Rivet <grive@u256.net>

lib/dpif-netdev.c
@@ -3503,7 +3503,10 @@ queue_netdev_flow_del(struct dp_netdev_pmd_thread *pmd,
         return;
     }
 
-    e2e_cache_flow_del(&flow->mega_ufid, pmd->dp);
+    if (dp_netdev_e2e_cache_enabled) {
+        e2e_cache_flow_del(&flow->mega_ufid, pmd->dp);
+    }
+
     offload = dp_netdev_alloc_flow_offload(pmd, flow,
                                            DP_NETDEV_FLOW_OFFLOAD_OP_DEL);
     dp_netdev_append_flow_offload(offload);
@@ -3525,7 +3528,11 @@ queue_netdev_flow_put(struct dp_netdev_pmd_thread *pmd,
         return;
     }
 
-    e2e_cache_flow_put(false, &flow->mega_ufid, match, actions, actions_len);
+    if (dp_netdev_e2e_cache_enabled) {
+        e2e_cache_flow_put(false, &flow->mega_ufid, match,
+                           actions, actions_len);
+    }
+
     op = modify ? DP_NETDEV_FLOW_OFFLOAD_OP_MOD : DP_NETDEV_FLOW_OFFLOAD_OP_ADD;
     offload = dp_netdev_alloc_flow_offload(pmd, flow, op);
     offload->match = *match;