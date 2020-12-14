Mellanox/OVS

commit ec1da9aecf56a4de85a4cd5e6eafbcbe52abc2bf

netdev-offload-dpdk: Allocate CT IDs from seq-pool
Instead of calculation MT CT IDs before offload API pass
unique key value (pointer of conntrack connection object)
to the HW offloading layer. In the HW offloading layer
allocate a new resource - ct_id using seq-pool and
passed ct_id_key as a unique key for MT CT IDs allocation.

Issue: 2333202
Change-Id: I376cf16f60ebe66ba8491409554f036cf8e03f8a
Signed-off-by: Michael Savisko <michaelsav@nvidia.com>
Reviewed-by: Eli Britstein <elibr@nvidia.com>
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/netdev-offload-dpdk.c
@@ -66,6 +66,7 @@ struct act_resources {
     uint32_t ct_match_label_id;
     uint32_t ct_action_label_id;
     uint32_t ct_shared_age_id;
+    uint32_t ctid;
 };
 
 #define NUM_RTE_FLOWS_PER_PORT 2
@@ -1556,6 +1557,74 @@ put_shared_age_id(uint32_t shared_age_id)
     put_context_data_by_id(&shared_age_id_md, NULL, shared_age_id);
 }
 
+static struct ds *
+dump_counter_id(struct ds *s, void *data)
+{
+    uintptr_t ct_id_key = *(uintptr_t *) data;
+
+    ds_put_format(s, "CT id key = 0x%"PRIxPTR, ct_id_key);
+    return s;
+}
+
+#define MIN_COUNTER_ID       1U
+#define NUM_COUNTER_IDS      (UINT32_MAX - 2U)
+
+static struct seq_pool *counter_id_pool = NULL;
+
+static uint32_t
+counter_id_alloc(void *arg OVS_UNUSED)
+{
+    unsigned int tid = netdev_offload_thread_id();
+    uint32_t counter_id;
+
+    if (OVS_UNLIKELY(!counter_id_pool)) {
+        /* if not yet initialized, do it here */
+        counter_id_pool = seq_pool_create(netdev_offload_dpdk_thread_nb(),
+                                          MIN_COUNTER_ID, NUM_COUNTER_IDS);
+    }
+
+    if (OVS_LIKELY(seq_pool_new_id(counter_id_pool, tid, &counter_id))) {
+        return counter_id;
+    }
+    return 0;
+}
+
+static void
+counter_id_free(const void *arg OVS_UNUSED, uint32_t counter_id)
+{
+    unsigned int tid = netdev_offload_thread_id();
+
+    seq_pool_free_id(counter_id_pool, tid, counter_id);
+}
+
+static struct context_metadata counter_id_md = {
+    .name = "counter_id",
+    .dump_context_data = dump_counter_id,
+    .maps_lock = OVS_MUTEX_INITIALIZER,
+    .d2i_map = CMAP_INITIALIZER,
+    .i2d_map = CMAP_INITIALIZER,
+    .id_alloc = &counter_id_alloc,
+    .id_free = &counter_id_free,
+    .data_size = sizeof(uintptr_t),
+};
+
+static int
+get_ct_counter_id(uintptr_t ctid_key, uint32_t *ct_id)
+{
+    struct context_data ct_id_ctx = {
+        .data = &ctid_key,
+    };
+
+    return get_context_data_id_by_data(&counter_id_md, &ct_id_ctx, NULL,
+                                       ct_id);
+}
+
+static void
+put_ct_counter_id(uint32_t ct_id)
+{
+    put_context_data_by_id(&counter_id_md, NULL, ct_id);
+}
+
 static void
 put_action_resources(struct netdev *netdev,
                      struct act_resources *act_resources)
@@ -1578,6 +1647,7 @@ put_action_resources(struct netdev *netdev,
     put_label_id(act_resources->ct_match_label_id);
     put_label_id(act_resources->ct_action_label_id);
     put_shared_age_id(act_resources->ct_shared_age_id);
+    put_ct_counter_id(act_resources->ctid);
 }
 
 /*
@@ -2169,7 +2239,6 @@ struct act_vars {
     uint32_t recirc_id;
     struct flow_tnl *tnl_key;
     struct flow_tnl tnl_mask;
-    uint32_t ctid;
     struct rte_flow_action_jump *jump;
     bool is_e2e_cache_flow;
     uint32_t app_flows_counter;
@@ -3814,14 +3883,20 @@ parse_ct_actions(struct flow_actions *actions,
             act_vars->ct_mode = CT_MODE_CT_NAT;
         } else if (nl_attr_type(cta) == OVS_CT_ATTR_HELPER) {
             const char *helper = nl_attr_get(cta);
+            uintptr_t ctid_key;
 
             if (strncmp(helper, "offl", strlen("offl"))) {
                 continue;
             }
 
-            if (!ovs_scan(helper, "offl,st(0x%"SCNx8"),id(0x%"SCNx32")",
-                          &ct_miss_ctx.state, &act_vars->ctid)) {
+            if (!ovs_scan(helper, "offl,st(0x%"SCNx8"),id_key(0x%"SCNxPTR")",
+                          &ct_miss_ctx.state, &ctid_key)) {
                 VLOG_ERR("Invalid offload helper: '%s'", helper);
+                return -1;
+            }
+
+            if (get_ct_counter_id(ctid_key, &act_resources->ctid)) {
+                VLOG_ERR("Could not create CT id");
                 return -1;
             }
 
@@ -3900,7 +3975,8 @@ create_ct_conn(struct netdev *netdev,
                      &act_resources->ct_nat_table_id)) {
         return -1;
     }
-    split_ct_conn_actions(actions, &ct_actions, &nat_actions, act_vars->ctid);
+    split_ct_conn_actions(actions, &ct_actions, &nat_actions,
+                          act_resources->ctid);
     fi->has_count[0] = true;
     ret = create_offload_flow(netdev, act_resources->ct_nat_table_id, items,
                               nat_actions.actions, error,

lib/conntrack.c
@@ -291,6 +291,12 @@ ct_print_conn_info(const struct conn *c, const char *log_msg,
     }
 }
 
+static uintptr_t
+conntrack_offload_get_ctid_key(struct conn *conn)
+{
+    return (uintptr_t) (conn->master_conn ? conn->master_conn : conn);
+}
+
 static bool
 conntrack_offload_fill_item_common(struct ct_flow_offload_item *item,
                                    struct conn *conn,
@@ -299,6 +305,7 @@ conntrack_offload_fill_item_common(struct ct_flow_offload_item *item,
     item->ufid = conn->offloads.dir_info[dir].ufid;
     item->ct_match.odp_port = conn->offloads.dir_info[dir].port;
     item->dp = conn->offloads.dir_info[dir].dp;
+    item->ctid_key = conntrack_offload_get_ctid_key(conn);
     item->status = &conn->offloads.dir_info[dir].status;
 
     return dir == CT_DIR_REP
@@ -1457,17 +1464,6 @@ process_one(struct conntrack *ct, struct dp_packet *pkt,
 }
 
 static void
-conntrack_calc_ctid(const ovs_u128 *ufid_init,
-                    const ovs_u128 *ufid_rep,
-                    uint32_t *ctid)
-{
-    ovs_u128 ct_ufids[E2E_CACHE_MAX_TRACE] = {
-        [0] = ovs_u128_xor(*ufid_init, *ufid_rep), };
-
-    *ctid = hash_bytes(ct_ufids, sizeof ct_ufids, 0);
-}
-
-static void
 conntrack_swap_conn_key(const struct conn_key *key,
                         struct conn_key *swapped)
 {
@@ -1543,7 +1539,7 @@ e2e_cache_trace_add_ct(struct conntrack *ct,
                        uint32_t mark,
                        ovs_u128 label)
 {
-    struct ct_flow_offload_item item[CT_DIR_NUM];
+    struct ct_flow_offload_item item;
     uint32_t e2e_trace_size = p->e2e_trace_size;
     struct ct_dir_info *dir_info;
     uint8_t e2e_seen_pkts;
@@ -1562,8 +1558,8 @@ e2e_cache_trace_add_ct(struct conntrack *ct,
                conn->master_conn->offloads.dir_info[dir].dp) {
         conn = conn->master_conn;
     }
-    conntrack_offload_fill_item_add(&item[0], conn, dir, mark, label);
-    item[0].ct_match.odp_port = p->md.in_port.odp_port;
+    conntrack_offload_fill_item_add(&item, conn, dir, mark, label);
+    item.ct_match.odp_port = p->md.in_port.odp_port;
 
     dir_info = &conn->offloads.dir_info[dir];
     dir = ct_get_packet_dir(!reply);
@@ -1576,13 +1572,9 @@ e2e_cache_trace_add_ct(struct conntrack *ct,
     }
     if (!dir_info->e2e_flow) {
         dir_info->e2e_flow = true;
-        ct->offload_class->conn_get_ufid(&item[0], &dir_info->ufid);
-        item[0].ufid = dir_info->ufid;
-        /* Calc the opposite direction ufid, to shared counters. */
-        conntrack_offload_fill_item_add(&item[1], conn, dir, mark, label);
-        ct->offload_class->conn_get_ufid(&item[1], &item[1].ufid);
-        conntrack_calc_ctid(&dir_info->ufid, &item[1].ufid, &item[0].ctid);
-        ct->offload_class->conn_e2e_add(&item[0]);
+        ct->offload_class->conn_get_ufid(&item, &dir_info->ufid);
+        item.ufid = dir_info->ufid;
+        ct->offload_class->conn_e2e_add(&item);
     }
 
     /* Prevent sending E2E trace messages for every packet. Send only
@@ -1668,10 +1660,6 @@ conntrack_offload_add_conn(struct conntrack *ct,
                                          &item[CT_DIR_REP].ufid);
         *ufid[CT_DIR_INIT] = item[CT_DIR_INIT].ufid;
         *ufid[CT_DIR_REP] = item[CT_DIR_REP].ufid;
-        conntrack_calc_ctid(ufid[CT_DIR_INIT], ufid[CT_DIR_REP],
-                            &item[CT_DIR_INIT].ctid);
-        conntrack_calc_ctid(ufid[CT_DIR_INIT], ufid[CT_DIR_REP],
-                            &item[CT_DIR_REP].ctid);
         refcnt = xmalloc(sizeof *refcnt);
         ovs_refcount_init(refcnt);
         ovs_refcount_ref(refcnt);

lib/conntrack.h
@@ -127,7 +127,7 @@ struct ct_flow_offload_item {
     int  op;
     ovs_u128 ufid;
     void *dp;
-    uint32_t ctid;
+    uintptr_t ctid_key;
 
     /* matches */
     struct ct_match ct_match;

lib/dpif-netdev.c
@@ -3139,7 +3139,7 @@ dp_netdev_create_ct_actions(struct ofpbuf *buf,
                             struct ct_flow_offload_item *offload)
 {
     size_t offset;
-    char helper[] = "offl,st(0x  ),id(0x        )";
+    char helper[] = "offl,st(0x  ),id_key(0x                )";
     char s[9];
     char *end;
 
@@ -3239,8 +3239,9 @@ dp_netdev_create_ct_actions(struct ofpbuf *buf,
     end = helper;
     ovs_strcat(helper, sizeof helper, &end, "offl,st(0x");
     ovs_strcat(helper, sizeof helper, &end, u32_to_hex(s, offload->ct_state));
-    ovs_strcat(helper, sizeof helper, &end, "),id(0x");
-    ovs_strcat(helper, sizeof helper, &end, u32_to_hex(s, offload->ctid));
+    ovs_strcat(helper, sizeof helper, &end, "),id_key(0x");
+    ovs_strcat(helper, sizeof helper, &end, uintptr_to_hex(s,
+                                                           offload->ctid_key));
     ovs_strcat(helper, sizeof helper, &end, ")");
 
     nl_msg_put_string(buf, OVS_CT_ATTR_HELPER, helper);

lib/odp-util.c
@@ -941,7 +941,7 @@ static const struct nl_policy ovs_conntrack_policy[] = {
     [OVS_CT_ATTR_LABELS] = { .type = NL_A_UNSPEC, .optional = true,
                              .min_len = sizeof(struct ovs_key_ct_labels) * 2 },
     [OVS_CT_ATTR_HELPER] = { .type = NL_A_STRING, .optional = true,
-                             .min_len = 1, .max_len = 32 },
+                             .min_len = 1, .max_len = 64 },
     [OVS_CT_ATTR_NAT] = { .type = NL_A_UNSPEC, .optional = true },
     [OVS_CT_ATTR_TIMEOUT] = { .type = NL_A_STRING, .optional = true,
                               .min_len = 1, .max_len = 32 },

lib/util.c
@@ -989,6 +989,24 @@ u32_to_hex(char *buf, uint32_t x)
     return p;
 }
 
+/* Convert a uintptr_t into its hexadecimal string representation.
+ * Result is right-aligned in buf with no left-padding.
+ * Return value points to the start of the written string.
+ */
+char *
+uintptr_to_hex(char *buf, uintptr_t x)
+{
+    char *p = &buf[16];
+
+    *--p = '\0';
+    do {
+        *--p = "0123456789abcdef"[x & 0xf];
+        x >>= 4;
+    } while (x);
+
+    return p;
+}
+
 /* Returns the value of 'c' as a hexadecimal digit. */
 int
 hexit_value(unsigned char c)

lib/util.h
@@ -232,6 +232,7 @@ bool ovs_scan_len(const char *s, int *n, const char *format, ...);
 bool str_to_double(const char *, double *);
 
 char *u32_to_hex(char *, uint32_t);
+char *uintptr_to_hex(char *, uintptr_t);
 
 int hexit_value(unsigned char c);
 uintmax_t hexits_value(const char *s, size_t n, bool *ok);

commit dca2acc5b80b721e8fd1dd62594791552780baef

netdev-offload-dpdk: Fix counter pool allocation
The counter-id pool is potentially accessed from multiple threads, but
it is not protected against concurrent initialization.

Use an ovsthread_once to fix the concurrency issues.

Issue: 2333202
Change-Id: I7eae0524215e8ced8a20e4d035a43530fb040a8a
Fixes: ec1da9aecf56 ("netdev-offload-dpdk: Allocate CT IDs from
seq-pool")
Signed-off-by: Gaetan Rivet <grive@u256.net>

lib/netdev-offload-dpdk.c
@@ -1584,18 +1584,19 @@ dump_counter_id(struct ds *s, void *data)
 #define MIN_COUNTER_ID       1U
 #define NUM_COUNTER_IDS      (UINT32_MAX - 2U)
 
-static struct seq_pool *counter_id_pool = NULL;
+static struct seq_pool *counter_id_pool;
 
 static uint32_t
 counter_id_alloc(void *arg OVS_UNUSED)
 {
+    static struct ovsthread_once once = OVSTHREAD_ONCE_INITIALIZER;
     unsigned int tid = netdev_offload_thread_id();
     uint32_t counter_id;
 
-    if (OVS_UNLIKELY(!counter_id_pool)) {
-        /* if not yet initialized, do it here */
+    if (ovsthread_once_start(&once)) {
         counter_id_pool = seq_pool_create(netdev_offload_dpdk_thread_nb(),
                                           MIN_COUNTER_ID, NUM_COUNTER_IDS);
+        ovsthread_once_done(&once);
     }
 
     if (OVS_LIKELY(seq_pool_new_id(counter_id_pool, tid, &counter_id))) {