Mellanox/OVS

commit 2312174e19b86937707eec537a9d6d8a7c7cca8c

conntrack: Use rcu-lists to store conn expirations
Change the connection expiration lists from ovs_list to rculist.
This is not sufficient to allow moving the 'ct_lock' taking and
releasing but it is necessary as a pre-step towards reduced granularity
of 'ct_lock'.

Issue: 2322334
Change-Id: I94b47b4831bd9bae28ec50276b1962ef67fa7db0
Signed-off-by: Gaetan Rivet <grive@u256.net>

lib/conntrack-private.h
@@ -29,6 +29,7 @@
 #include "openvswitch/list.h"
 #include "openvswitch/types.h"
 #include "packets.h"
+#include "rculist.h"
 #include "unaligned.h"
 #include "dp-packet.h"
 
@@ -91,12 +92,17 @@ struct ct_offloads {
     struct ct_dir_info dir_info[CT_DIR_NUM];
 };
 
+struct conn_exp_node {
+    struct rculist node;
+    struct conn *up;
+};
+
 struct conn {
     /* Immutable data. */
     struct conn_key key;
     struct conn_key rev_key;
     struct conn_key master_key; /* Only used for orig_tuple support. */
-    struct ovs_list exp_node;
+    struct conn_exp_node *exp;
     struct cmap_node cm_node;
     struct nat_action_info_t *nat_info;
     char *alg;
@@ -162,7 +168,7 @@ enum ct_timeout {
 struct conntrack {
     struct ovs_mutex ct_lock; /* Protects 2 following fields. */
     struct cmap conns OVS_GUARDED;
-    struct ovs_list exp_lists[N_CT_TM] OVS_GUARDED;
+    struct rculist exp_lists[N_CT_TM] OVS_GUARDED;
     struct hmap zone_limits OVS_GUARDED;
     struct hmap timeout_policies OVS_GUARDED;
     uint32_t hash_basis; /* Salt for hashing a connection key. */

lib/conntrack-tp.c
@@ -231,6 +231,20 @@ tm_to_ct_dpif_tp(enum ct_timeout tm)
 }
 
 static void
+conn_init_expiration__(struct conntrack *ct, struct conn *conn,
+                       enum ct_timeout tm, long long now,
+                       uint32_t tp_value)
+{
+    conn->expiration = now + tp_value * 1000;
+    if (conn->exp != NULL) {
+        ovsrcu_postpone(free, conn->exp);
+    }
+    conn->exp = xmalloc(sizeof *conn->exp);
+    conn->exp->up = conn;
+    rculist_push_back(&ct->exp_lists[tm], &conn->exp->node);
+}
+
+static void
 conn_update_expiration__(struct conntrack *ct, struct conn *conn,
                          enum ct_timeout tm, long long now,
                          uint32_t tp_value)
@@ -241,9 +255,8 @@ conn_update_expiration__(struct conntrack *ct, struct conn *conn,
                 ct_timeout_str[tm], conn->key.zone, conn->tp_id, tp_value);
 
     if (!conn->cleaned) {
-        conn->expiration = now + tp_value * 1000;
-        ovs_list_remove(&conn->exp_node);
-        ovs_list_push_back(&ct->exp_lists[tm], &conn->exp_node);
+        rculist_remove(&conn->exp->node);
+        conn_init_expiration__(ct, conn, tm, now, tp_value);
     }
 }
 
@@ -289,15 +302,6 @@ conn_update_expiration(struct conntrack *ct, struct conn *conn,
     conn_protected_update_expiration(ct, conn, tm, now);
 
     ovs_mutex_unlock(&ct->ct_lock);
-}
-
-static void
-conn_init_expiration__(struct conntrack *ct, struct conn *conn,
-                       enum ct_timeout tm, long long now,
-                       uint32_t tp_value)
-{
-    conn->expiration = now + tp_value * 1000;
-    ovs_list_push_back(&ct->exp_lists[tm], &conn->exp_node);
 }
 
 /* ct_lock must be held. */

lib/conntrack.c
@@ -369,7 +369,7 @@ conntrack_init(void *dp)
     ovs_mutex_lock(&ct->ct_lock);
     cmap_init(&ct->conns);
     for (unsigned i = 0; i < ARRAY_SIZE(ct->exp_lists); i++) {
-        ovs_list_init(&ct->exp_lists[i]);
+        rculist_init(&ct->exp_lists[i]);
     }
     hmap_init(&ct->zone_limits);
     ct->zone_limit_seq = 0;
@@ -527,7 +527,7 @@ conn_clean(struct conntrack *ct, struct conn *conn)
         uint32_t hash = conn_key_hash(&conn->nat_conn->key, ct->hash_basis);
         cmap_remove(&ct->conns, &conn->nat_conn->cm_node, hash);
     }
-    ovs_list_remove(&conn->exp_node);
+    rculist_remove(&conn->exp->node);
     conn->cleaned = true;
     ovsrcu_postpone(delete_conn, conn);
     atomic_count_dec(&ct->n_conn);
@@ -539,7 +539,7 @@ conn_clean_one(struct conntrack *ct, struct conn *conn)
 {
     conn_clean_cmn(ct, conn);
     if (conn->conn_type == CT_CONN_TYPE_DEFAULT) {
-        ovs_list_remove(&conn->exp_node);
+        rculist_remove(&conn->exp->node);
         conn->cleaned = true;
         atomic_count_dec(&ct->n_conn);
     }
@@ -1137,8 +1137,8 @@ conn_not_found(struct conntrack *ct, struct dp_packet *pkt,
      * can limit DoS impact. */
 nat_res_exhaustion:
     free(nat_conn);
-    ovs_list_remove(&nc->exp_node);
-    delete_conn_cmn(nc);
+    rculist_remove(&nc->exp->node);
+    ovsrcu_postpone(delete_conn_cmn, nc);
     static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(5, 5);
     VLOG_WARN_RL(&rl, "Unable to NAT due to tuple space exhaustion - "
                  "if DoS attack, use firewalling and/or zone partitioning.");
@@ -1800,7 +1800,8 @@ set_label(struct dp_packet *pkt, struct conn *conn,
 static long long
 ct_sweep(struct conntrack *ct, long long now, size_t limit)
 {
-    struct conn *conn, *next;
+    struct conn *conn;
+    struct conn_exp_node *conn_it;
     long long min_expiration = LLONG_MAX;
     struct ct_flow_offload_item item;
     enum ct_timeout tm = 0;
@@ -1811,7 +1812,8 @@ ct_sweep(struct conntrack *ct, long long now, size_t limit)
     ovs_mutex_lock(&ct->ct_lock);
 
     for (unsigned i = 0; i < N_CT_TM; i++) {
-        LIST_FOR_EACH_SAFE (conn, next, exp_node, &ct->exp_lists[i]) {
+        RCULIST_FOR_EACH (conn_it, node, &ct->exp_lists[i]) {
+            conn = conn_it->up;
             ovs_mutex_lock(&conn->lock);
             hw_updated = false;
             for (dir = 0; ct->offload_class &&
@@ -2712,6 +2714,7 @@ new_conn(struct conntrack *ct, struct dp_packet *pkt, struct conn_key *key,
 static void
 delete_conn_cmn(struct conn *conn)
 {
+    free(conn->exp);
     free(conn->nat_info);
     free(conn->alg);
     free(conn);

commit 19872ff36a3e8e28b632562f1550546f05de1655

conntrack-tp: Do not reallocate expiration node
The rculist nodes require that after removal from a list, RCU is
synchronized at least once before being re-inserted.

To sidestep the issue, they were instead freed with a delay, and a new
one would be allocated, able to be used immediately.

This induce a great memory allocation pressure: many small structures
needs to be allocated, each time a connection change state.

Replace it by a regular intrusive node within the connection, that is
protected from multiple updates by atomic flags. Remove it only once
from the conn timeout queue, and schedule its re-insertion by
ovsrcu_postpone(), ensuring an RCU sync happens before any re-insertion.

Issue: 2440745
Fixes: 2312174e19b8 ("conntrack: Use rcu-lists to store conn expirations")
Change-Id: I152e2e9c57976aa1e3292571ed704ff6d6e30fce
Signed-off-by: Gaetan Rivet <grive@u256.net>

lib/conntrack-private.h
@@ -57,6 +57,29 @@ struct alg_exp_node {
     bool nat_rpl_dst;
 };
 
+/* Timeouts: all the possible timeout states passed to update_expiration()
+ * are listed here. The name will be prefix by CT_TM_ and the value is in
+ * milliseconds */
+#define CT_TIMEOUTS \
+    CT_TIMEOUT(TCP_FIRST_PACKET) \
+    CT_TIMEOUT(TCP_OPENING) \
+    CT_TIMEOUT(TCP_ESTABLISHED) \
+    CT_TIMEOUT(TCP_CLOSING) \
+    CT_TIMEOUT(TCP_FIN_WAIT) \
+    CT_TIMEOUT(TCP_CLOSED) \
+    CT_TIMEOUT(OTHER_FIRST) \
+    CT_TIMEOUT(OTHER_MULTIPLE) \
+    CT_TIMEOUT(OTHER_BIDIR) \
+    CT_TIMEOUT(ICMP_FIRST) \
+    CT_TIMEOUT(ICMP_REPLY)
+
+enum ct_timeout {
+#define CT_TIMEOUT(NAME) CT_TM_##NAME,
+    CT_TIMEOUTS
+#undef CT_TIMEOUT
+    N_CT_TM
+};
+
 enum OVS_PACKED_ENUM ct_conn_type {
     CT_CONN_TYPE_DEFAULT,
     CT_CONN_TYPE_UN_NAT,
@@ -92,9 +115,20 @@ struct ct_offloads {
     struct ct_dir_info dir_info[CT_DIR_NUM];
 };
 
-struct conn_exp_node {
+struct conn_expire {
+    /* Set once when initializing the expiration node. */
+    struct conntrack *ct;
+    /* Timeout state of the connection.
+     * It follows the connection state updates.
+     */
+    enum ct_timeout tm;
+    /* Insert and remove the expiration node only once per RCU syncs.
+     * If multiple threads update the connection, its expiration should
+     * be removed only once and added only once to timeout lists.
+     */
+    atomic_flag insert_once;
+    atomic_flag remove_once;
     struct rculist node;
-    struct conn *up;
 };
 
 struct conn {
@@ -102,7 +136,6 @@ struct conn {
     struct conn_key key;
     struct conn_key rev_key;
     struct conn_key master_key; /* Only used for orig_tuple support. */
-    struct conn_exp_node *exp;
     struct cmap_node cm_node;
     struct nat_action_info_t *nat_info;
     char *alg;
@@ -115,6 +148,7 @@ struct conn {
 
     /* Mutable data. */
     struct ovs_mutex lock; /* Guards all mutable fields. */
+    struct conn_expire exp;
     ovs_u128 label;
     atomic_llong expiration;
     long long prev_query;
@@ -143,29 +177,6 @@ enum ct_update_res {
     CT_UPDATE_VALID,
     CT_UPDATE_NEW,
     CT_UPDATE_VALID_NEW,
-};
-
-/* Timeouts: all the possible timeout states passed to update_expiration()
- * are listed here. The name will be prefix by CT_TM_ and the value is in
- * milliseconds */
-#define CT_TIMEOUTS \
-    CT_TIMEOUT(TCP_FIRST_PACKET) \
-    CT_TIMEOUT(TCP_OPENING) \
-    CT_TIMEOUT(TCP_ESTABLISHED) \
-    CT_TIMEOUT(TCP_CLOSING) \
-    CT_TIMEOUT(TCP_FIN_WAIT) \
-    CT_TIMEOUT(TCP_CLOSED) \
-    CT_TIMEOUT(OTHER_FIRST) \
-    CT_TIMEOUT(OTHER_MULTIPLE) \
-    CT_TIMEOUT(OTHER_BIDIR) \
-    CT_TIMEOUT(ICMP_FIRST) \
-    CT_TIMEOUT(ICMP_REPLY)
-
-enum ct_timeout {
-#define CT_TIMEOUT(NAME) CT_TM_##NAME,
-    CT_TIMEOUTS
-#undef CT_TIMEOUT
-    N_CT_TM
 };
 
 struct conntrack {
@@ -232,6 +243,22 @@ tcp_payload_length(struct dp_packet *pkt)
                 - tcp_payload);
     } else {
         return 0;
+    }
+}
+
+static inline void
+conn_expire_remove(struct conn_expire *exp, bool need_ct_lock)
+    OVS_NO_THREAD_SAFETY_ANALYSIS
+{
+    if (!atomic_flag_test_and_set(&exp->remove_once)
+        && rculist_next(&exp->node)) {
+        if (need_ct_lock) {
+            ovs_mutex_lock(&exp->ct->ct_lock);
+        }
+        rculist_remove(&exp->node);
+        if (need_ct_lock) {
+            ovs_mutex_unlock(&exp->ct->ct_lock);
+        }
     }
 }
 

lib/conntrack-tp.c
@@ -237,18 +237,52 @@ tm_to_ct_dpif_tp(enum ct_timeout tm)
 }
 
 static void
-conn_init_expiration__(struct conntrack *ct, struct conn *conn,
-                       enum ct_timeout tm, long long now,
-                       uint32_t tp_value)
+conn_expire_init(struct conn *conn, struct conntrack *ct)
 {
-    atomic_store_relaxed(&conn->expiration, now + tp_value * 1000);
-    if (conn->exp != NULL) {
-        ovsrcu_postpone(free, conn->exp);
+    struct conn_expire *exp = &conn->exp;
+
+    if (exp->ct != NULL) {
+        return;
     }
-    conn->exp = xmalloc(sizeof *conn->exp);
-    conn->exp->up = conn;
-    rculist_push_back(&ct->exp_lists[tm], &conn->exp->node);
+
+    exp->ct = ct;
+    atomic_flag_clear(&exp->insert_once);
+    atomic_flag_clear(&exp->remove_once);
+    /* The expiration is initially unscheduled, flag it as 'removed'. */
+    atomic_flag_test_and_set(&exp->remove_once);
 }
+
+static void
+conn_expire_insert(struct conn *conn)
+{
+    struct conn_expire *exp = &conn->exp;
+
+    ovs_mutex_lock(&conn->lock);
+
+    if (!conn->cleaned) {
+        ovs_mutex_lock(&exp->ct->ct_lock);
+        rculist_push_back(&exp->ct->exp_lists[exp->tm], &exp->node);
+        ovs_mutex_unlock(&exp->ct->ct_lock);
+
+        atomic_flag_clear(&exp->insert_once);
+        atomic_flag_clear(&exp->remove_once);
+    }
+
+    ovs_mutex_unlock(&conn->lock);
+}
+
+static void
+conn_schedule_expiration(struct conntrack *ct, struct conn *conn,
+                         enum ct_timeout tm, long long now, uint32_t tp_value)
+{
+    conn_expire_init(conn, ct);
+    conn->expiration = now + tp_value * 1000;
+    conn->exp.tm = tm;
+    if (!atomic_flag_test_and_set(&conn->exp.insert_once)) {
+        ovsrcu_postpone(conn_expire_insert, conn);
+    }
+}
+
 
 static void
 conn_update_expiration__(struct conntrack *ct, struct conn *conn,
@@ -261,12 +295,8 @@ conn_update_expiration__(struct conntrack *ct, struct conn *conn,
                 ct_timeout_str[tm], conn->key.zone, conn->tp_id, tp_value);
 
     if (!conn->cleaned) {
-        ovs_mutex_lock(&ct->ct_lock);
-
-        rculist_remove(&conn->exp->node);
-        conn_init_expiration__(ct, conn, tm, now, tp_value);
-
-        ovs_mutex_unlock(&ct->ct_lock);
+        conn_expire_remove(&conn->exp, true);
+        conn_schedule_expiration(ct, conn, tm, now, tp_value);
     }
 }
 
@@ -315,7 +345,5 @@ conn_init_expiration(struct conntrack *ct, struct conn *conn,
     VLOG_DBG_RL(&rl, "Init timeout %s zone=%u with policy id=%d val=%u sec.",
                 ct_timeout_str[tm], conn->key.zone, conn->tp_id, val);
 
-    ovs_mutex_lock(&ct->ct_lock);
-    conn_init_expiration__(ct, conn, tm, now, val);
-    ovs_mutex_unlock(&ct->ct_lock);
+    conn_schedule_expiration(ct, conn, tm, now, val);
 }

lib/conntrack.c
@@ -566,7 +566,7 @@ conn_clean(struct conntrack *ct, struct conn *conn)
         uint32_t hash = conn_key_hash(&conn->nat_conn->key, ct->hash_basis);
         cmap_remove(&ct->conns, &conn->nat_conn->cm_node, hash);
     }
-    rculist_remove(&conn->exp->node);
+    conn_expire_remove(&conn->exp, false);
     conn->cleaned = true;
     ovsrcu_postpone(delete_conn, conn);
     atomic_count_dec(&ct->n_conn);
@@ -588,7 +588,7 @@ conn_clean_one(struct conntrack *ct, struct conn *conn)
 
     conn_clean_cmn(ct, conn);
     if (conn->conn_type == CT_CONN_TYPE_DEFAULT) {
-        rculist_remove(&conn->exp->node);
+        conn_expire_remove(&conn->exp, false);
         conn->cleaned = true;
         atomic_count_dec(&ct->n_conn);
     }
@@ -1213,7 +1213,7 @@ conn_not_found(struct conntrack *ct, struct dp_packet *pkt,
      * can limit DoS impact. */
 nat_res_exhaustion:
     free(nat_conn);
-    rculist_remove(&nc->exp->node);
+    conn_expire_remove(&nc->exp, false);
     ovsrcu_postpone(delete_conn_cmn, nc);
     static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(5, 5);
     VLOG_WARN_RL(&rl, "Unable to NAT due to tuple space exhaustion - "
@@ -1949,7 +1949,6 @@ ct_sweep(struct conntrack *ct, long long now, size_t limit)
 {
     struct conntrack_offload_class *offload_class = NULL;
     struct conn *conn;
-    struct conn_exp_node *conn_it;
     struct conn *conn_batch[CT_SWEEP_BATCH_SIZE];
     size_t batch_count = 0;
     long long min_expiration = LLONG_MAX;
@@ -1978,10 +1977,9 @@ rcu_quiesce:
                                        &ct->offload_class);
         }
 
-        RCULIST_FOR_EACH (conn_it, node, &ct->exp_lists[i]) {
+        RCULIST_FOR_EACH (conn, exp.node, &ct->exp_lists[i]) {
             bool next_list = false;
 
-            conn = conn_it->up;
             ovs_mutex_lock(&conn->lock);
             rv_active = 0;
             hw_updated = conn_hw_update(ct, offload_class, conn, now,
@@ -2899,7 +2897,6 @@ new_conn(struct conntrack *ct, struct dp_packet *pkt, struct conn_key *key,
 static void
 delete_conn_cmn(struct conn *conn)
 {
-    free(conn->exp);
     free(conn->nat_info);
     free(conn->alg);
     free(conn);