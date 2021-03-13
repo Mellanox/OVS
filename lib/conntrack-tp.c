Mellanox/OVS

commit b29bcfa4c9d7347b83343f841cd7b2e57f53e309

conntrack: Use an atomic conn expiration value
A lock is taken during conn_lookup() to check whether a connection is
expired before returning it. This lock can have some contention.

Even though this lock ensures the sequence of writes will result in a
consistent result, it does not enforce a specific order, in particular
regarding reads. A ct_clean thread taking the lock first could read a
value that would be updated immediately after by a PMD waiting on the
same lock, just as well as the inverse order.

As such, the expiration time can be stale anytime it is read. In this
context, using an atomic will ensure the same write consistency while
allowing the same level of guarantees for reads. It will however avoid
having threads waiting on the lock.

Issue: 2322334
Change-Id: Iaba7d8e894fb4eeccd3888ae3d68d1b9257ce48e
Signed-off-by: Gaetan Rivet <grive@u256.net>

lib/conntrack-tp.c
@@ -241,7 +241,7 @@ conn_init_expiration__(struct conntrack *ct, struct conn *conn,
                        enum ct_timeout tm, long long now,
                        uint32_t tp_value)
 {
-    conn->expiration = now + tp_value * 1000;
+    atomic_store_relaxed(&conn->expiration, now + tp_value * 1000);
     if (conn->exp != NULL) {
         ovsrcu_postpone(free, conn->exp);
     }

lib/conntrack-private.h
@@ -116,7 +116,7 @@ struct conn {
     /* Mutable data. */
     struct ovs_mutex lock; /* Guards all mutable fields. */
     ovs_u128 label;
-    long long expiration;
+    atomic_llong expiration;
     long long prev_query;
     uint32_t mark;
     int seq_skew;

lib/conntrack.c
@@ -102,6 +102,7 @@ static enum ct_update_res conn_update(struct conntrack *ct, struct conn *conn,
                                       struct dp_packet *pkt,
                                       struct conn_lookup_ctx *ctx,
                                       long long now);
+static long long int conn_expiration(const struct conn *);
 static bool conn_expired(struct conn *, long long now);
 static void set_mark(struct dp_packet *, struct conn *,
                      uint32_t val, uint32_t mask);
@@ -1923,7 +1924,7 @@ rcu_quiesce:
             conn->prev_query = now;
 
             if (!hw_updated) {
-                if (now < conn->expiration || count >= limit) {
+                if (!conn_expired(conn, now) || count >= limit) {
                     min_expiration = MIN(min_expiration, conn->expiration);
                     next_list = true;
                 } else {
@@ -2786,14 +2787,20 @@ conn_update(struct conntrack *ct, struct conn *conn, struct dp_packet *pkt,
     return update_res;
 }
 
+static long long int
+conn_expiration(const struct conn *conn)
+{
+    long long int expiration;
+
+    atomic_read_relaxed(&conn->expiration, &expiration);
+    return expiration;
+}
+
 static bool
 conn_expired(struct conn *conn, long long now)
 {
     if (conn->conn_type == CT_CONN_TYPE_DEFAULT) {
-        ovs_mutex_lock(&conn->lock);
-        bool expired = now >= conn->expiration ? true : false;
-        ovs_mutex_unlock(&conn->lock);
-        return expired;
+        return now >= conn_expiration(conn);
     }
     return false;
 }
@@ -2937,7 +2944,7 @@ conn_to_ct_dpif_entry(const struct conn *conn, struct ct_dpif_entry *entry,
     entry->mark = conn->mark;
     memcpy(&entry->labels, &conn->label, sizeof entry->labels);
 
-    long long expiration = conn->expiration - now;
+    long long expiration = conn_expiration(conn) - now;
 
     struct ct_l4_proto *class = l4_protos[conn->key.nw_proto];
     if (class->conn_get_protoinfo) {

commit 99d37fcd7a5e49d15b9bddc44aeb7e88dacc625c

conntrack-tp: Use atomic store to write conn expiration
Field was made atomic some time ago, but writes were not updated.

Issue: 2440745
Change-Id: I095802563a8519303a2e4da67eb78d094524cfd0
Fixes: b29bcfa4c9d7 ("conntrack: Use an atomic conn expiration value")
Signed-off-by: Gaetan Rivet <grive@u256.net>

lib/conntrack-tp.c
@@ -276,7 +276,7 @@ conn_schedule_expiration(struct conntrack *ct, struct conn *conn,
                          enum ct_timeout tm, long long now, uint32_t tp_value)
 {
     conn_expire_init(conn, ct);
-    conn->expiration = now + tp_value * 1000;
+    atomic_store_relaxed(&conn->expiration, now + tp_value * 1000);
     conn->exp.tm = tm;
     if (!atomic_flag_test_and_set(&conn->exp.insert_once)) {
         ovsrcu_postpone(conn_expire_insert, conn);