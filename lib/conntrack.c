Mellanox/OVS

commit 487172d85ec8aeba37a24251d0529a0ba99fb747

conntrack: Clean conn as soon as they are seen expired
When the datapath encounters an expired connection, clean it
immediately. This avoids lingering connections throwing off the offload
queue, as they share the safe flow ufid and will use the same nodes in
netdev-offload layer.

Cleaning the connection and inserting a new one is necessary for
stateful connections.

Issue: 2373196
Change-Id: Ic25c9d9257ec38c88eebc6ff19243431d0d7f665
Signed-off-by: Gaetan Rivet <grive@u256.net>

lib/conntrack.c
@@ -660,6 +660,17 @@ conn_key_lookup(struct conntrack *ct, const struct conn_key *key,
         }
 
         if (conn_expired(conn, now)) {
+            /* If the ct_sweep was not yet able to cleanup the connection,
+             * but it is expired, cleaning it immediately will avoid
+             * having a second insertion with the same key / flow from
+             * the datapath.
+             *
+             * This can happen when a connection was created at T0, then
+             * a new packet in the same flow is received at T1, and delta
+             * T1 - T0 as well as current ct_sweep expiration backlog is
+             * over the connection timeout.
+             */
+            conn_clean(ct, conn);
             break;
         }
 

commit cdcfd4abed3d6036110252723649ca51aa1aef0c

conntrack: Fix NAT conn lookup expiration
Connections that are expired are immediately cleaned by the thread
looking it up, in order to reduce the amount of lingering connections
and double-insertion from PMDs.

NAT connections are inserted in the same CMAP however, and will match
(either from the key or its reverse). They should never be cleaned by
calling conn_clean() however.

Instead, call the function on its master conn.

Issue: 2448725
Change-Id: If1733367848fb46fe468fc8f17ab25610420c31c
Fixes: 487172d85ec8 ("conntrack: Clean conn as soon as they are seen expired")
Signed-off-by: Gaetan Rivet <grive@u256.net>

lib/conntrack.c
@@ -665,7 +665,11 @@ conn_key_lookup(struct conntrack *ct, const struct conn_key *key,
              * T1 - T0 as well as current ct_sweep expiration backlog is
              * over the connection timeout.
              */
-            conn_clean(ct, conn);
+            if (conn->conn_type == CT_CONN_TYPE_DEFAULT) {
+                conn_clean(ct, conn);
+            } else {
+                conn_clean(ct, conn->master_conn);
+            }
             break;
         }
 