Mellanox/OVS

commit a16fdbfab04ce8700ff25332830161e2a4b256f6

netdev-offload-dpdk: Fix assert on miss flow race condition
Miss flows are added for tnl_pop / recirc / e2e if needed. If such a miss
flow already exists, it is not created again. However, in a multi-thread
environment, it is possible that another thread creates such miss flow
after the querying of the current thread that did not find any, so
associating it will trigger an assert. For this reason, allow such
scenario, and rollback the newly redundant HW flow for this case.

Issue: 2494817
Change-Id: I60003bae00d4326ec777d1579029ae5b756929c8
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/netdev-offload-dpdk.c
@@ -290,7 +290,8 @@ ufid_to_rte_flow_data_find_protected(struct netdev *netdev,
 static inline struct ufid_to_rte_flow_data *
 ufid_to_rte_flow_associate(struct netdev *netdev, const ovs_u128 *ufid,
                            struct flows_handle *flows, bool actions_offloaded,
-                           struct act_resources *act_resources)
+                           struct act_resources *act_resources,
+                           bool abort_on_dup)
 {
     size_t hash = hash_bytes(ufid, sizeof *ufid, 0);
     struct cmap *map = offload_data_map(netdev);
@@ -301,7 +302,6 @@ ufid_to_rte_flow_associate(struct netdev *netdev, const ovs_u128 *ufid,
         return NULL;
     }
 
-    data = xzalloc(sizeof *data);
     offload_data_lock(netdev);
 
     /*
@@ -309,12 +309,18 @@ ufid_to_rte_flow_associate(struct netdev *netdev, const ovs_u128 *ufid,
      * We should have deleted it first before re-adding it.
      * Thus, if following assert triggers, something is wrong:
      * the rte_flow is not destroyed.
+     * However, in a multh-thread environment, miss rules might be added by
+     * another thread while not in protected mode, so this is possible. For
+     * this, allow it.
      */
     data_prev = ufid_to_rte_flow_data_find_protected(netdev, ufid);
     if (data_prev && !data_prev->dead) {
-        ovs_assert(data_prev->flows.cnt == 0);
+        ovs_assert(!(abort_on_dup && data_prev->flows.cnt));
+        offload_data_unlock(netdev);
+        return NULL;
     }
 
+    data = xzalloc(sizeof *data);
     data->ufid = *ufid;
     data->actions_offloaded = actions_offloaded;
     memcpy(&data->flows, flows, sizeof data->flows);
@@ -2506,6 +2512,11 @@ create_rte_flow(struct netdev *netdev,
 }
 
 static int
+netdev_offload_dpdk_destroy_flow(struct netdev *netdev,
+                                 struct rte_flow *rte_flow,
+                                 unsigned int creation_tid,
+                                 const ovs_u128 *ufid);
+static int
 add_e2e_miss_flow(struct netdev *netdev,
                   const char *devargs,
                   uint32_t e2e_table_id,
@@ -2551,7 +2562,12 @@ add_e2e_miss_flow(struct netdev *netdev,
                 netdev_get_name(netdev), flow_item.rte_flow[0],
                 UUID_ARGS((struct uuid *) &ufid));
     add_flow_item(&flows, &flow_item);
-    ufid_to_rte_flow_associate(netdev, &ufid, &flows, true, &act_resources);
+    if (!ufid_to_rte_flow_associate(netdev, &ufid, &flows, true,
+                                    &act_resources, false)) {
+        netdev_offload_dpdk_destroy_flow(netdev, flow_item.rte_flow[0],
+                                         netdev_offload_thread_id(), &ufid);
+        free_flow_handle(&flows);
+    }
     return 0;
 }
 
@@ -4272,7 +4288,12 @@ add_miss_flow(struct netdev *netdev,
                 netdev_get_name(netdev), flow_item.rte_flow[0],
                 UUID_ARGS((struct uuid *) &ufid));
     add_flow_item(&flows, &flow_item);
-    ufid_to_rte_flow_associate(netdev, &ufid, &flows, true, &act_resources);
+    if (!ufid_to_rte_flow_associate(netdev, &ufid, &flows, true,
+                                    &act_resources, false)) {
+        netdev_offload_dpdk_destroy_flow(netdev, flow_item.rte_flow[0],
+                                         netdev_offload_thread_id(), &ufid);
+        free_flow_handle(&flows);
+    }
     return 0;
 }
 
@@ -5005,7 +5026,8 @@ netdev_offload_dpdk_add_flow(struct netdev *netdev,
         goto out;
     }
     flows_data = ufid_to_rte_flow_associate(netdev, ufid, &flows,
-                                            actions_offloaded, &act_resources);
+                                            actions_offloaded, &act_resources,
+                                            true);
 
 out:
     if (ret) {

commit 5d56975ed2eb1a541eedcdc4a19ce31903bb16a1

netdev-offload-dpdk: Fix freeing flow handle in case of miss race
There is a race condition with inserting miss rules between threads.
That was addressed in [1]. However, in the handling, the flow handle is
freed, which is done in a postponed manner, leading to potential
use-after-free (flows in on the function's stack).
Fix it by forcing immediate freeing for that case.

[1]: a16fdbfab04c ("netdev-offload-dpdk: Fix assert on miss flow race condition")

Issue: 2576604
Change-Id: I77242b99d1364527de16b770b7ca75c4eb856512
Fixes: a16fdbfab04c ("netdev-offload-dpdk: Fix assert on miss flow race condition")
Signed-off-by: Eli Britstein <elibr@nvidia.com>

lib/netdev-offload-dpdk.c
@@ -98,7 +98,7 @@ flows_handle_unref(struct flows_handle *flows)
 
 static void put_table_id(const char *devargs, uint32_t table_id);
 static void
-free_flow_handle(struct flows_handle *flows)
+free_flow_handle(struct flows_handle *flows, bool postpone_unref)
 {
     int i;
 
@@ -113,7 +113,11 @@ free_flow_handle(struct flows_handle *flows)
             free(CONST_CAST(void *, fi->devargs));
         }
     }
-    ovsrcu_postpone(flows_handle_unref, flows);
+    if (postpone_unref) {
+        ovsrcu_postpone(flows_handle_unref, flows);
+    } else {
+        flows_handle_unref(flows);
+    }
 }
 
 static void
@@ -2545,7 +2549,7 @@ add_e2e_miss_flow(struct netdev *netdev,
                                     &act_resources, false)) {
         netdev_offload_dpdk_destroy_flow(netdev, flow_item.rte_flow[0],
                                          netdev_offload_thread_id(), &ufid);
-        free_flow_handle(&flows);
+        free_flow_handle(&flows, false);
     }
     return 0;
 }
@@ -4271,7 +4275,7 @@ add_miss_flow(struct netdev *netdev,
                                     &act_resources, false)) {
         netdev_offload_dpdk_destroy_flow(netdev, flow_item.rte_flow[0],
                                          netdev_offload_thread_id(), &ufid);
-        free_flow_handle(&flows);
+        free_flow_handle(&flows, false);
     }
     return 0;
 }
@@ -5077,7 +5081,7 @@ netdev_offload_dpdk_remove_flows(struct netdev *netdev,
     }
     ret = 0;
 out:
-    free_flow_handle(flows);
+    free_flow_handle(flows, true);
     ovs_mutex_unlock(&data->lock);
     if (!ret) {
         put_action_resources(netdev, &data->act_resources);