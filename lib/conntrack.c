Mellanox/OVS

commit f889e1c635feae9d26977dbe3e0cfefe27233e79

conntrack: Avoid querying during high lock contention
Querying HW stats requires locking on port configuration. During high
contention periods on that lock, avoid querying CT connections.

Issue: 2399419
Change-Id: Id8743d7481865b04dac2cc57a5bd1bfdbdb690c8
Signed-off-by: Eli Britstein <elibr@nvidia.com>
Signed-off-by: Gaetan Rivet <grive@u256.net>

lib/conntrack.c
@@ -1877,6 +1877,7 @@ conn_batch_clean(struct conntrack *ct,
 
 #define CT_SWEEP_BATCH_SIZE 32
 #define CT_SWEEP_QUIESCE_INTERVAL_MS 10
+#define CT_SWEEP_TIMEOUT_MS 1000
 
 /* Delete the expired connections from 'ctb', up to 'limit'. Returns the
  * earliest expiration time among the remaining connections in 'ctb'.  Returns
@@ -1896,6 +1897,7 @@ ct_sweep(struct conntrack *ct, long long now, size_t limit)
     enum ct_timeout tm = 0;
     size_t count = 0;
     bool hw_updated;
+    int rv_active;
     int dir;
 
     next_rcu_quiesce = now + CT_SWEEP_QUIESCE_INTERVAL_MS;
@@ -1905,6 +1907,8 @@ ct_sweep(struct conntrack *ct, long long now, size_t limit)
          * no reference should be held. */
         if (now >= next_rcu_quiesce) {
 rcu_quiesce:
+            /* Do not delay further releasing batched conns if any. */
+            conn_batch_clean(ct, conn_batch, &batch_count);
             ovsrcu_quiesce();
             now = time_msec();
             next_rcu_quiesce = now + CT_SWEEP_QUIESCE_INTERVAL_MS;
@@ -1916,27 +1920,50 @@ rcu_quiesce:
             conn = conn_it->up;
             ovs_mutex_lock(&conn->lock);
             hw_updated = false;
+            rv_active = 0;
             for (dir = 0; ct->offload_class &&
                           ct->offload_class->conn_active && dir < CT_DIR_NUM;
                  dir++) {
                 if (!hw_updated && !conn_get_tm(conn, &tm) &&
-                    conntrack_offload_fill_item_common(&item, conn, dir) &&
-                    ct->offload_class->conn_active(&item, now,
-                                                   conn->prev_query)) {
-                    conn_update_expiration(ct, conn, tm, now);
-                    hw_updated = true;
+                    conntrack_offload_fill_item_common(&item, conn, dir)) {
+                    rv_active =
+                        ct->offload_class->conn_active(&item, now,
+                                                       conn->prev_query);
+                    if (!rv_active) {
+                        conn_update_expiration(ct, conn, tm, now);
+                        hw_updated = true;
+                        break;
+                    }
                 }
                 if (!hw_updated && conn->nat_conn &&
                     !conn_get_tm(conn->nat_conn, &tm) &&
                     conntrack_offload_fill_item_common(&item, conn->nat_conn,
-                                                       dir) &&
-                    ct->offload_class->conn_active(&item, now,
-                                                   conn->prev_query)) {
-                    conn_update_expiration(ct, conn, tm, now);
-                    hw_updated = true;
+                                                       dir)) {
+                    rv_active =
+                        ct->offload_class->conn_active(&item, now,
+                                                       conn->prev_query);
+                    if (!rv_active) {
+                        conn_update_expiration(ct, conn, tm, now);
+                        hw_updated = true;
+                        break;
+                    }
                 }
             }
             conn->prev_query = now;
+
+            if (rv_active == EAGAIN) {
+                /* Impossible to query offload status, try later. */
+                ovs_mutex_unlock(&conn->lock);
+                if (now - start > CT_SWEEP_TIMEOUT_MS) {
+                    /* The hardware might be unavailable for status
+                     * update. Do not repeat RCU quiescing endlessly,
+                     * return to poll block if the sweep timeout is triggered.
+                     */
+                    goto out;
+                } else {
+                    goto rcu_quiesce;
+                }
+            }
 
             if (!hw_updated) {
                 if (!conn_expired(conn, now) || count >= limit) {
@@ -1956,8 +1983,6 @@ rcu_quiesce:
 
             /* Attempt quiescing at fixed interval. */
             if (time_msec() >= next_rcu_quiesce) {
-                /* Do not delay further releasing batched conns if any. */
-                conn_batch_clean(ct, conn_batch, &batch_count);
                 /* Restarting rculist iteration after quiescing should be fine,
                  * we are always reading the list front.
                  */

lib/conntrack.h
@@ -160,8 +160,8 @@ struct conntrack_offload_class {
     void (*conn_get_ufid)(struct ct_flow_offload_item *, ovs_u128 *);
     void (*conn_add)(struct ct_flow_offload_item *);
     void (*conn_del)(struct ct_flow_offload_item *);
-    bool (*conn_active)(struct ct_flow_offload_item *, long long now,
-                        long long prev_now);
+    int (*conn_active)(struct ct_flow_offload_item *, long long now,
+                       long long prev_now);
     void (*conn_e2e_add)(struct ct_flow_offload_item *);
     void (*conn_e2e_del)(ovs_u128 *, void *dp);
 };

lib/dpif-netdev.c
@@ -1204,35 +1204,37 @@ dp_netdev_ct_offload_e2e_add(struct ct_flow_offload_item *offload)
     dp_netdev_ct_add(offload, NULL, dp_netdev_ct_e2e_add_cb);
 }
 
-static bool
+static int
 dpif_netdev_get_flow_offload_status(const struct dp_netdev *dp,
                                     struct dp_netdev_flow *netdev_flow,
                                     struct dpif_flow_stats *stats,
                                     struct dpif_flow_attrs *attrs,
                                     long long now,
                                     long long prev_now);
-static bool
+static int
 dp_netdev_ct_offload_active(struct ct_flow_offload_item *offload,
                             long long now, long long prev_now)
 {
     struct dp_netdev_flow netdev_flow;
     struct dpif_flow_stats stats;
     struct dpif_flow_attrs attrs;
+    int ret;
 
     /* if a direction is not offloaded do not attempt to query it from HW */
     if (!offload->dp) {
-        return false;
+        return EINVAL;
     }
     memset(&netdev_flow, 0, sizeof netdev_flow);
     *CONST_CAST(odp_port_t *, &netdev_flow.flow.in_port.odp_port) =
         offload->ct_match.odp_port;
     *CONST_CAST(ovs_u128 *, &netdev_flow.mega_ufid) = offload->ufid;
-    if (!dpif_netdev_get_flow_offload_status(offload->dp, &netdev_flow,
-                                             &stats, &attrs, now, prev_now)) {
-        return false;
+    ret = dpif_netdev_get_flow_offload_status(offload->dp, &netdev_flow,
+                                              &stats, &attrs, now, prev_now);
+    if (ret) {
+        return ret;
     }
 
-    return stats.used >= now;
+    return stats.used >= now ? 0 : EINVAL;
 }
 
 static int
@@ -4064,7 +4066,7 @@ dp_netdev_flow_get_last_stats_attrs(struct dp_netdev_flow *netdev_flow,
     atomic_read_relaxed(&last_attrs->dp_layer,     &attrs->dp_layer);
 }
 
-static bool
+static int
 dpif_netdev_get_flow_offload_status(const struct dp_netdev *dp,
                                     struct dp_netdev_flow *netdev_flow,
                                     struct dpif_flow_stats *stats,
@@ -4081,13 +4083,13 @@ dpif_netdev_get_flow_offload_status(const struct dp_netdev *dp,
     int ret = 0;
 
     if (!netdev_is_flow_api_enabled()) {
-        return false;
+        return EINVAL;
     }
 
     netdev = netdev_ports_get(netdev_flow->flow.in_port.odp_port,
                               dpif_normalize_type(dp->class->type));
     if (!netdev) {
-        return false;
+        return EINVAL;
     }
     ofpbuf_use_stack(&buf, &act_buf, sizeof act_buf);
     /* Taking a global 'port_mutex' to fulfill thread safety
@@ -4128,10 +4130,10 @@ dpif_netdev_get_flow_offload_status(const struct dp_netdev *dp,
     }
     netdev_close(netdev);
     if (ret) {
-        return merged_ret;
+        return merged_ret ? 0 : ret;
     }
 
-    return true;
+    return 0;
 }
 
 static void
@@ -4158,9 +4160,9 @@ get_dpif_flow_status(const struct dp_netdev *dp,
     atomic_read_relaxed(&netdev_flow->stats.tcp_flags, &flags);
     stats->tcp_flags = flags;
 
-    if (dpif_netdev_get_flow_offload_status(dp, netdev_flow,
-                                            &offload_stats, &offload_attrs, 0,
-                                            0)) {
+    if (!dpif_netdev_get_flow_offload_status(dp, netdev_flow,
+                                             &offload_stats, &offload_attrs, 0,
+                                             0)) {
         stats->n_packets += offload_stats.n_packets;
         stats->n_bytes += offload_stats.n_bytes;
         stats->used = MAX(stats->used, offload_stats.used);

commit 0e2c0b507473582088e171511605c51632279d7f

conntrack: Fix sweep timeout
A timeout of 1000 ms was implemented for ct_sweep() in case hardware
queries were momentarily blocked. If the thread takes longer, the
poller will warn about 'Unreasonably long poll interval'.

Without going back to the poller, ct_clean will not yield and the thread
will hold the core without giving the opportunity to other threads to
work as well.

This behavior is only expected from PMDs, not utility threads. Instead
of limiting the timeout check to the busy hardware queries, check it
anytime ct_clean quiesced, updating its 'now' timestamp. Wait for the
next conn to be popped to be able to evaluate the next wake-up time, and
exit the sweep if the timeout is reached.

Issue: 2590888
Change-Id: I24d95a3cf6600b007b4e6dc2009d361f96ca9bb2
Fixes: f889e1c635fe ("conntrack: Avoid querying during high lock contention")
Signed-off-by: Gaetan Rivet <grive@u256.net>

lib/conntrack.c
@@ -2015,22 +2015,18 @@ rcu_quiesce:
                 break;
             }
 
+            if (now - start >= CT_SWEEP_TIMEOUT_MS) {
+                min_expiration = MIN(min_expiration, conn_expiration(conn));
+                conn_expire_push_back(ct, conn);
+                goto out;
+            }
+
             rv_active = conn_hw_update(ct, offload_class, conn, exp->tm, now);
 
             if (rv_active == EAGAIN) {
                 /* Impossible to query offload status, try later. */
                 conn_expire_push_back(ct, conn);
-                if (now - start > CT_SWEEP_TIMEOUT_MS) {
-                    /* The hardware might be unavailable for status
-                     * update. Do not repeat RCU quiescing endlessly,
-                     * return to poll block if the sweep timeout is triggered.
-                     */
-                    min_expiration = MIN(now,
-                            MIN(min_expiration, conn_expiration(conn)));
-                    goto out;
-                } else {
-                    goto rcu_quiesce;
-                }
+                goto rcu_quiesce;
             }
 
             expiration = conn_expiration(conn);