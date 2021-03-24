Mellanox/OVS

commit ab3c3995fb640899c010a2788d1f2b8a9304ffcf

conntrack: Update expirations only from ct_clean
With the improved mpsc-queue API, this structure can be used with the
ct_clean process. This has several benefits:

  * No locks are necessary either on conn or ct_lock in the datapath
    to change a connection expiration.

  * The queue node itself takes one pointer less.

  * Having the rule that once enqueued, the expiration is managed *only*
    by the 'ct_clean' thread is simpler to reason with and verify for
    correctness.

The conn_expire node is thus redefined. It is meant to be enqueued once
when the connection is created, then only modified by 'ct_clean'.

A flag marks a connection when it was hit in the datapath. As its
expiration has been updated then, it could fool the ct_sweep process in
believing the end of expirable connections was reached. If the
connection is flagged for rescheduling however, then ct_sweep is aware
that the connection should instead be re-inserted at the back of the
queue, and continue processing.

To allow a connection to be freed by other threads (PMD, main), the
expiration node is allocated separately. This is not an issue for the
datapath as it will never write to it anyway, and 'ct_clean' is not a
bottleneck so can take the performance hit.

A refcount is held on the expiration node while it is in the queue,
which is then reduced once dequeued. If no other reference is held,
then the parent connection is known to have been removed, the update
process being skipped for it.

Rewriting 'ct_sweep()' to follow those changes, the 'updated' flag
marking a hardware hit on the connection is seen as redundant. The
'conn_hw_update()' is refactored and simplified. The sweeping loop
should be simpler to read.

Issue: 2440745
Change-Id: I06cd75dc25efa39f56bfef87bda7bb2c4412745f
Signed-off-by: Gaetan Rivet <grive@u256.net>

lib/conntrack.c
@@ -379,7 +379,7 @@ conntrack_init(void *dp)
     conntrack_lock(ct);
     cmap_init(&ct->conns);
     for (unsigned i = 0; i < ARRAY_SIZE(ct->exp_lists); i++) {
-        rculist_init(&ct->exp_lists[i]);
+        mpsc_queue_init(&ct->exp_lists[i]);
     }
     cmap_init(&ct->zone_limits);
 
@@ -528,6 +528,14 @@ zone_limit_delete(struct conntrack *ct, uint16_t zone)
 }
 
 static void
+conn_do_delete(struct conn *conn,
+               void (*delete_cb)(struct conn *))
+{
+    conn_expire_unref(conn->exp);
+    ovsrcu_postpone(delete_cb, conn);
+}
+
+static void
 conn_clean_cmn(struct conntrack *ct, struct conn *conn)
     OVS_REQUIRES(conn->lock, ct->ct_lock)
 {
@@ -568,9 +576,7 @@ conn_clean(struct conntrack *ct, struct conn *conn)
         uint32_t hash = conn_key_hash(&conn->nat_conn->key, ct->hash_basis);
         cmap_remove(&ct->conns, &conn->nat_conn->cm_node, hash);
     }
-    conn_expire_remove(&conn->exp, false);
-    conn->cleaned = true;
-    ovsrcu_postpone(delete_conn, conn);
+    conn_do_delete(conn, delete_conn);
     atomic_count_dec(&ct->n_conn);
 
     conntrack_unlock(ct);
@@ -590,11 +596,9 @@ conn_clean_one(struct conntrack *ct, struct conn *conn)
 
     conn_clean_cmn(ct, conn);
     if (conn->conn_type == CT_CONN_TYPE_DEFAULT) {
-        conn_expire_remove(&conn->exp, false);
-        conn->cleaned = true;
         atomic_count_dec(&ct->n_conn);
     }
-    ovsrcu_postpone(delete_conn_one, conn);
+    conn_do_delete(conn, delete_conn_one);
 
     conntrack_unlock(ct);
     conn_unlock(conn);
@@ -1231,22 +1235,11 @@ conn_not_found(struct conntrack *ct, struct dp_packet *pkt,
      * can limit DoS impact. */
 nat_res_exhaustion:
     free(nat_conn);
-    conn_expire_remove(&nc->exp, false);
-    ovsrcu_postpone(delete_conn_cmn, nc);
+    conn_do_delete(nc, delete_conn_cmn);
     static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(5, 5);
     VLOG_WARN_RL(&rl, "Unable to NAT due to tuple space exhaustion - "
                  "if DoS attack, use firewalling and/or zone partitioning.");
     return NULL;
-}
-
-static int
-conn_get_tm(struct conn *conn, enum ct_timeout *tm)
-{
-    if (!l4_protos[conn->key.nw_proto]->get_tm) {
-        return -1;
-    }
-    *tm = l4_protos[conn->key.nw_proto]->get_tm(conn);
-    return 0;
 }
 
 static bool
@@ -1915,29 +1908,24 @@ conn_batch_clean(struct conntrack *ct,
     *batch_count = 0;
 }
 
-static bool
+static int
 conn_hw_update(struct conntrack *ct,
                struct conntrack_offload_class *offload_class,
                struct conn *conn,
-               long long now,
-               int *rv_active)
+               enum ct_timeout tm,
+               long long now)
 {
     struct ct_flow_offload_item item;
-    enum ct_timeout tm = 0;
     bool updated = false;
+    int ret = 0;
     int dir;
-
-    if (!offload_class || !offload_class->conn_active ||
-        conn_get_tm(conn, &tm)) {
-        return false;
-    }
 
     for (dir = 0; dir < CT_DIR_NUM; dir++) {
         if (!updated &&
             conntrack_offload_fill_item_common(&item, conn, dir)) {
-            *rv_active = offload_class->conn_active(&item, now,
-                                                    conn->prev_query);
-            if (!*rv_active) {
+            ret = offload_class->conn_active(&item, now,
+                                             conn->prev_query);
+            if (!ret) {
                 conn_lock(conn);
                 conn_update_expiration(ct, conn, tm, now);
                 conn_unlock(conn);
@@ -1948,9 +1936,9 @@ conn_hw_update(struct conntrack *ct,
         if (!updated && conn->nat_conn &&
             conntrack_offload_fill_item_common(&item, conn->nat_conn,
                                                dir)) {
-            *rv_active = offload_class->conn_active(&item, now,
-                                                    conn->prev_query);
-            if (!*rv_active) {
+            ret = offload_class->conn_active(&item, now,
+                                             conn->prev_query);
+            if (!ret) {
                 conn_lock(conn);
                 conn_update_expiration(ct, conn, tm, now);
                 conn_unlock(conn);
@@ -1960,7 +1948,7 @@ conn_hw_update(struct conntrack *ct,
         }
     }
     conn->prev_query = now;
-    return updated;
+    return ret;
 }
 
 #define CT_SWEEP_BATCH_SIZE 32
@@ -1975,21 +1963,28 @@ static long long
 ct_sweep(struct conntrack *ct, long long now, size_t limit)
 {
     struct conntrack_offload_class *offload_class = NULL;
-    struct conn *conn;
     struct conn *conn_batch[CT_SWEEP_BATCH_SIZE];
+    struct mpsc_queue_node *node;
     size_t batch_count = 0;
     long long min_expiration = LLONG_MAX;
     long long int next_rcu_quiesce;
     long long int start = now;
     size_t count = 0;
-    bool hw_updated;
     int rv_active;
+
+    {
+        static bool queue_acquire_once;
+        if (!queue_acquire_once) {
+            for (unsigned i = 0; i < N_CT_TM; i++) {
+                mpsc_queue_acquire_wait(&ct->exp_lists[i]);
+            }
+            queue_acquire_once = true;
+        }
+    }
 
     next_rcu_quiesce = now + CT_SWEEP_QUIESCE_INTERVAL_MS;
     for (unsigned i = 0; i < N_CT_TM; i++) {
 
-        /* Quiesce outside of RCU list iteration,
-         * no reference should be held. */
         if (now >= next_rcu_quiesce) {
 rcu_quiesce:
             /* Do not delay further releasing batched conns if any. */
@@ -2004,15 +1999,25 @@ rcu_quiesce:
                                        &ct->offload_class);
         }
 
-        RCULIST_FOR_EACH (conn, exp.node, &ct->exp_lists[i]) {
-            bool next_list = false;
+        MPSC_QUEUE_FOR_EACH_POP (node, &ct->exp_lists[i]) {
+            struct conn_expire *exp;
+            long long int expiration;
+            struct conn *conn;
 
-            rv_active = 0;
-            hw_updated = conn_hw_update(ct, offload_class, conn, now,
-                                        &rv_active);
+            exp = CONTAINER_OF(node, struct conn_expire, node);
+            if (conn_expire_unref(exp)) {
+                /* Expire node was destroyed by conn RCU calls, nothing
+                 * remaining to do here.
+                 */
+                continue;
+            }
+            conn = exp->up;
+
+            rv_active = conn_hw_update(ct, offload_class, conn, exp->tm, now);
 
             if (rv_active == EAGAIN) {
                 /* Impossible to query offload status, try later. */
+                conn_expire_push_back(ct, conn);
                 if (now - start > CT_SWEEP_TIMEOUT_MS) {
                     /* The hardware might be unavailable for status
                      * update. Do not repeat RCU quiescing endlessly,
@@ -2026,42 +2031,43 @@ rcu_quiesce:
                 }
             }
 
-            if (!hw_updated) {
-                long long int expiration = conn_expiration(conn);
+            expiration = conn_expiration(conn);
 
-                if (now < expiration || count >= limit) {
-                    min_expiration = MIN(min_expiration, expiration);
-                    next_list = true;
+            if (now < expiration) {
+                if (atomic_flag_test_and_set(&conn->exp->reschedule)) {
+                    /* Reschedule was true, another thread marked
+                     * this conn to be enqueued again.
+                     * The conn is not yet expired, still valid, and
+                     * this list should still be iterated.
+                     */
+                    conn_expire_push_front(ct, conn);
                 } else {
-                    conn_batch[batch_count++] = conn;
-                    count++;
+                    /* This connection is still valid, while no other thread
+                     * modified it: it means this list iteration is finished
+                     * for now. Put back the connection within the list.
+                     */
+                    atomic_flag_clear(&conn->exp->reschedule);
+                    conn_expire_push_back(ct, conn);
+                    min_expiration = MIN(min_expiration, expiration);
+                    break;
                 }
-            }
-
-            if (batch_count == ARRAY_SIZE(conn_batch)) {
-                conn_batch_clean(ct, conn_batch, &batch_count);
+            } else {
+                conn_batch[batch_count++] = conn;
+                if (batch_count == ARRAY_SIZE(conn_batch)) {
+                    conn_batch_clean(ct, conn_batch, &batch_count);
+                }
+                count++;
+                if (count >= limit) {
+                    min_expiration = MIN(min_expiration, expiration);
+                    /* Do not check other lists. */
+                    COVERAGE_INC(conntrack_long_cleanup);
+                    goto out;
+                }
             }
 
             /* Attempt quiescing at fixed interval. */
             if (time_msec() >= next_rcu_quiesce) {
-                /* Restarting rculist iteration after quiescing should be fine,
-                 * we are always reading the list front.
-                 */
                 goto rcu_quiesce;
-            }
-
-            if (hw_updated) {
-                continue;
-            }
-
-            if (count >= limit) {
-                /* Do not check other lists. */
-                COVERAGE_INC(conntrack_long_cleanup);
-                goto out;
-            }
-
-            if (next_list) {
-                break;
             }
         }
     }

lib/conntrack-private.h
@@ -116,19 +116,14 @@ struct ct_offloads {
 };
 
 struct conn_expire {
-    /* Set once when initializing the expiration node. */
-    struct conntrack *ct;
+    struct mpsc_queue_node node;
+    struct conn *up;
     /* Timeout state of the connection.
      * It follows the connection state updates.
      */
     enum ct_timeout tm;
-    /* Insert and remove the expiration node only once per RCU syncs.
-     * If multiple threads update the connection, its expiration should
-     * be removed only once and added only once to timeout lists.
-     */
-    atomic_flag insert_once;
-    atomic_flag remove_once;
-    struct rculist node;
+    struct ovs_refcount refcount;
+    atomic_flag reschedule;
 };
 
 struct conn {
@@ -146,9 +141,11 @@ struct conn {
                             * True as soon as a thread has started freeing
                             * its memory. */
 
+    /* Inserted once by a PMD, then managed by the 'ct_clean' thread. */
+    struct conn_expire *exp;
+
     /* Mutable data. */
     struct ovs_spin lock; /* Guards all mutable fields. */
-    struct conn_expire exp;
     ovs_u128 label;
     atomic_llong expiration;
     long long prev_query;
@@ -195,7 +192,7 @@ enum ct_update_res {
 struct conntrack {
     struct ovs_spin ct_lock; /* Protects 2 following fields. */
     struct cmap conns OVS_GUARDED;
-    struct rculist exp_lists[N_CT_TM] OVS_GUARDED;
+    struct mpsc_queue exp_lists[N_CT_TM] OVS_GUARDED;
     struct cmap zone_limits OVS_GUARDED;
     struct cmap timeout_policies OVS_GUARDED;
 
@@ -272,19 +269,40 @@ tcp_payload_length(struct dp_packet *pkt)
     }
 }
 
-static inline void
-conn_expire_remove(struct conn_expire *exp, bool need_ct_lock)
-    OVS_NO_THREAD_SAFETY_ANALYSIS
+static inline bool
+conn_expire_tryref(struct conn_expire *exp)
 {
-    if (!atomic_flag_test_and_set(&exp->remove_once)
-        && rculist_next(&exp->node)) {
-        if (need_ct_lock) {
-            conntrack_lock(exp->ct);
-        }
-        rculist_remove(&exp->node);
-        if (need_ct_lock) {
-            conntrack_unlock(exp->ct);
-        }
+    return ovs_refcount_try_ref_rcu(&exp->refcount);
+}
+
+static inline bool
+conn_expire_unref(struct conn_expire *exp)
+{
+    if (ovs_refcount_unref(&exp->refcount) == 1) {
+        ovsrcu_postpone(free, exp);
+        return true;
+    }
+
+    return false;
+}
+
+static inline void
+conn_expire_push_front(struct conntrack *ct, struct conn *conn)
+{
+    if (conn_expire_tryref(conn->exp)) {
+        atomic_flag_clear(&conn->exp->reschedule);
+        mpsc_queue_insert(&ct->exp_lists[conn->exp->tm], &conn->exp->node);
+    }
+}
+
+static inline void
+conn_expire_push_back(struct conntrack *ct, struct conn *conn)
+{
+    if (conn_expire_tryref(conn->exp)) {
+        /* Do not change 'reschedule' state, if this expire node is put
+         * at the tail of the list, it will be re-examined next sweep.
+         */
+        mpsc_queue_push_back(&ct->exp_lists[conn->exp->tm], &conn->exp->node);
     }
 }
 

lib/conntrack-tp.c
@@ -237,49 +237,12 @@ tm_to_ct_dpif_tp(enum ct_timeout tm)
 }
 
 static void
-conn_expire_init(struct conn *conn, struct conntrack *ct)
-{
-    struct conn_expire *exp = &conn->exp;
-
-    if (exp->ct != NULL) {
-        return;
-    }
-
-    exp->ct = ct;
-    atomic_flag_clear(&exp->insert_once);
-    atomic_flag_clear(&exp->remove_once);
-    /* The expiration is initially unscheduled, flag it as 'removed'. */
-    atomic_flag_test_and_set(&exp->remove_once);
-}
-
-static void
-conn_expire_insert(struct conn *conn)
-{
-    struct conn_expire *exp = &conn->exp;
-
-    conn_lock(conn);
-
-    if (!conn->cleaned) {
-        conntrack_lock(exp->ct);
-        rculist_push_back(&exp->ct->exp_lists[exp->tm], &exp->node);
-        conntrack_unlock(exp->ct);
-
-        atomic_flag_clear(&exp->insert_once);
-        atomic_flag_clear(&exp->remove_once);
-    }
-
-    conn_unlock(conn);
-}
-
-static void
 conn_schedule_expiration(struct conn *conn, enum ct_timeout tm, long long now,
                          uint32_t tp_value)
 {
     atomic_store_relaxed(&conn->expiration, now + tp_value * 1000);
-    conn->exp.tm = tm;
-    if (!atomic_flag_test_and_set(&conn->exp.insert_once)) {
-        ovsrcu_postpone(conn_expire_insert, conn);
-    }
+    conn->exp->tm = tm;
+    ignore(atomic_flag_test_and_set(&conn->exp->reschedule));
 }
 
 static uint32_t
@@ -311,10 +274,17 @@ conn_update_expiration(struct conntrack *ct, struct conn *conn,
                 "val=%u sec.",
                 ct_timeout_str[tm], conn->key.zone, conn->tp_id, tp_value);
 
-    if (!conn->cleaned) {
-        conn_expire_remove(&conn->exp, true);
-        conn_schedule_expiration(conn, tm, now, tp_value);
-    }
+    conn_schedule_expiration(conn, tm, now, tp_value);
+}
+
+static void
+conn_expire_init(struct conn *conn)
+{
+    ovs_assert(conn->exp == NULL);
+    conn->exp = xmalloc(sizeof *conn->exp);
+    conn->exp->up = conn;
+    atomic_flag_clear(&conn->exp->reschedule);
+    ovs_refcount_init(&conn->exp->refcount);
 }
 
 void
@@ -334,6 +304,7 @@ conn_init_expiration(struct conntrack *ct, struct conn *conn,
     VLOG_DBG_RL(&rl, "Init timeout %s zone=%u with policy id=%d val=%u sec.",
                 ct_timeout_str[tm], conn->key.zone, conn->tp_id, val);
 
-    conn_expire_init(conn, ct);
+    conn_expire_init(conn);
     conn_schedule_expiration(conn, tm, now, val);
+    conn_expire_push_front(ct, conn);
 }

commit d4e1de4261fb260a6a1fdd4d467314389bac45af

conntrack: Sweep the queue only once
When a connection is still active, hardware queries will update it and
flag it for rescheduling. The connection will then be re-enqueued at the
end of the current queue.

If the queue only contains such active connections, then the iteration
is endless: connections are always moved at the end of the queue, and
the sweep will continue querying the hardware until timeout or an
expiration.

This is useless. If the current connection was already processed during
the sweep, then the queue was fully processed and the sweep should
yield. It will start again close to the next expiration time.

Mark the first rescheduled connection and stop the sweep when it is
seen a second time.

Issue: 2590429
Change-Id: I812858616eadb6456e77203cc74ec5af958316b3
Fixes: ab3c3995fb64 ("conntrack: Update expirations only from ct_clean")
Signed-off-by: Gaetan Rivet <grive@u256.net>

lib/conntrack.c
@@ -1977,6 +1977,7 @@ ct_sweep(struct conntrack *ct, long long now, size_t limit)
 
     next_rcu_quiesce = now + CT_SWEEP_QUIESCE_INTERVAL_MS;
     for (unsigned i = 0; i < N_CT_TM; i++) {
+        struct conn *end_of_queue = NULL;
 
         if (now >= next_rcu_quiesce) {
 rcu_quiesce:
@@ -2005,6 +2006,14 @@ rcu_quiesce:
                 continue;
             }
             conn = exp->up;
+            if (conn == end_of_queue) {
+                /* If we already re-enqueued this conn during this sweep,
+                 * stop iterating this list and skip to the next.
+                 */
+                min_expiration = MIN(min_expiration, conn_expiration(conn));
+                conn_expire_push_back(ct, conn);
+                break;
+            }
 
             rv_active = conn_hw_update(ct, offload_class, conn, exp->tm, now);
 
@@ -2034,6 +2043,9 @@ rcu_quiesce:
                      * this list should still be iterated.
                      */
                     conn_expire_push_front(ct, conn);
+                    if (end_of_queue == NULL) {
+                        end_of_queue = conn;
+                    }
                 } else {
                     /* This connection is still valid, while no other thread
                      * modified it: it means this list iteration is finished