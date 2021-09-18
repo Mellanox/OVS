Mellanox/OVS

commit 2c539742ecdb7ad0b06e0c42c77362d36fde8a02

cmap: Add set min load function
Add a new function to the cmap API, that configures the min load factor
triggering a shrinking rehash.

Setting this attribute will change internal representation of the cmap,
as such it needs to be done only when the cmap is write-protected.

It can be done immediately after cmap init.

Issue: 2440745
Change-Id: I41c3f7d091e0da84e4603e57d984116152672b94
Signed-off-by: Gaetan Rivet <grive@u256.net>

lib/cmap.c
@@ -167,6 +167,7 @@ struct cmap_impl {
         uint32_t mask;              /* Number of 'buckets', minus one. */
         uint32_t basis;             /* Basis for rehashing client's
                                        hash values. */
+        uint32_t min_load;          /* User-configured min-load. */
     );
 
     PADDED_MEMBERS_CACHELINE_MARKER(CACHE_LINE_SIZE, cacheline1,
@@ -219,6 +220,12 @@ calc_min_n(uint32_t mask)
     return ((uint64_t) (mask + 1) * CMAP_K * CMAP_MIN_LOAD) >> 32;
 }
 
+static void
+set_custom_min_n(struct cmap_impl *impl, uint32_t mask)
+{
+    impl->min_n = ((uint64_t) (mask + 1) * CMAP_K * impl->min_load) >> 32;
+}
+
 static struct cmap_impl *
 cmap_impl_create(uint32_t mask)
 {
@@ -236,6 +243,21 @@ cmap_impl_create(uint32_t mask)
     impl->basis = random_uint32();
 
     return impl;
+}
+
+void
+cmap_set_min_load(struct cmap *cmap, double load)
+{
+    struct cmap_impl *impl = cmap_get_impl(cmap);
+
+    if (impl == &empty_cmap) {
+        /* Do no set the min_load for the shared empty_cmap,
+         * it would propagate to all CMAPs. Force a rehash to get
+         * a new implementation, and set it there.
+         */
+        impl = cmap_rehash(cmap, impl->mask);
+    }
+    impl->min_load = ((uint32_t) (UINT32_MAX * load));
 }
 
 /* Initializes 'cmap' as an empty concurrent hash map. */
@@ -960,6 +982,11 @@ cmap_rehash(struct cmap *cmap, uint32_t mask)
 
     new = cmap_impl_create(mask);
     ovs_assert(old->n < new->max_n);
+
+    if (old->min_load != CMAP_MIN_LOAD) {
+        new->min_load = old->min_load;
+        set_custom_min_n(new, mask);
+    }
 
     while (!cmap_try_rehash(old, new)) {
         memset(new->buckets, 0, (mask + 1) * sizeof *new->buckets);

lib/cmap.h
@@ -89,6 +89,8 @@ extern OVS_ALIGNED_VAR(CACHE_LINE_SIZE) const struct cmap_impl empty_cmap;
 void cmap_init(struct cmap *);
 void cmap_destroy(struct cmap *);
 
+void cmap_set_min_load(struct cmap *, double load);
+
 /* Count. */
 size_t cmap_count(const struct cmap *);
 bool cmap_is_empty(const struct cmap *);

commit d6ca2b41782441def1f3e6f608a7db1985f10afa

cmap: Fix default min load
All CMAPs start empty, and are linked to the shared empty_cmap
implementation. This default instance is initialized with all fields
zeroed, however it means its default min-load factor is also zero.

This is incorrect, as it should instead be CMAP_MIN_LOAD.
The error has for effect that the default min-load of zero
is propagated to all subsequent CMAP implementations allocated
during cmap_rehash() as copies.

Issue: 2793876
Change-Id: I73862ad33f350a265be4ee88d8f4727e6b724601
Fixes: 2c539742ecdb ("cmap: Add set min load function")
Signed-off-by: Gaetan Rivet <grive@u256.net>

lib/cmap.c
@@ -177,7 +177,9 @@ struct cmap_impl {
 BUILD_ASSERT_DECL(sizeof(struct cmap_impl) == CACHE_LINE_SIZE * 2);
 
 /* An empty cmap. */
-OVS_ALIGNED_VAR(CACHE_LINE_SIZE) const struct cmap_impl empty_cmap;
+OVS_ALIGNED_VAR(CACHE_LINE_SIZE) const struct cmap_impl empty_cmap = {
+    .min_load = CMAP_MIN_LOAD,
+};
 
 static struct cmap_impl *cmap_rehash(struct cmap *, uint32_t mask);
 